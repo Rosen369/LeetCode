2f44472510221b0aba3b983d7aa643e4dd27c193
rosen <rosen369@outlook.com>
2018-05-10 15:46:35 +0800

    resolve palindrome number

--- 9. Palindrome Number/Solution.cs
+++ 9. Palindrome Number/Solution.cs
@@ -1,5 +1,15 @@
 public class Solution {
-    public bool IsPalindrome(int x) {
-        
+    public bool IsPalindrome (int x) {
+        if (x < 0) {
+            return false;
+        }
+        var left = x;
+        var reverse = 0;
+        while (left > 0) {
+            var digit = left % 10;
+            left = left / 10;
+            reverse = reverse * 10 + digit;
+        }
+        return reverse == x;
     }
 }
