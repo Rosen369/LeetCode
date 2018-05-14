35e0db1a014bd2b772a437066c92ef4b831d4873
rosen <rosen369@outlook.com>
2018-05-14 17:14:35 +0800

    resolve container with most water

--- 11. Container With Most Water/Solution.cs
+++ 11. Container With Most Water/Solution.cs
@@ -1,5 +1,15 @@
 public class Solution {
-    public int MaxArea(int[] height) {
-        
+    public int MaxArea (int[] height) {
+        var max = 0;
+        var l = 0;
+        var r = height.Length - 1;
+        while (l < r) {
+            max = Math.Max (max, Math.Min (height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return max;
     }
 }
