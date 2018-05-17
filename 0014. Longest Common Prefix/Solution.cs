76d8662383c30c51706fc80398576fd4415c9487
rosen <rosen369@outlook.com>
2018-05-17 09:45:04 +0800

    resolve longest common prefix

--- 0014. Longest Common Prefix/Solution.cs
+++ 0014. Longest Common Prefix/Solution.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public string LongestCommonPrefix (string[] strs) {
-
+        var result = string.Empty;
+        if (strs.Length == 0) {
+            return result;
+        }
+        for (int i = 0; i < strs[0].Length; i++) {
+            for (int j = 0; j < strs.Length; j++) {
+                if (strs[j].Length <= i) {
+                    return result;
+                }
+                if (strs[j][i] != strs[0][i]) {
+                    return result;
+                }
+            }
+            result += strs[0][i];
+        }
+        return result;
     }
 }
