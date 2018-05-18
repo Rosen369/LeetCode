69cd3cb3ea40c1955dbc9661570c0b07bc160730
rosen <rosen369@outlook.com>
2018-05-18 14:58:00 +0800

    resolve 4sum

--- 0018. 4Sum/Solution.cs
+++ 0018. 4Sum/Solution.cs
@@ -1,5 +1,28 @@
 public class Solution {
     public IList<IList<int>> FourSum (int[] nums, int target) {
-
+        Array.Sort (nums);
+        var map = new Dictionary<string, IList<int>> ();
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = i + 1; j < nums.Length; j++) {
+                var left = j + 1;
+                var right = nums.Length - 1;
+                while (left < right) {
+                    var sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        var key = string.Empty + nums[i] + nums[j] + nums[left] + nums[right];
+                        if (!map.ContainsKey (key)) {
+                            map.Add (key, new List<int> { nums[i], nums[j], nums[left], nums[right] });
+                        }
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return map.Values.ToList ();
     }
 }
