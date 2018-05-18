f9923979c26651c45da7a1e19ed6c01fb4f28620
rosen <rosen369@outlook.com>
2018-05-18 16:10:16 +0800

    resolve remove nth node from end of list

--- 0019. Remove Nth Node From End of List/Solution.cs
+++ 0019. Remove Nth Node From End of List/Solution.cs
@@ -1,6 +1,24 @@
 public class Solution {
     public ListNode RemoveNthFromEnd (ListNode head, int n) {
-
+        var index = 0;
+        var map = new Dictionary<int, ListNode> ();
+        var current = head;
+        while (current != null) {
+            map.Add (index, current);
+            index++;
+            current = current.next;
+        }
+        var tail = index - n;
+        if (map.ContainsKey (tail - 1) && map.ContainsKey (tail + 1)) {
+            map[tail - 1].next = map[tail + 1];
+        } else if (map.ContainsKey (tail - 1) && !map.ContainsKey (tail + 1)) {
+            map[tail - 1].next = null;
+        } else if (!map.ContainsKey (tail - 1) && map.ContainsKey (tail + 1)) {
+            return map[tail + 1];
+        } else {
+            return null;
+        }
+        return head;
     }
 }
 
