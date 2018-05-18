c469a91812499d70ab53b984bdea58d23e961ddf
rosen <rosen369@outlook.com>
2018-05-18 16:33:21 +0800

    add merge two sorted lists

---
+++ 0021. Merge Two Sorted Lists/Solution.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public ListNode MergeTwoLists (ListNode l1, ListNode l2) {
+
+    }
+}
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode (int x) { val = x; }
+}
