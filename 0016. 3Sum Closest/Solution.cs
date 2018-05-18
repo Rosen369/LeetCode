9d232b11685efd3d7a9454d9da7ef9bd091cd1e5
rosen <rosen369@outlook.com>
2018-05-18 10:14:23 +0800

    resolve 3sum closest

--- 0016. 3Sum Closest/Solution.cs
+++ 0016. 3Sum Closest/Solution.cs
@@ -1,5 +1,25 @@
 public class Solution {
     public int ThreeSumClosest (int[] nums, int target) {
-
+        var result = nums[0] + nums[1] + nums[2];
+        Array.Sort (nums);
+        for (int i = 0; i < nums.Length; i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            var left = i + 1;
+            var right = nums.Length - 1;
+            while (left < right) {
+                var sum = nums[i] + nums[left] + nums[right];
+                if (Math.Abs (sum - target) < Math.Abs (result - target)) {
+                    result = sum;
+                }
+                if (target > sum) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
     }
 }
