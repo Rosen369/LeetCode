cd5243486f14cc4b08f015261c472cec74c7182c
rosen <rosen369@outlook.com>
2018-05-21 10:42:39 +0800

    resolve by circulation

--- 0021. Merge Two Sorted Lists/Solution.cs
+++ 0021. Merge Two Sorted Lists/Solution.cs
@@ -1,6 +1,33 @@
 public class Solution {
     public ListNode MergeTwoLists (ListNode l1, ListNode l2) {
-
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        var head = l1;
+        var current = l1;
+        var insert = l2;
+        if (l1.val > l2.val) {
+            head = l2;
+            current = l2;
+            insert = l1;
+        }
+        while (insert != null) {
+            if (current.next == null) {
+                current.next = insert;
+                break;
+            }
+            if (current.next.val > insert.val) {
+                var next = current.next;
+                current.next = insert;
+                insert = insert.next;
+                current.next.next = next;
+            }
+            current = current.next;
+        }
+        return head;
     }
 }
 
