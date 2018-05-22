0fb7abcafe479ff025520728f63169a7da578f80
rosen <rosen369@outlook.com>
2018-05-22 16:16:11 +0800

    resolve swap nodes in pairs

--- 0024. Swap Nodes in Pairs/Solution.cs
+++ 0024. Swap Nodes in Pairs/Solution.cs
@@ -1,6 +1,12 @@
 public class Solution {
     public ListNode SwapPairs (ListNode head) {
-
+        if (head == null || head.next == null) {
+            return head;
+        }
+        var res = head.next;
+        head.next = SwapPairs (head.next.next);
+        res.next = head;
+        return res;
     }
 }
 
