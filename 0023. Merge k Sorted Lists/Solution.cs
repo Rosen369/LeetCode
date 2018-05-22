13807c6fddbc7ea1f0aec0c4c53998304b460fde
rosen <rosen369@outlook.com>
2018-05-22 13:54:05 +0800

    resolve merge k sorted lists

---
+++ 0023. Merge k Sorted Lists/Solution.cs
@@ -0,0 +1,43 @@
+public class Solution {
+    public ListNode MergeKLists (ListNode[] lists) {
+        if (lists.Length == 0) {
+            return null;
+        }
+        while (lists.Length > 1) {
+            var roundRes = new List<ListNode> ();
+            for (int i = 0; i < lists.Length; i = i + 2) {
+                if (i + 1 < lists.Length) {
+                    roundRes.Add (MergeTwoLists (lists[i], lists[i + 1]));
+                } else {
+                    roundRes.Add (lists[i]);
+                }
+            }
+            lists = roundRes.ToArray ();
+        }
+        return lists[0];
+    }
+
+    public ListNode MergeTwoLists (ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode res = null;
+        if (l1.val < l2.val) {
+            res = l1;
+            res.next = MergeTwoLists (l1.next, l2);
+        } else {
+            res = l2;
+            res.next = MergeTwoLists (l1, l2.next);
+        }
+        return res;
+    }
+}
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode (int x) { val = x; }
+}
