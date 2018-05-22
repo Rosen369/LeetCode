5a9335eefacf2b586fd7084074be1e230fc795a4
rosen <rosen369@outlook.com>
2018-05-22 16:21:05 +0800

    add reverse nodes in k group

---
+++ 0025. Reverse Nodes in k-Group/Solution.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public ListNode ReverseKGroup (ListNode head, int k) {
+
+    }
+}
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode (int x) { val = x; }
+}
