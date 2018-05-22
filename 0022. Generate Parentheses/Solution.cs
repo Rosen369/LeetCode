0d2c5925ebd3ba688ddff08aacd0c30bafa8403b
rosen <rosen369@outlook.com>
2018-05-22 10:28:37 +0800

    resolve generate parentheses

--- 0022. Generate Parentheses/Solution.cs
+++ 0022. Generate Parentheses/Solution.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public IList<string> GenerateParenthesis (int n) {
+        var res = new List<string> ();
+        BackTrack (res, string.Empty, 0, 0, n);
+        return res;
+    }
 
+    public void BackTrack (IList<string> list, string subStr, int open, int close, int n) {
+        if (subStr.Length == n * 2) {
+            list.Add (subStr);
+            return;
+        }
+        if (open < n) {
+            BackTrack (list, subStr + '(', open + 1, close, n);
+        }
+        if (close < open) {
+            BackTrack (list, subStr + ')', open, close + 1, n);
+        }
     }
 }
