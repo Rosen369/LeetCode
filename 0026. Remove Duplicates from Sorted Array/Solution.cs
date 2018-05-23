850b14315111ff996a8fb77759cfaa263b036d70
rosen <rosen369@outlook.com>
2018-05-23 16:55:30 +0800

    resolve remove duplicates from sorted array

--- 0026. Remove Duplicates from Sorted Array/Solution.cs
+++ 0026. Remove Duplicates from Sorted Array/Solution.cs
@@ -1,5 +1,15 @@
 public class Solution {
     public int RemoveDuplicates (int[] nums) {
-
+        if (nums.Length < 2) {
+            return nums.Length;
+        }
+        var len = 0;
+        for (var i = 1; i < nums.Length; i++) {
+            if (nums[i] != nums[len]) {
+                len++;
+                nums[len] = nums[i];
+            }
+        }
+        return len + 1;
     }
 }
