3e0c0b5e9fad0e37eb1275aac91ce9f1ff6d2362
rosen <rosen369@outlook.com>
2018-05-23 17:32:49 +0800

    resolve remove element

--- 0027. Remove Element/Solution.cs
+++ 0027. Remove Element/Solution.cs
@@ -1,5 +1,19 @@
 public class Solution {
     public int RemoveElement (int[] nums, int val) {
-
+        var left = 0;
+        var right = nums.Length - 1;
+        while (left <= right) {
+            if (nums[left] == val) {
+                if (nums[right] == val) {
+                    right--;
+                    continue;
+                } else {
+                    nums[left] = nums[right];
+                    right--;
+                }
+            }
+            left++;
+        }
+        return right + 1;
     }
 }
