56523331e1c216d02cda8a24a907c607042f0446
rosen <rosen369@outlook.com>
2018-05-23 14:47:16 +0800

    resolve reverse nodes in k group

--- 0025. Reverse Nodes in k-Group/Solution.cs
+++ 0025. Reverse Nodes in k-Group/Solution.cs
@@ -1,6 +1,28 @@
 public class Solution {
     public ListNode ReverseKGroup (ListNode head, int k) {
-
+        var pre = new ListNode (0);
+        var curr = pre;
+        var index = 0;
+        var buffer = new ListNode[k];
+        while (head != null) {
+            buffer[index] = head;
+            head = head.next;
+            buffer[index].next = null;
+            if (index == k - 1) {
+                for (int i = k - 1; i >= 0; i--) {
+                    curr.next = buffer[i];
+                    curr = curr.next;
+                }
+                index = 0;
+                continue;
+            }
+            index++;
+        }
+        for (int i = 0; i < index; i++) {
+            curr.next = buffer[i];
+            curr = curr.next;
+        }
+        return pre.next;
     }
 }
 
