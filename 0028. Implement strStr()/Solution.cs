e4ff85582ad0772660a3f145c2207cf1ce1d44d5
rosen <rosen369@outlook.com>
2018-05-24 17:40:16 +0800

    resolve implement strStr()

--- 0028. Implement strStr()/Solution.cs
+++ 0028. Implement strStr()/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public int StrStr (string haystack, string needle) {
-
+        for (int i = 0;; i++) {
+            for (int j = 0;; j++) {
+                if (j == needle.Length) {
+                    return i;
+                }
+                if (i + j == haystack.Length) {
+                    return -1;
+                }
+                if (needle[j] != haystack[i + j]) {
+                    break;
+                }
+            }
+        }
     }
 }
