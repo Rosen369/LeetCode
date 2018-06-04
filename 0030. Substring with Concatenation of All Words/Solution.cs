f88c8b4fcb623a54422d5cf1016e51b1f64cb7c7
rosen <rosen369@outlook.com>
2018-06-04 15:54:58 +0800

    Time Limit Exceeded

--- 0030. Substring with Concatenation of All Words/Solution.cs
+++ 0030. Substring with Concatenation of All Words/Solution.cs
@@ -1,5 +1,35 @@
 public class Solution {
     public IList<int> FindSubstring (string s, string[] words) {
+        var res = new List<int> ();
+        if (words.Length == 0) {
+            return res;
+        }
+        for (int i = 0; i < s.Length; i++) {
+            if (MatchNextWord (s.Substring (i), words.ToList ())) {
+                res.Add (i);
+            }
+        }
+        return res;
+    }
 
+    public bool MatchNextWord (string s, IList<string> words) {
+        if (words.Count () == 0) {
+            return true;
+        }
+        for (int i = 0; i < words.Count (); i++) {
+            if (words.Count () == 1 && s == words[i]) {
+                return true;
+            }
+            if (s.Length < words[i].Length) {
+                continue;
+            }
+            var match = s.Substring (0, words[i].Length) == words[i];
+            if (match) {
+                var sub = s.Substring (words[i].Length);
+                words.RemoveAt (i);
+                return MatchNextWord (sub, words);
+            }
+        }
+        return false;
     }
 }
