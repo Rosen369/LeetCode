0a278a0e963f2d00b852b55bf1bbe1af3c1e6c00
rosen <rosen369@outlook.com>
2018-06-06 14:25:17 +0800

    resolve longest valid parentheses

--- 0032. Longest Valid Parentheses/Solution.cs
+++ 0032. Longest Valid Parentheses/Solution.cs
@@ -1,5 +1,24 @@
 public class Solution {
     public int LongestValidParentheses (string s) {
-
+        var res = 0;
+        var stack = new Stack<int> ();
+        stack.Push (-1);
+        for (int i = 0; i < s.Length; i++) {
+            if (s[i] == '(') {
+                stack.Push (i);
+            } else {
+                if (stack.Count == 0) {
+                    break;
+                } else {
+                    stack.Pop ();
+                    if (stack.Count == 0) {
+                        stack.Push (i);
+                    } else {
+                        res = Math.Max (res, i - stack.Peek ());
+                    }
+                }
+            }
+        }
+        return res;
     }
 }
