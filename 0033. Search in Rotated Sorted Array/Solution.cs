f3ea329806e6d0a7563e6b382474d3ba949cf5aa
rosen <rosen369@outlook.com>
2018-06-12 14:30:19 +0800

    resolve search in rotated sorted array

--- 0033. Search in Rotated Sorted Array/Solution.cs
+++ 0033. Search in Rotated Sorted Array/Solution.cs
@@ -1,5 +1,25 @@
 public class Solution {
     public int Search (int[] nums, int target) {
-
+        var low = 0;
+        var high = nums.Length;
+        while (low < high) {
+            var mid = (low + high) / 2;
+            var num = nums[mid];
+            if (nums[mid] < nums[0] != target < nums[0]) {
+                if (target < nums[0]) {
+                    num = int.MinValue;
+                } else {
+                    num = int.MaxValue;
+                }
+            }
+            if (num < target) {
+                low = mid + 1;
+            } else if (num > target) {
+                high = mid;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
