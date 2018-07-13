3026bdc8a1d63c3b6c61bcadb33a831cfa423761
rosen369 <rosen369@outlook.com>
2018-07-13 16:24:24 +0800

    optimize run time

--- 0037. Sudoku Solver/Solution.cs
+++ 0037. Sudoku Solver/Solution.cs
@@ -1,16 +1,16 @@
 public class Solution {
     public void SolveSudoku (char[, ] board) {
-        FillAndValid (board);
+        FillSudoku (board);
     }
 
-    public bool FillAndValid (char[, ] board) {
+    public bool FillSudoku (char[, ] board) {
         for (int x = 0; x < 9; x++) {
             for (int y = 0; y < 9; y++) {
                 if (board[x, y] == '.') {
                     for (char c = '1'; c <= '9'; c++) {
                         board[x, y] = (char) c;
-                        if (IsValidSudoku (board)) {
-                            if (FillAndValid (board)) {
+                        if (ValidSudoku (board, x, y)) {
+                            if (FillSudoku (board)) {
                                 return true;
                             }
                         }
@@ -23,52 +23,42 @@ public class Solution {
         return true;
     }
 
-    public bool IsValidSudoku (char[, ] board) {
-        // valid each row
-        for (int row = 0; row < 9; row++) {
-            var list = new List<char> ();
-            for (int i = 0; i < 9; i++) {
-                if (board[i, row] == '.') {
-                    continue;
-                }
-                if (list.Contains (board[i, row])) {
-                    return false;
-                } else {
-                    list.Add (board[i, row]);
-                }
+    public bool ValidSudoku (char[, ] board, int x, int y) {
+        // valid row
+        for (int i = 0; i < 9; i++) {
+            if (i == x) {
+                continue;
+            }
+            if (board[i, y] == '.') {
+                continue;
+            }
+            if (board[i, y] == board[x, y]) {
+                return false;
             }
         }
-        // valid each column
-        for (int column = 0; column < 9; column++) {
-            var list = new List<char> ();
-            for (int i = 0; i < 9; i++) {
-                if (board[column, i] == '.') {
-                    continue;
-                }
-                if (list.Contains (board[column, i])) {
-                    return false;
-                } else {
-                    list.Add (board[column, i]);
-                }
+        // valid column
+        for (int i = 0; i < 9; i++) {
+            if (i == y) {
+                continue;
+            }
+            if (board[x, i] == '.') {
+                continue;
+            }
+            if (board[x, i] == board[x, y]) {
+                return false;
             }
         }
-        // valid each box
-        for (int boxRow = 0; boxRow < 3; boxRow++) {
-            for (int boxColumn = 0; boxColumn < 3; boxColumn++) {
-                var list = new List<char> ();
-                for (int row = 0; row < 3; row++) {
-                    for (int column = 0; column < 3; column++) {
-                        var x = boxColumn * 3 + column;
-                        var y = boxRow * 3 + row;
-                        if (board[x, y] == '.') {
-                            continue;
-                        }
-                        if (list.Contains (board[x, y])) {
-                            return false;
-                        } else {
-                            list.Add (board[x, y]);
-                        }
-                    }
+        // valid box
+        for (int row = y / 3 * 3; row < y / 3 * 3 + 3; row++) {
+            for (int column = x / 3 * 3; column < x / 3 * 3 + 3; column++) {
+                if (column == x && row == y) {
+                    continue;
+                }
+                if (board[column, row] == '.') {
+                    continue;
+                }
+                if (board[column, row] == board[x, y]) {
+                    return false;
                 }
             }
         }
