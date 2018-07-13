ac31332b2b63182e715b6e64c6bc6933824ad58e
rosen369 <rosen369@outlook.com>
2018-07-13 10:33:20 +0800

    resolve search insert position

--- 0035. Search Insert Position/Solution.cs
+++ 0035. Search Insert Position/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public int SearchInsert (int[] nums, int target) {
-
+        var left = 0;
+        var right = nums.Length;
+        while (left < right) {
+            var mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
