3e6980e941347c2f13b4a39748736c1706a36a74
rosen369 <rosen369@outlook.com>
2018-07-16 16:46:58 +0800

    optimize runtime

--- 0040. Combination Sum II/Solution.cs
+++ 0040. Combination Sum II/Solution.cs
@@ -2,30 +2,23 @@ public class Solution {
     public IList<IList<int>> CombinationSum2 (int[] candidates, int target) {
         var res = new Dictionary<string, IList<int>> ();
         Array.Sort (candidates);
-        SearchNext (res, candidates, target, 0, new List<int> ());
+        SearchNext (res, candidates, target, 0, new List<int> (), 0);
         return res.Values.ToList ();
     }
 
-    public void SearchNext (IDictionary<string, IList<int>> res, int[] candidates, int target, int index, IList<int> current) {
+    public void SearchNext (IDictionary<string, IList<int>> res, int[] candidates, int target, int index, IList<int> current, int sum) {
         for (int i = index; i < candidates.Length; i++) {
             var combine = new List<int> (current);
-            var sum = SumList (combine);
-            if (sum == target) {
+            combine.Add (candidates[i]);
+            var curr_sum = sum + candidates[i];
+            if (curr_sum == target) {
                 var key = string.Join ("_", combine.ToArray ());
                 if (!res.ContainsKey (key)) {
                     res.Add (key, combine);
                 }
-            } else if (sum < target) {
-                SearchNext (res, candidates, target, i + 1, combine);
+            } else if (curr_sum < target) {
+                SearchNext (res, candidates, target, i + 1, combine, curr_sum);
             }
         }
-    }
-
-    public int SumList (IList<int> list) {
-        var res = 0;
-        foreach (var item in list) {
-            res += item;
-        }
-        return res;
     }
 }
