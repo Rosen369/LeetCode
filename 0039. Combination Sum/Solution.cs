91e7a9988d10a42c1e0c34f1e1b74e794ca161a7
rosen369 <rosen369@outlook.com>
2018-07-16 16:10:30 +0800

    resolve combination sum

--- 0039. Combination Sum/Solution.cs
+++ 0039. Combination Sum/Solution.cs
@@ -1,5 +1,33 @@
 public class Solution {
     public IList<IList<int>> CombinationSum (int[] candidates, int target) {
+        var res = new Dictionary<string, IList<int>> ();
+        BackTrack (res, candidates, target, new List<int> ());
+        return res.Values.ToList ();
+    }
 
+    public void BackTrack (IDictionary<string, IList<int>> res, int[] candidates, int target, IList<int> current) {
+        for (int i = 0; i < candidates.Length; i++) {
+            var combine = new List<int> ();
+            combine.AddRange (current.ToArray ());
+            combine.Add (candidates[i]);
+            var sum = SumList (combine);
+            if (sum == target) {
+                combine.Sort ();
+                var key = string.Join ("_", combine.ToArray ());
+                if (!res.ContainsKey (key)) {
+                    res.Add (key, combine);
+                }
+            } else if (sum < target) {
+                BackTrack (res, candidates, target, combine);
+            }
+        }
+    }
+
+    public int SumList (IList<int> list) {
+        var res = 0;
+        foreach (var item in list) {
+            res += item;
+        }
+        return res;
     }
 }
