000589c25d3e8e753f234146111cad302d42d0cc
rosen369 <rosen369@outlook.com>
2018-07-16 16:48:58 +0800

    optimize runtime

--- 0039. Combination Sum/Solution.cs
+++ 0039. Combination Sum/Solution.cs
@@ -1,29 +1,20 @@
 public class Solution {
     public IList<IList<int>> CombinationSum (int[] candidates, int target) {
         var res = new List<IList<int>> ();
-        SearchNext (res, candidates, target, 0, new List<int> ());
+        SearchNext (res, candidates, target, 0, new List<int> (), 0);
         return res;
     }
 
-    public void SearchNext (IList<IList<int>> res, int[] candidates, int target, int index, IList<int> current) {
+    public void SearchNext (IList<IList<int>> res, int[] candidates, int target, int index, IList<int> current, int sum) {
         for (int i = index; i < candidates.Length; i++) {
-            var combine = new List<int> ();
-            combine.AddRange (current.ToArray ());
+            var combine = new List<int> (current);
             combine.Add (candidates[i]);
-            var sum = SumList (combine);
-            if (sum == target) {
+            var curr_sum = sum + candidates[i];
+            if (curr_sum == target) {
                 res.Add (combine);
-            } else if (sum < target) {
-                SearchNext (res, candidates, target, i, combine);
+            } else if (curr_sum < target) {
+                SearchNext (res, candidates, target, i, combine, curr_sum);
             }
         }
-    }
-
-    public int SumList (IList<int> list) {
-        var res = 0;
-        foreach (var item in list) {
-            res += item;
-        }
-        return res;
     }
 }
