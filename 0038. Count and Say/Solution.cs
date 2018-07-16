d8ec1f147cccb3763dbf150ca5c7de86943ed8fa
rosen369 <rosen369@outlook.com>
2018-07-16 09:48:02 +0800

    resolve count and say

--- 0038. Count and Say/Solution.cs
+++ 0038. Count and Say/Solution.cs
@@ -1,5 +1,29 @@
 public class Solution {
     public string CountAndSay (int n) {
+        var res = "1";
+        if (n == 1) {
+            return res;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            res = CountNext (res);
+        }
+        return res;
+    }
 
+    public string CountNext (string current) {
+        var sb = new StringBuilder ();
+        var count = 1;
+        var say = current[0];
+        for (int i = 1; i < current.Length; i++) {
+            if (current[i] == say) {
+                count++;
+            } else {
+                sb.Append (count.ToString ()).Append (say);
+                say = current[i];
+                count = 1;
+            }
+        }
+        sb.Append (count.ToString ()).Append (say);
+        return sb.ToString ();
     }
 }
