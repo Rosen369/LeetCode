e2db4e31a51dab3bc429dc71ca63d50793815b7a
rosen369 <rosen369@outlook.com>
2018-07-18 16:33:59 +0800

    resolve first missing positive

--- 0041. First Missing Positive/Solution.cs
+++ 0041. First Missing Positive/Solution.cs
@@ -1,5 +1,19 @@
 public class Solution {
     public int FirstMissingPositive (int[] nums) {
-
+        var dic = new Dictionary<int, bool> ();
+        for (int i = 0; i < nums.Length; i++) {
+            dic.Add (i + 1, false);
+        }
+        for (int i = 0; i < nums.Length; i++) {
+            if (dic.ContainsKey (nums[i])) {
+                dic[nums[i]] = true;
+            }
+        }
+        for (int i = 0; i < nums.Length; i++) {
+            if (dic[i + 1] == false) {
+                return i + 1;
+            }
+        }
+        return nums.Length + 1;
     }
 }
