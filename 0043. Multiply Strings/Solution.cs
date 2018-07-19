fc709e4903011c9012ef4fd68ec9eb1b35d34f03
rosen369 <rosen369@outlook.com>
2018-07-19 17:19:11 +0800

    a more optimal solution

--- 0043. Multiply Strings/Solution.cs
+++ 0043. Multiply Strings/Solution.cs
@@ -3,69 +3,24 @@ public class Solution {
         if (num1 == "0" || num2 == "0") {
             return "0";
         }
-        var sum = new List<char> ();
-        for (int i = num2.Length - 1; i >= 0; i--) {
-            var multiply = MultiplyBit (num1, ToInt (num2[i]), num2.Length - 1 - i);
-            AddMultiplied (sum, multiply);
-        }
-        sum.Reverse ();
-        return new String (sum.ToArray ());
-    }
+        var bit_buffer = new int[num1.Length + num2.Length];
+        for (int i = num1.Length - 1; i >= 0; i--) {
+            for (int j = num2.Length - 1; j >= 0; j--) {
+                var mul = (num1[i] - '0') * (num2[j] - '0');
+                var pos = i + j;
+                var next_pos = i + j + 1;
+                var sum = mul + bit_buffer[next_pos];
 
-    public void AddMultiplied (List<char> sum, List<char> add) {
-        var sumLength = sum.Count;
-        var addLength = add.Count;
-        if (sumLength < addLength) {
-            for (int i = 0; i < addLength - sumLength; i++) {
-                sum.Add ('0');
+                bit_buffer[pos] += sum / 10;
+                bit_buffer[next_pos] = (sum) % 10;
             }
         }
-        if (addLength < sumLength) {
-            for (int i = 0; i < sumLength - addLength; i++) {
-                add.Add ('0');
+        var sb = new StringBuilder ();
+        for (var i = 0; i < bit_buffer.Length; i++) {
+            if (!(sb.Length == 0 && bit_buffer[i] == 0)) {
+                sb.Append (bit_buffer[i]);
             }
         }
-        var flow = 0;
-        for (int i = 0; i < sum.Count; i++) {
-            var total = ToInt (sum[i]) + ToInt (add[i]) + flow;
-            var strTotal = total.ToString ();
-            if (total >= 10) {
-                flow = 1;
-                sum[i] = strTotal[1];
-            } else {
-                flow = 0;
-                sum[i] = strTotal[0];
-            }
-        }
-        if (flow == 1) {
-            sum.Add ('1');
-        }
-    }
-
-    public List<char> MultiplyBit (string num, int bit, int digit) {
-        var res = new List<char> ();
-        for (int i = 0; i < digit; i++) {
-            res.Add ('0');
-        }
-        var flow = 0;
-        for (int i = num.Length - 1; i >= 0; i--) {
-            var multiply = ToInt (num[i]) * bit + flow;
-            var strMultiply = multiply.ToString ();
-            if (multiply >= 10) {
-                flow = ToInt (strMultiply[0]);
-                res.Add (strMultiply[1]);
-            } else {
-                flow = 0;
-                res.Add (strMultiply[0]);
-            }
-        }
-        if (flow != 0) {
-            res.Add (flow.ToString () [0]);
-        }
-        return res;
-    }
-
-    public int ToInt (char c) {
-        return (int) (c - '0');
+        return sb.ToString ();
     }
 }
