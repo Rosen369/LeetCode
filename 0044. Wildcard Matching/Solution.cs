7c328ac94e5e2255b9967fbc5fe6d403e34814a3
rosen369 <rosen369@outlook.com>
2018-07-20 14:56:51 +0800

    Time Limit Exceeded

--- 0044. Wildcard Matching/Solution.cs
+++ 0044. Wildcard Matching/Solution.cs
@@ -1,5 +1,43 @@
 public class Solution {
     public bool IsMatch (string s, string p) {
-
+        if (string.IsNullOrEmpty (p) && string.IsNullOrEmpty (s)) {
+            return true;
+        }
+        if (string.IsNullOrEmpty (p)) {
+            return false;
+        }
+        if (string.IsNullOrEmpty (s)) {
+            for (int i = 0; i < p.Length; i++) {
+                if (p[i] != '*') {
+                    return false;
+                }
+            }
+            return true;
+        }
+        if (p[0] == '?') {
+            return IsMatch (s.Substring (1), p.Substring (1));
+        }
+        if (p[0] == '*') {
+            if (p.Length == 1) {
+                return true;
+            }
+            //compress '*' for speed up
+            for (int i = 1; i < p.Length; i++) {
+                if (p[i] != '*') {
+                    p = p.Substring (i - 1);
+                    break;
+                }
+            }
+            for (int i = 0; i < s.Length; i++) {
+                if (IsMatch (s.Substring (i), p.Substring (1))) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        if (p[0] == s[0]) {
+            return IsMatch (s.Substring (1), p.Substring (1));
+        }
+        return false;
     }
 }
