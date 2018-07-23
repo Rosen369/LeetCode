f336721d47094c29cab6f7d8c0fc16ad681b7ebc
rosen369 <rosen369@outlook.com>
2018-07-23 14:45:31 +0800

    Time Limit Exceeded

--- 0045. Jump Game II/Solution.cs
+++ 0045. Jump Game II/Solution.cs
@@ -1,5 +1,23 @@
 public class Solution {
     public int Jump (int[] nums) {
+        if (nums.Length == 1) {
+            return 0;
+        }
+        var res = nums.Length;
+        JumpNext (nums, 0, ref res, 0);
+        return res;
+    }
 
+    public void JumpNext (int[] nums, int index, ref int res, int steps) {
+        if (res <= steps + 1) {
+            return;
+        }
+        if (nums.Length - 1 - index <= nums[index]) {
+            res = steps + 1;
+            return;
+        }
+        for (int i = 1; i <= nums[index]; i++) {
+            JumpNext (nums, index + i, ref res, steps + 1);
+        }
     }
 }
