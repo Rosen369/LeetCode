036be91b84206cda0b2f0f1b535e72dcaee39a53
rosen369 <rosen369@outlook.com>
2018-07-24 14:36:20 +0800

    optimize runtime

--- 0047. Permutations II/Solution.cs
+++ 0047. Permutations II/Solution.cs
@@ -1,34 +1,28 @@
 public class Solution {
     public IList<IList<int>> PermuteUnique (int[] nums) {
-        var res = new Dictionary<string, IList<int>> ();
+        var res = new List<IList<int>> ();
+        Array.Sort (nums);
         AddNext (nums, res, new List<int> (), new List<int> ());
-        return res.Values.ToList ();
+        return res;
     }
 
-    public void AddNext (int[] nums, IDictionary<string, IList<int>> res, IList<int> lastList, IList<int> usedIndex) {
+    public void AddNext (int[] nums, IList<IList<int>> res, IList<int> lastList, IList<int> usedIndex) {
         if (lastList.Count () == nums.Length) {
-            var key = GetKey (lastList);
-            if (!res.ContainsKey (key)) {
-                res.Add (key, lastList);
-            }
+            res.Add (lastList);
             return;
         }
         for (int i = 0; i < nums.Length; i++) {
-            if (!usedIndex.Contains (i)) {
-                var currList = new List<int> (lastList);
-                currList.Add (nums[i]);
-                var currIndex = new List<int> (usedIndex);
-                currIndex.Add (i);
-                AddNext (nums, res, currList, currIndex);
+            if (i > 0 && nums[i - 1] == nums[i] && usedIndex.Contains (i - 1)) {
+                continue;
             }
+            if (usedIndex.Contains (i)) {
+                continue;
+            }
+            var currList = new List<int> (lastList);
+            currList.Add (nums[i]);
+            var currIndex = new List<int> (usedIndex);
+            currIndex.Add (i);
+            AddNext (nums, res, currList, currIndex);
         }
-    }
-
-    public string GetKey (IList<int> lastList) {
-        var res = string.Empty;
-        for (int i = 0; i < lastList.Count (); i++) {
-            res += lastList[i] + '_';
-        }
-        return res;
     }
 }
