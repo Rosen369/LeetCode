51cd2e23b10d8105ea20da85b8c0a8fc71390076
rosen369 <rosen369@outlook.com>
2018-07-24 13:56:57 +0800

    resolve permutations

--- 0046. Permutations/Solution.cs
+++ 0046. Permutations/Solution.cs
@@ -1,5 +1,21 @@
 public class Solution {
     public IList<IList<int>> Permute (int[] nums) {
+        var res = new List<IList<int>> ();
+        AddNext (nums, res, new List<int> ());
+        return res;
+    }
 
+    public void AddNext (int[] nums, IList<IList<int>> res, IList<int> lastList) {
+        if (lastList.Count () == nums.Length) {
+            res.Add (lastList);
+            return;
+        }
+        for (int i = 0; i < nums.Length; i++) {
+            if (!lastList.Contains (nums[i])) {
+                var currList = new List<int> (lastList);
+                currList.Add (nums[i]);
+                AddNext (nums, res, currList);
+            }
+        }
     }
 }
