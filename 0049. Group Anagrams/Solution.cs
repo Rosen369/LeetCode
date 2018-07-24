29c5b5b41c220c6b9f7c3030b0d6c6c289bd4bbe
rosen369 <rosen369@outlook.com>
2018-07-24 16:48:26 +0800

    resolve group anagrams

--- 0049. Group Anagrams/Solution.cs
+++ 0049. Group Anagrams/Solution.cs
@@ -1,5 +1,15 @@
 public class Solution {
     public IList<IList<string>> GroupAnagrams (string[] strs) {
-
+        var res = new Dictionary<string, IList<string>> ();
+        for (int i = 0; i < strs.Length; i++) {
+            var arr = strs[i].ToCharArray ();
+            Array.Sort (arr);
+            var key = new string (arr);
+            if (!res.ContainsKey (key)) {
+                res.Add (key, new List<string> ());
+            }
+            res[key].Add (strs[i]);
+        }
+        return res.Values.ToList ();
     }
 }
