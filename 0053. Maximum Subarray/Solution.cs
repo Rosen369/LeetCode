6763da145c6a2cb85d41fca17ce6ec3aa9bdd5ab
rosen369 <rosen369@outlook.com>
2018-07-26 16:34:38 +0800

    resolved by dynamic programming

--- 0053. Maximum Subarray/Solution.cs
+++ 0053. Maximum Subarray/Solution.cs
@@ -1,5 +1,16 @@
 public class Solution {
     public int MaxSubArray (int[] nums) {
-
+        var dp = new int[nums.Length];
+        dp[0] = nums[0];
+        var max = dp[0];
+        for (int i = 1; i < nums.Length; i++) {
+            if (dp[i - 1] > 0) {
+                dp[i] = dp[i - 1] + nums[i];
+            } else {
+                dp[i] = nums[i];
+            }
+            max = Math.Max (max, dp[i]);
+        }
+        return max;
     }
 }
