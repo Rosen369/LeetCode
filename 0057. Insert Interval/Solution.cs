b590dd1833b391a086eecc7e6dccd812fc63c785
rosen369 <rosen369@outlook.com>
2018-07-27 16:01:04 +0800

    add insert interval

---
+++ 0057. Insert Interval/Solution.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public IList<Interval> Insert (IList<Interval> intervals, Interval newInterval) {
+
+    }
+}
+
+//Definition for an interval.
+public class Interval {
+    public int start;
+    public int end;
+    public Interval () { start = 0; end = 0; }
+    public Interval (int s, int e) { start = s; end = e; }
+}
