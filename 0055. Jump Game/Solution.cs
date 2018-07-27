113771978adaa533de6fd880d074bb67b5578426
rosen369 <rosen369@outlook.com>
2018-07-27 15:25:52 +0800

    resolved by greedy

--- 0055. Jump Game/Solution.cs
+++ 0055. Jump Game/Solution.cs
@@ -1,30 +1,11 @@
 public class Solution {
     public bool CanJump (int[] nums) {
-        if (nums.Length <= 1) {
-            return true;
-        }
-        var blocked = new bool[nums.Length];
-        for (int i = 0; i < nums.Length; i++) {
-            blocked[i] = false;
-        }
-        var res = JumpNext (0, nums, blocked);
-        return res;
-    }
-
-    public bool JumpNext (int index, int[] nums, bool[] blocked) {
-        for (int i = nums[index]; i > 0; i--) {
-            if (i + index >= nums.Length - 1) {
-                return true;
-            }
-            if (blocked[i + index] == true) {
-                continue;
-            }
-            var res = JumpNext (i + index, nums, blocked);
-            if (res == true) {
-                return true;
+        int lastPos = nums.Length - 1;
+        for (int i = nums.Length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
             }
         }
-        blocked[index] = true;
-        return false;
+        return lastPos == 0;
     }
 }
