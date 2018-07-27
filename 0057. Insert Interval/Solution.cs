3982cea1bb104202ac2ec2c797640a5f13c3320f
rosen369 <rosen369@outlook.com>
2018-07-27 16:34:36 +0800

    resolve insert interval

--- 0057. Insert Interval/Solution.cs
+++ 0057. Insert Interval/Solution.cs
@@ -1,6 +1,31 @@
 public class Solution {
     public IList<Interval> Insert (IList<Interval> intervals, Interval newInterval) {
-
+        var res = new List<Interval> ();
+        if (intervals.Count == 0) {
+            res.Add (newInterval);
+            return res;
+        }
+        intervals = intervals.OrderBy (e => e.start).ToList ();
+        for (int i = 0; i < intervals.Count (); i++) {
+            var curr = intervals[i];
+            if (curr.start > newInterval.end) {
+                res.Add (newInterval);
+                for (int j = i; j < intervals.Count (); j++) {
+                    res.Add (intervals[j]);
+                }
+                break;
+            } else {
+                if (curr.end >= newInterval.start) {
+                    newInterval = new Interval (Math.Min (curr.start, newInterval.start), Math.Max (curr.end, newInterval.end));
+                } else {
+                    res.Add (curr);
+                }
+                if (i == intervals.Count () - 1) {
+                    res.Add (newInterval);
+                }
+            }
+        }
+        return res;
     }
 }
 
