161b2a1bdf165fb901890041447d26a74ae6d53f
rosen369 <rosen369@outlook.com>
2018-07-27 16:42:40 +0800

    resolve length of last word

--- 0058. Length of Last Word/Solution.cs
+++ 0058. Length of Last Word/Solution.cs
@@ -1,5 +1,7 @@
 public class Solution {
     public int LengthOfLastWord (string s) {
-
+        var array = s.Trim ().Split (' ');
+        var last = array[array.Length - 1];
+        return last.Length;
     }
 }
