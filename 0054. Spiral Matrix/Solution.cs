60c8c578e37eedd6d5eb582f7d88a9cc43f195ea
rosen369 <rosen369@outlook.com>
2018-07-27 09:15:08 +0800

    optimize runtime

--- 0054. Spiral Matrix/Solution.cs
+++ 0054. Spiral Matrix/Solution.cs
@@ -5,58 +5,31 @@ public class Solution {
             return res;
         }
         var top = 0;
-        var right = matrix.GetLength (1);
-        var bottom = matrix.GetLength (0);
+        var right = matrix.GetLength (1) - 1;
+        var bottom = matrix.GetLength (0) - 1;
         var left = 0;
-        MoveRight (0, 0, top, right, bottom, left, matrix, res);
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                res.Add (matrix[top, i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                res.Add (matrix[i, right]);
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    res.Add (matrix[bottom, i]);
+                }
+            }
+            bottom--;
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    res.Add (matrix[i, left]);
+                }
+            }
+            left++;
+        }
         return res;
-    }
-
-    public void MoveRight (int x, int y, int top, int right, int bottom, int left, int[, ] matrix, IList<int> res) {
-        while (x < right) {
-            res.Add (matrix[y, x]);
-            x++;
-        }
-        if (y + 1 == bottom) {
-            return;
-        } else {
-            MoveDown (x - 1, y + 1, top + 1, right, bottom, left, matrix, res);
-        }
-    }
-
-    public void MoveDown (int x, int y, int top, int right, int bottom, int left, int[, ] matrix, IList<int> res) {
-        while (y < bottom) {
-            res.Add (matrix[y, x]);
-            y++;
-        }
-        if (x == left) {
-            return;
-        } else {
-            MoveLeft (x - 1, y - 1, top, right - 1, bottom, left, matrix, res);
-        }
-    }
-
-    public void MoveLeft (int x, int y, int top, int right, int bottom, int left, int[, ] matrix, IList<int> res) {
-        while (x >= left) {
-            res.Add (matrix[y, x]);
-            x--;
-        }
-        if (y == top) {
-            return;
-        } else {
-            MoveUp (x + 1, y - 1, top, right, bottom - 1, left, matrix, res);
-        }
-    }
-
-    public void MoveUp (int x, int y, int top, int right, int bottom, int left, int[, ] matrix, IList<int> res) {
-        while (y >= top) {
-            res.Add (matrix[y, x]);
-            y--;
-        }
-        if (x + 1 == right) {
-            return;
-        } else {
-            MoveRight (x + 1, y + 1, top, right, bottom, left + 1, matrix, res);
-        }
     }
 }
