08f58ca5aa6f47345bdb1e8f87c0b0913e70ee57
rosen369 <rosen369@outlook.com>
2018-07-30 15:14:57 +0800

    Time Limit Exceeded

--- 0060. Permutation Sequence/Solution.cs
+++ 0060. Permutation Sequence/Solution.cs
@@ -1,5 +1,24 @@
 public class Solution {
     public string GetPermutation (int n, int k) {
+        var res = new List<string> ();
+        CombineNext (n, new List<char> (), res);
+        return res[k - 1];
+    }
 
+    public void CombineNext (int n, IList<char> curr, IList<string> list) {
+        if (curr.Count () == n) {
+            var str = new string (curr.ToArray ());
+            if (!list.Contains (str)) {
+                list.Add (str);
+            }
+            return;
+        }
+        for (char i = '1'; i <= n.ToString () [0]; i++) {
+            if (!curr.Contains (i)) {
+                curr.Add (i);
+                CombineNext (n, curr, list);
+                curr.Remove (i);
+            }
+        }
     }
 }
