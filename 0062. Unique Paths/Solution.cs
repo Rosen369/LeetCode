2bf86bb35cd0ae3cc0fedd910175398a5df4e416
rosen369 <rosen369@outlook.com>
2018-07-30 17:30:17 +0800

    format code

--- 0062. Unique Paths/Solution.cs
+++ 0062. Unique Paths/Solution.cs
@@ -1,14 +1,15 @@
 public class Solution {
     public int UniquePaths (int m, int n) {
-        var grid = new int[m, n];
+        var dp = new int[m, n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0)
-                    grid[i, j] = 1;
-                else
-                    grid[i, j] = grid[i, j - 1] + grid[i - 1, j];
+                if (i == 0 || j == 0) {
+                    dp[i, j] = 1;
+                } else {
+                    dp[i, j] = dp[i, j - 1] + dp[i - 1, j];
+                }
             }
         }
-        return grid[m - 1, n - 1];
+        return dp[m - 1, n - 1];
     }
 }
