797ddd8d63bac8278dbdad1ec18b5579ad64bf6e
rosen369 <rosen369@outlook.com>
2018-07-30 16:22:14 +0800

    resolve permutation sequence

--- 0060. Permutation Sequence/Solution.cs
+++ 0060. Permutation Sequence/Solution.cs
@@ -1,24 +1,24 @@
 public class Solution {
     public string GetPermutation (int n, int k) {
-        var res = new List<string> ();
-        CombineNext (n, new List<char> (), res);
-        return res[k - 1];
-    }
-
-    public void CombineNext (int n, IList<char> curr, IList<string> list) {
-        if (curr.Count () == n) {
-            var str = new string (curr.ToArray ());
-            if (!list.Contains (str)) {
-                list.Add (str);
-            }
-            return;
-        }
+        var res = string.Empty;
+        var factorial = 1;
+        var charList = new List<char> ();
+        // for (char i = n.ToString () [0]; i >= '1'; i--) {
         for (char i = '1'; i <= n.ToString () [0]; i++) {
-            if (!curr.Contains (i)) {
-                curr.Add (i);
-                CombineNext (n, curr, list);
-                curr.Remove (i);
-            }
+            charList.Add (i);
         }
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+        k--;
+        while (n > 0) {
+            var pos = k / (factorial / n);
+            factorial = factorial / n;
+            k = k - factorial * pos;
+            n = n - 1;
+            res += charList[pos];
+            charList.Remove (charList[pos]);
+        }
+        return res;
     }
 }
