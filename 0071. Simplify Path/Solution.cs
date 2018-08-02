b1f88598b7882da9d82e84b7443847dd4dfd7411
rosen369 <rosen369@outlook.com>
2018-08-02 15:29:56 +0800

    resolved by stack

--- 0071. Simplify Path/Solution.cs
+++ 0071. Simplify Path/Solution.cs
@@ -1,5 +1,22 @@
 public class Solution {
     public string SimplifyPath (string path) {
-
+        var pathArr = path.Split ('/');
+        var stack = new Stack<string> ();
+        for (int i = 0; i < pathArr.Length; i++) {
+            var str = pathArr[i];
+            if (string.IsNullOrEmpty (str)) {
+                continue;
+            } else if (str == ".") {
+                continue;
+            } else if (str == "..") {
+                if (stack.Count != 0) {
+                    stack.Pop ();
+                }
+            } else {
+                stack.Push (str);
+            }
+        }
+        var res = string.Join ("/", stack.ToArray ().Reverse());
+        return "/" + res;
     }
 }
