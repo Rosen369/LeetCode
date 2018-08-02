e3047d754cd06107a0536cd97429f3326f7722ce
rosen369 <rosen369@outlook.com>
2018-08-02 14:22:53 +0800

    resolved by binary search

--- 0069. Sqrt(x)/Solution.cs
+++ 0069. Sqrt(x)/Solution.cs
@@ -1,5 +1,22 @@
 public class Solution {
     public int MySqrt (int x) {
-
+        if (x == 1) {
+            return 1;
+        }
+        long left = 1;
+        long right = x;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long sqrt = mid * mid;
+            if (sqrt == x) {
+                return (int) mid;
+            }
+            if (sqrt < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return (int) left - 1;
     }
 }
