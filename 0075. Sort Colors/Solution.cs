2fc0e42fa153fb43d0a101196ea8f9bfa4e427cc
rosen369 <rosen369@outlook.com>
2018-08-08 16:00:15 +0800

    resolve sort colors

--- 0075. Sort Colors/Solution.cs
+++ 0075. Sort Colors/Solution.cs
@@ -1,5 +1,19 @@
 public class Solution {
     public void SortColors (int[] nums) {
-
+        var n0 = -1;
+        var n1 = -1;
+        var n2 = -1;
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] == 0) {
+                nums[++n2] = 2;
+                nums[++n1] = 1;
+                nums[++n0] = 0;
+            } else if (nums[i] == 1) {
+                nums[++n2] = 2;
+                nums[++n1] = 1;
+            } else if (nums[i] == 2) {
+                nums[++n2] = 2;
+            }
+        }
     }
 }
