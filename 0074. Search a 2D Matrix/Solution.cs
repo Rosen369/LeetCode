dbeb67b4845ed0de68c905a596d5d9eef349cc4d
rosen369 <rosen369@outlook.com>
2018-08-08 14:44:04 +0800

    resolved by binary search

--- 0074. Search a 2D Matrix/Solution.cs
+++ 0074. Search a 2D Matrix/Solution.cs
@@ -1,5 +1,21 @@
 public class Solution {
     public bool SearchMatrix (int[, ] matrix, int target) {
-
+        if (matrix.Length == 0) {
+            return false;
+        }
+        var m = matrix.GetLength (0);
+        var n = matrix.GetLength (1);
+        var left = 0;
+        var right = matrix.Length - 1;
+        while (left < right) {
+            var mid = (left + right) / 2;
+            var curr = matrix[mid / n, mid % n];
+            if (curr < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return matrix[right / n, right % n] == target;
     }
 }
