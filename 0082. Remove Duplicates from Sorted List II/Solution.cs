8b28acfd28e7cdba26f2502282968c2ebd67f8b1
rosen369 <rosen369@outlook.com>
2018-08-13 13:59:52 +0800

    resolved using dictionary

--- 0082. Remove Duplicates from Sorted List II/Solution.cs
+++ 0082. Remove Duplicates from Sorted List II/Solution.cs
@@ -1,33 +1,21 @@
 public class Solution {
     public ListNode DeleteDuplicates (ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+        var dic = new Dictionary<int, int> ();
+        while (head != null) {
+            if (dic.ContainsKey (head.val)) {
+                dic[head.val] = dic[head.val] + 1;
+            } else {
+                dic.Add (head.val, 1);
+            }
+            head = head.next;
         }
-        var res = new ListNode (-1);
-        var cache = res;
-        var mark = head;
-        var curr = head.next;
-        if (mark.val != curr.val) {
-            cache.next = new ListNode (mark.val);
-            cache = cache.next;
-        }
-        while (curr != null) {
-            if (curr.val == mark.val) {
+        var res = new ListNode (0);
+        curr = res;
+        foreach (var pair in dic) {
+            if (pair.Value == 1) {
+                curr.next = new ListNode (pair.Key);
                 curr = curr.next;
-                continue;
             }
-            if (curr.next == null) {
-                cache.next = curr;
-                break;
-            }
-            if (curr.val == curr.next.val) {
-                mark = curr;
-                curr = curr.next;
-                continue;
-            }
-            cache.next = new ListNode (curr.val);
-            cache = cache.next;
-            curr = curr.next;
         }
         return res.next;
     }
