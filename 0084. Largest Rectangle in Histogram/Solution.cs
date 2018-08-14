12a2925a8ce3657abb42e9b15e6a8c5d3a49cda6
rosen369 <rosen369@outlook.com>
2018-08-14 14:38:35 +0800

    time limit exceeded

--- 0084. Largest Rectangle in Histogram/Solution.cs
+++ 0084. Largest Rectangle in Histogram/Solution.cs
@@ -1,5 +1,30 @@
 public class Solution {
     public int LargestRectangleArea (int[] heights) {
-
+        var list = new List<int> (heights);
+        list.Add (0);
+        heights = list.ToArray ();
+        var largest = 0;
+        var min = int.MaxValue;
+        var index = 0;
+        var length = 0;
+        for (int i = 0; i < heights.Length; i++) {
+            for (int j = 0; j < heights.Length; j++) {
+                if (heights[j] == 0) {
+                    largest = Math.Max (largest, min * length);
+                    length = 0;
+                    min = int.MaxValue;
+                    continue;
+                }
+                length++;
+                min = Math.Min (min, heights[j]);
+                if (heights[j] != 0) {
+                    if (heights[index] == 0 || heights[j] < heights[index]) {
+                        index = j;
+                    }
+                }
+            }
+            heights[index] = 0;
+        }
+        return largest;
     }
 }
