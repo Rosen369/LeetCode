cee84f160aa122768aba42261b229af72859e334
rosen369 <rosen369@outlook.com>
2018-08-17 14:27:19 +0800

    resolve partition list

--- 0086. Partition List/Solution.cs
+++ 0086. Partition List/Solution.cs
@@ -1,6 +1,21 @@
 public class Solution {
     public ListNode Partition (ListNode head, int x) {
-
+        var less = new ListNode (-1);
+        var currLess = less;
+        var greater = new ListNode (-1);
+        var currGreater = greater;
+        while (head != null) {
+            if (head.val < x) {
+                currLess.next = new ListNode (head.val);
+                currLess = currLess.next;
+            } else {
+                currGreater.next = new ListNode (head.val);
+                currGreater = currGreater.next;
+            }
+            head = head.next;
+        }
+        currLess.next = greater.next;
+        return less.next;
     }
 }
 
