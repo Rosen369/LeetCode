f985543c42e460b21aa09e708aaf5b98d2cb4afe
rosen369 <rosen369@outlook.com>
2018-08-17 14:07:00 +0800

    add partition list

---
+++ 0086. Partition List/Solution.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public ListNode Partition (ListNode head, int x) {
+
+    }
+}
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode (int x) { val = x; }
+}
