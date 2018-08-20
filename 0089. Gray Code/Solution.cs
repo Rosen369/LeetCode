9d08f9fd1005ac593407f7ee937e4bb6edee1067
rosen369 <rosen369@outlook.com>
2018-08-20 13:46:06 +0800

    resolve gray code

--- 0089. Gray Code/Solution.cs
+++ 0089. Gray Code/Solution.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public IList<int> GrayCode (int n) {
-
+        var res = new List<int> ();
+        if (n == 0) {
+            res.Add (0);
+            return res;
+        }
+        res.Add (0);
+        res.Add (1);
+        int val = 2;
+        for (int i = 1; i < n; i++) {
+            int length = res.Count;
+            for (int j = length - 1; j >= 0; j--) {
+                res.Add (val + res[j]);
+            }
+            val *= 2;
+        }
+        return res;
     }
 }
