0c5de7d2e3a8ee80c020789c76616b8a61ec8db9
rosen369 <rosen369@outlook.com>
2018-08-20 16:27:19 +0800

    resolve reverse linked list II

---
+++ 0092. Reverse Linked List II/README.md
@@ -0,0 +1,12 @@
+# Reverse Linked List II
+
+Reverse a linked list from position _m_ to _n_. Do it in one-pass.
+
+__Note:__ 1 ≤ _m_ ≤ _n_ ≤ length of list.
+
+__Example:__
+
+```
+Input: 1->2->3->4->5->NULL, m = 2, n = 4
+Output: 1->4->3->2->5->NULL
+```
---
+++ 0092. Reverse Linked List II/Solution.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public ListNode ReverseBetween (ListNode head, int m, int n) {
+        var res = new ListNode (-1);
+        var curr = res;
+        var list = new List<int> ();
+        while (head != null) {
+            list.Add (head.val);
+            head = head.next;
+        }
+        for (int i = 0; i < list.Count (); i++) {
+            if (i == m - 1) {
+                i = n - 1;
+                for (; i >= m - 1; i--) {
+                    curr.next = new ListNode (list[i]);
+                    curr = curr.next;
+                }
+                i = n - 1;
+            } else {
+                curr.next = new ListNode (list[i]);
+                curr = curr.next;
+            }
+        }
+        return res.next;
+    }
+}
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode (int x) { val = x; }
+}
