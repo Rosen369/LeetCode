3deab3c4c99fb6677f63ef740d627ed97b80a534
rosen369 <rosen369@outlook.com>
2018-08-20 16:04:19 +0800

    resolve decode ways

--- 0091. Decode Ways/Solution.cs
+++ 0091. Decode Ways/Solution.cs
@@ -1,5 +1,21 @@
 public class Solution {
     public int NumDecodings (string s) {
-
+        if (string.IsNullOrEmpty (s) || s[0] == '0') {
+            return 0;
+        }
+        var dp = new int[s.Length + 1];
+        dp[0] = 1;
+        dp[1] = s[0] != '0' ? 1 : 0;
+        for (int i = 2; i <= s.Length; i++) {
+            var first = Convert.ToInt32 (s.Substring (i - 1, 1));
+            var second = Convert.ToInt32 (s.Substring (i - 2, 2));
+            if (first >= 1 && first <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[s.Length];
     }
 }
