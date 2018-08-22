446dd2290e8bbef05ec36e3f1c14287bef857efb
rosen369 <rosen369@outlook.com>
2018-08-22 14:44:11 +0800

    add iteration solution

--- 0094. Binary Tree Inorder Traversal/Solution.cs
+++ 0094. Binary Tree Inorder Traversal/Solution.cs
@@ -17,6 +17,22 @@ public class Solution {
             Inorder (root.right, list);
         }
     }
+
+    public IList<int> Iterating (TreeNode root) {
+        var res = new List<int> ();
+        var stack = new Stack<TreeNode> ();
+        var curr = root;
+        while (curr != null || stack.Count () != 0) {
+            while (curr != null) {
+                stack.Push (curr);
+                curr = curr.left;
+            }
+            curr = stack.Pop ();
+            res.Add (curr.val);
+            curr = curr.right;
+        }
+        return res;
+    }
 }
 
 public class TreeNode {
@@ -25,3 +41,12 @@ public class TreeNode {
     public TreeNode right;
     public TreeNode (int x) { val = x; }
 }
+
+/*
+In-order (LNR)
+	1.Check if the current node is empty or null.
+	2.Traverse the left subtree by recursively calling the in-order function.
+	3.Display the data part of the root (or current node).
+	4.Traverse the right subtree by recursively calling the in-order function.
+In a binary search tree, in-order traversal retrieves data in sorted order.
+ */
