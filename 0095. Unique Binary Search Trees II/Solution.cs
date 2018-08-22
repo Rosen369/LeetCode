a6c30d9bd65fa215917710e9a1aaf2a9652eb940
rosen369 <rosen369@outlook.com>
2018-08-22 16:30:09 +0800

    resolved using divide and conquer

---
+++ 0095. Unique Binary Search Trees II/Solution.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    public IList<TreeNode> GenerateTrees (int n) {
+        if (n == 0) {
+            return new List<TreeNode> ();
+        }
+        return GenerateSubTrees (1, n);
+    }
+
+    public IList<TreeNode> GenerateSubTrees (int start, int end) {
+        var res = new List<TreeNode> ();
+        if (start > end) {
+            res.Add (null);
+            return res;
+        }
+        for (int i = start; i <= end; i++) {
+            var leftTrees = GenerateSubTrees (start, i - 1);
+            var rightTrees = GenerateSubTrees (i + 1, end);
+            foreach (var leftTree in leftTrees) {
+                foreach (var rightTree in rightTrees) {
+                    var root = new TreeNode (i);
+                    root.left = leftTree;
+                    root.right = rightTree;
+                    res.Add (root);
+                }
+            }
+        }
+        return res;
+    }
+
+    // public TreeNode CloneTree (TreeNode node) {
+    //     if (node == null) {
+    //         return null;
+    //     }
+    //     var clone = new TreeNode (node.val);
+    //     clone.left = CloneTree (node.left);
+    //     clone.right = CloneTree (node.right);
+    //     return clone;
+    // }
+}
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode (int x) { val = x; }
+}
