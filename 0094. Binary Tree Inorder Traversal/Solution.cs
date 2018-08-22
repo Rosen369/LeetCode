29631b1735e382176388917955366f60e096d90b
rosen369 <rosen369@outlook.com>
2018-08-22 09:40:19 +0800

    resolve by recursive

--- 0094. Binary Tree Inorder Traversal/Solution.cs
+++ 0094. Binary Tree Inorder Traversal/Solution.cs
@@ -1,6 +1,21 @@
 public class Solution {
     public IList<int> InorderTraversal (TreeNode root) {
+        var res = new List<int> ();
+        if (root == null) {
+            return res;
+        }
+        Inorder (root, res);
+        return res;
+    }
 
+    public void Inorder (TreeNode root, IList<int> list) {
+        if (root.left != null) {
+            Inorder (root.left, list);
+        }
+        list.Add (root.val);
+        if (root.right != null) {
+            Inorder (root.right, list);
+        }
     }
 }
 
