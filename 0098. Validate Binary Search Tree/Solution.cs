800fcc6f66a7c78e070b8d1a6a0fb42f5df36715
rosen369 <rosen369@outlook.com>
2018-08-23 16:33:02 +0800

    resolved using inorder traversal

---
+++ 0098. Validate Binary Search Tree/Solution.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public bool IsValidBST (TreeNode root) {
+        var list = new List<int> ();
+        Inorder (root, list);
+        for (int i = 1; i < list.Count (); i++) {
+            if (list[i] <= list[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void Inorder (TreeNode root, IList<int> res) {
+        if (root == null) {
+            return;
+        }
+        if (root.left != null) {
+            Inorder (root.left, res);
+        }
+        res.Add (root.val);
+        if (root.right != null) {
+            Inorder (root.right, res);
+        }
+    }
+}
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode (int x) { val = x; }
+}
