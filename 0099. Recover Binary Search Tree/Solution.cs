8bf022172f7a1d15ebe5d22937c00100444237bf
rosen369 <rosen369@outlook.com>
2018-08-24 10:48:25 +0800

    resolved using inorder traversal

--- 0099. Recover Binary Search Tree/Solution.cs
+++ 0099. Recover Binary Search Tree/Solution.cs
@@ -1,6 +1,33 @@
 public class Solution {
     public void RecoverTree (TreeNode root) {
+        var list = new List<TreeNode> ();
+        Inorder (root, list);
+        var start = 0;
+        for (int i = 0; i < list.Count () - 1; i++) {
+            if (list[i].val > list[i + 1].val) {
+                start = i;
+                break;
+            }
+        }
+        var end = list.Count () - 1;
+        for (int i = start + 1; i < list.Count (); i++) {
+            if (list[start].val < list[i].val) {
+                end = i - 1;
+                break;
+            }
+        }
+        var temp = list[start].val;
+        list[start].val = list[end].val;
+        list[end].val = temp;
+    }
 
+    public void Inorder (TreeNode root, IList<TreeNode> list) {
+        if (root == null) {
+            return;
+        }
+        Inorder (root.left, list);
+        list.Add (root);
+        Inorder (root.right, list);
     }
 }
 
