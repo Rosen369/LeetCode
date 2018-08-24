4e069e1ba3c0979b8098f19868346f908e687cef
rosen369 <rosen369@outlook.com>
2018-08-24 16:42:34 +0800

    add construct binary tree from preorder and inorder traversal

---
+++ 0105. Construct Binary Tree from Preorder and Inorder Traversal/Solution.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public TreeNode BuildTree (int[] preorder, int[] inorder) {
+
+    }
+}
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode (int x) { val = x; }
+}
