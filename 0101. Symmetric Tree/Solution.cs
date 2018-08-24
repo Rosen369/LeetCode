131504b212f01f5a9e0e18a781b56fa05d89bb9d
rosen369 <rosen369@outlook.com>
2018-08-24 14:33:39 +0800

    resolved using recursion

--- 0101. Symmetric Tree/Solution.cs
+++ 0101. Symmetric Tree/Solution.cs
@@ -1,6 +1,25 @@
 public class Solution {
     public bool IsSymmetric (TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Symmetric (root.left, root.right);
+    }
 
+    public bool Symmetric (TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left != null && right == null) {
+            return false;
+        }
+        if (left == null & right != null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return Symmetric (left.left, right.right) && Symmetric (left.right, right.left);
     }
 }
 
