4acddbc196be329b611264282806aefc3d270575
rosen369 <rosen369@outlook.com>
2018-08-27 15:08:25 +0800

    BFS solution with two list

---
+++ 0107. Binary Tree Level Order Traversal II/Solution.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public IList<IList<int>> LevelOrderBottom (TreeNode root) {
+        var res = new List<IList<int>> ();
+        var curr = new List<TreeNode> ();
+        var next = new List<TreeNode> ();
+        curr.Add (root);
+        while (curr.Count () != 0) {
+            next.Clear ();
+            var list = new List<int> ();
+            foreach (var node in curr) {
+                if (node != null) {
+                    list.Add (node.val);
+                    next.Add (node.left);
+                    next.Add (node.right);
+                }
+            }
+            if (list.Count () != 0) {
+                res.Add (list);
+            }
+            curr.Clear ();
+            curr.AddRange (next);
+        }
+        res.Reverse ();
+        return res;
+    }
+}
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode (int x) { val = x; }
+}
