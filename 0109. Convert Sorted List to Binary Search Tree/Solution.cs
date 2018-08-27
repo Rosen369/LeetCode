3379c2aa8543615366cf2524e5e5fbab5c1a50a8
rosen369 <rosen369@outlook.com>
2018-08-27 15:26:49 +0800

    recursive solution

---
+++ 0109. Convert Sorted List to Binary Search Tree/Solution.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public TreeNode SortedListToBST (ListNode head) {
+        var numList = new List<int> ();
+        while (head != null) {
+            numList.Add (head.val);
+            head = head.next;
+        }
+        return Build (numList.ToArray (), 0, numList.Count () - 1);
+    }
+
+    public TreeNode Build (int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        var mid = (start + end) / 2;
+        var node = new TreeNode (nums[mid]);
+        node.left = Build (nums, start, mid - 1);
+        node.right = Build (nums, mid + 1, end);
+        return node;
+    }
+}
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode (int x) { val = x; }
+}
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode (int x) { val = x; }
+}
