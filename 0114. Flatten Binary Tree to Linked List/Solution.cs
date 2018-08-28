d299d87e585834c717093adeb9341f3ffb2a0d91
rosen369 <rosen369@outlook.com>
2018-08-28 14:07:02 +0800

    add flatten binary tree to linked list

---
+++ 0114. Flatten Binary Tree to Linked List/Solution.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public void Flatten (TreeNode root) {
+
+    }
+}
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode (int x) { val = x; }
+}
