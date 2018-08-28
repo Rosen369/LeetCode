944d772c7f2b0f87eecbdaa024d683ad82f3e13e
rosen369 <rosen369@outlook.com>
2018-08-28 11:37:44 +0800

    recursive solution

---
+++ 0111. Minimum Depth of Binary Tree/Solution.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int MinDepth (TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        var left = MinDepth (root.left);
+        var right = MinDepth (root.right);
+        if (left == 0) {
+            return right + 1;
+        }
+        if (right == 0) {
+            return left + 1;
+        }
+        return Math.Min (left, right) + 1;
+    }
+}
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode (int x) { val = x; }
+}
