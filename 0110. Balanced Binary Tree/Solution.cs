511b4f85a70906d9606e6f500975af04c2b25541
rosen369 <rosen369@outlook.com>
2018-08-28 11:07:24 +0800

    recursive solution

--- 0110. Balanced Binary Tree/Solution.cs
+++ 0110. Balanced Binary Tree/Solution.cs
@@ -1,6 +1,27 @@
 public class Solution {
     public bool IsBalanced (TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Depth (root) != -1;
+    }
 
+    public int Depth (TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        var left = Depth (root.left);
+        if (left == -1) {
+            return -1;
+        }
+        var right = Depth (root.right);
+        if (right == -1) {
+            return -1;
+        }
+        if (left - right > 1 || right - left > 1) {
+            return -1;
+        }
+        return Math.Max (left, right) + 1;
     }
 }
 
