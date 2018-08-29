5e45906861eb8692af51914e29ea8663f02678a1
rosen369 <rosen369@outlook.com>
2018-08-29 14:37:36 +0800

    dp solution

--- 0115. Distinct Subsequences/Solution.cs
+++ 0115. Distinct Subsequences/Solution.cs
@@ -1,18 +1,19 @@
 public class Solution {
     public int NumDistinct (string s, string t) {
-        if (t.Length == 0) {
-            return 1;
+        var dp = new int[t.Length + 1, s.Length + 1];
+        for (int j = 0; j < s.Length + 1; j++) {
+            dp[0, j] = 1;
         }
-        if (t.Length > s.Length) {
-            return 0;
-        }
-        var count = 0;
-        for (int i = 0; i < s.Length; i++) {
-            if (s[i] == t[0]) {
-                count += NumDistinct (s.Substring (i + 1), t.Substring (1));
+        for (int i = 1; i < t.Length + 1; i++) {
+            for (int j = 1; j < s.Length + 1; j++) {
+                if (t[i - 1] == s[j - 1]) {
+                    dp[i, j] = dp[i - 1, j - 1] + dp[i, j - 1];
+                } else {
+                    dp[i, j] = dp[i, j - 1];
+                }
             }
         }
-        return count;
+        return dp[t.Length, s.Length];
     }
 }
 
