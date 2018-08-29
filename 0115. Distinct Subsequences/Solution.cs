a796fb91eeba7778d387bd1a38f4ef24d197cd73
rosen369 <rosen369@outlook.com>
2018-08-29 10:22:07 +0800

    time limit exceeded

--- 0115. Distinct Subsequences/Solution.cs
+++ 0115. Distinct Subsequences/Solution.cs
@@ -1,6 +1,18 @@
 public class Solution {
     public int NumDistinct (string s, string t) {
-
+        if (t.Length == 0) {
+            return 1;
+        }
+        if (t.Length > s.Length) {
+            return 0;
+        }
+        var count = 0;
+        for (int i = 0; i < s.Length; i++) {
+            if (s[i] == t[0]) {
+                count += NumDistinct (s.Substring (i + 1), t.Substring (1));
+            }
+        }
+        return count;
     }
 }
 
