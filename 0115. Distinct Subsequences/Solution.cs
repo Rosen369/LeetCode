0b845d9825cc69ee884e8943f11f4679a2db56fc
rosen369 <rosen369@outlook.com>
2018-08-29 14:50:35 +0800

    remove useless code

--- 0115. Distinct Subsequences/Solution.cs
+++ 0115. Distinct Subsequences/Solution.cs
@@ -16,10 +16,3 @@ public class Solution {
         return dp[t.Length, s.Length];
     }
 }
-
-public class TreeNode {
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
-    public TreeNode (int x) { val = x; }
-}
