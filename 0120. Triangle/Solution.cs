7c0f61523459ea82088ff5b07fa9ec59bfd5981e
rosen369 <rosen369@outlook.com>
2018-08-30 14:23:43 +0800

    bottom up solution

--- 0120. Triangle/Solution.cs
+++ 0120. Triangle/Solution.cs
@@ -3,22 +3,11 @@ public class Solution {
         if (triangle.Count () == 0) {
             return 0;
         }
-        for (var i = 1; i < triangle.Count (); i++) {
+        for (var i = triangle.Count () - 2; i >= 0; i--) {
             for (int j = 0; j < triangle[i].Count (); j++) {
-                if (j == 0) {
-                    triangle[i][j] += triangle[i - 1][j];
-                } else
-                if (j == triangle[i].Count () - 1) {
-                    triangle[i][j] += triangle[i - 1][j - 1];
-                } else {
-                    triangle[i][j] += Math.Min (triangle[i - 1][j], triangle[i - 1][j - 1]);
-                }
+                triangle[i][j] += Math.Min (triangle[i + 1][j], triangle[i + 1][j + 1]);
             }
         }
-        var min = triangle[triangle.Count () - 1][0];
-        for (int i = 0; i < triangle[triangle.Count () - 1].Count (); i++) {
-            min = Math.Min (min, triangle[triangle.Count () - 1][i]);
-        }
-        return min;
+        return triangle[0][0];
     }
 }
