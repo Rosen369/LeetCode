ef332e01f7c002879f936af7cdce516744a9bc50
rosen369 <rosen369@outlook.com>
2018-08-31 14:46:42 +0800

    expand dp solution met time limit

--- 0123. Best Time to Buy and Sell Stock III/Solution.cs
+++ 0123. Best Time to Buy and Sell Stock III/Solution.cs
@@ -1,16 +1,13 @@
 public class Solution {
     public int MaxProfit (int[] prices) {
-        if (prices.Length == 0) return 0;
-        var dp = new int[3, prices.Length];
-        for (int k = 1; k <= 2; k++) {
-            for (int i = 1; i < prices.Length; i++) {
-                int min = prices[0];
-                for (int j = 1; j <= i; j++) {
-                    min = Math.Min (min, prices[j] - dp[k - 1, j - 1]);
-                }
-                dp[k, i] = Math.Max (dp[k, i - 1], prices[i] - min);
-            }
+        int buy1 = int.MaxValue, buy2 = int.MaxValue;
+        int sell1 = 0, sell2 = 0;
+        for (int i = 0; i < prices.Length; i++) {
+            buy1 = Math.Min (buy1, prices[i]);
+            sell1 = Math.Max (sell1, prices[i] - buy1);
+            buy2 = Math.Min (buy2, prices[i] - sell1);
+            sell2 = Math.Max (sell2, prices[i] - buy2);
         }
-        return dp[2, prices.Length - 1];
+        return sell2;
     }
 }
