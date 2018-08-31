8acd17b3bb79558f7998d7747116ea7f3c5b837f
rosen369 <rosen369@outlook.com>
2018-08-31 14:38:20 +0800

    revise code format

--- 0123. Best Time to Buy and Sell Stock III/Solution.cs
+++ 0123. Best Time to Buy and Sell Stock III/Solution.cs
@@ -5,8 +5,9 @@ public class Solution {
         for (int k = 1; k <= 2; k++) {
             for (int i = 1; i < prices.Length; i++) {
                 int min = prices[0];
-                for (int j = 1; j <= i; j++)
+                for (int j = 1; j <= i; j++) {
                     min = Math.Min (min, prices[j] - dp[k - 1, j - 1]);
+                }
                 dp[k, i] = Math.Max (dp[k, i - 1], prices[i] - min);
             }
         }
