546e561760577dabcf0f7f9029cce15ec9b57b73
rosen369 <rosen369@outlook.com>
2018-09-06 14:46:08 +0800

    recursive solution

--- 0124. Binary Tree Maximum Path Sum/Solution.cs
+++ 0124. Binary Tree Maximum Path Sum/Solution.cs
@@ -9,6 +9,24 @@
  */
 public class Solution {
     public int MaxPathSum (TreeNode root) {
-
+        if (root == null) {
+            return int.MinValue;
+        }
+        var curr = root.val;
+        var leftMax = MaxPathSum (root.left);
+        var rightMax = MaxPathSum (root.right);
+        if (root.left != null) {
+            curr = Math.Max (curr, root.val + root.left.val);
+        }
+        if (root.right != null) {
+            curr = Math.Max (curr, root.val + root.right.val);
+        }
+        var max = curr;
+        if (root.left != null && root.right != null) {
+            max = Math.Max (max, root.val + root.left.val + root.right.val);
+        }
+        var childMax = Math.Max (leftMax, rightMax);
+        root.val = curr;
+        return Math.Max (max, childMax);
     }
 }
