1e16da87c3bd5ff12f26563cf41dc40286b58f0f
rosen369 <rosen369@outlook.com>
2018-09-10 11:18:35 +0800

    Time Limit Exceeded

--- 0126. Word Ladder II/Solution.cs
+++ 0126. Word Ladder II/Solution.cs
@@ -4,49 +4,29 @@ public class Solution {
         if (!wordList.Contains (endWord)) {
             return res;
         }
-        wordList.Remove (beginWord);
-        var startLink = FindLinked (beginWord, wordList);
-        var graph = new Dictionary<string, IList<string>> ();
-        for (int i = 0; i < wordList.Count (); i++) {
-            var leftList = new List<string> (wordList);
-            leftList.RemoveAt (i);
-            var linkList = FindLinked (wordList[i], leftList);
-            graph.Add (wordList[i], linkList);
-        }
-        graph.Add (beginWord, startLink);
-        FindNext (res, new List<string> () { beginWord }, wordList, graph, endWord, beginWord);
-        if (res.Count () != 0) {
-            var min = res.Min (e => e.Count ());
-            res = res.Where (e => e.Count () == min).ToList ();
-        }
+        var distance = new Dictionary<string, int> ();
+        var neighbors = new Dictionary<string, IList<string>> ();
+        LinkGraph (beginWord, wordList, distance, neighbors);
+        FindNext (res, new List<string> (), distance, neighbors, endWord, beginWord);
         return res;
     }
 
-    public void FindNext (IList<IList<string>> res, IList<string> path, IList<string> keys, IDictionary<string, IList<string>> graph, string endWord, string currWord) {
-        if (res.Count () != 0) {
-            var min = res.Min (e => e.Count ());
-            if (path.Count () > min) {
-                return;
-            }
-        }
+    public void FindNext (IList<IList<string>> res, IList<string> path, IDictionary<string, int> distance, IDictionary<string, IList<string>> neighbors, string endWord, string currWord) {
+        path.Add (currWord);
         if (currWord == endWord) {
             res.Add (new List<string> (path));
-            return;
-        }
-        var linked = graph[currWord];
-        for (int i = 0; i < linked.Count (); i++) {
-            if (!keys.Contains (linked[i])) {
-                continue;
+        } else {
+            var currNeighbors = neighbors[currWord];
+            foreach (var neighbor in currNeighbors) {
+                if (distance[currWord] + 1 == distance[neighbor]) {
+                    FindNext (res, path, distance, neighbors, endWord, neighbor);
+                }
             }
-            path.Add (linked[i]);
-            keys.Remove (linked[i]);
-            FindNext (res, path, keys, graph, endWord, linked[i]);
-            path.Remove (linked[i]);
-            keys.Add (linked[i]);
         }
+        path.RemoveAt (path.Count () - 1);
     }
 
-    public IList<string> FindLinked (string word, IList<string> wordList) {
+    public IList<string> FindNeighbors (string word, IList<string> wordList) {
         var list = new List<string> ();
         for (int i = 0; i < word.Length; i++) {
             for (char c = 'a'; c <= 'z'; c++) {
@@ -59,5 +39,27 @@ public class Solution {
             }
         }
         return list;
+    }
+
+    public void LinkGraph (string startWord, IList<string> wordList, IDictionary<string, int> distance, IDictionary<string, IList<string>> neighbors) {
+        var level = 0;
+        var currLevel = new HashSet<string> ();
+        var nextLevel = new HashSet<string> ();
+        currLevel.Add (startWord);
+        while (currLevel.Count () != 0) {
+            nextLevel.Clear ();
+            foreach (var word in currLevel) {
+                if (neighbors.ContainsKey (word)) {
+                    continue;
+                }
+                var wordNeighbots = FindNeighbors (word, wordList);
+                neighbors.Add (word, wordNeighbots);
+                distance.Add (word, level + 1);
+                nextLevel.UnionWith (wordNeighbots);
+            }
+            currLevel.Clear ();
+            currLevel.UnionWith (nextLevel);
+            level++;
+        }
     }
 }
