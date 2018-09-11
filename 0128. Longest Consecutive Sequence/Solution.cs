ed02f1d50f920915adddae271d769d9bbd377e76
rosen369 <rosen369@outlook.com>
2018-09-11 16:28:11 +0800

    hash set solution

--- 0128. Longest Consecutive Sequence/Solution.cs
+++ 0128. Longest Consecutive Sequence/Solution.cs
@@ -3,21 +3,19 @@ public class Solution {
         if (nums.Length == 0) {
             return 0;
         }
-        Array.Sort (nums);
-        var currLength = 1;
-        var longestLength = 1;
-        for (int i = 1; i < nums.Length; i++) {
-            if (nums[i] == nums[i - 1]) {
+        var set = new HashSet<int> (nums);
+        var longest = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            if (set.Contains (nums[i] - 1)) {
                 continue;
             }
-            if (nums[i] == nums[i - 1] + 1) {
-                currLength++;
-            } else {
-                longestLength = Math.Max (longestLength, currLength);
-                currLength = 1;
+            var num = nums[i];
+            var curr = 1;
+            while (set.Contains (++num)) {
+                curr++;
             }
+            longest = Math.Max (curr, longest);
         }
-        longestLength = Math.Max (longestLength, currLength);
-        return longestLength;
+        return longest;
     }
 }
