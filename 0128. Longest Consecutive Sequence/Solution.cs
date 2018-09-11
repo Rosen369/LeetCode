a742e72b0c91eb317f4826dec00e858bcf90ce7c
rosen369 <rosen369@outlook.com>
2018-09-11 16:06:17 +0800

    sorting solution

--- 0128. Longest Consecutive Sequence/Solution.cs
+++ 0128. Longest Consecutive Sequence/Solution.cs
@@ -1,5 +1,23 @@
 public class Solution {
     public int LongestConsecutive (int[] nums) {
-
+        if (nums.Length == 0) {
+            return 0;
+        }
+        Array.Sort (nums);
+        var currLength = 1;
+        var longestLength = 1;
+        for (int i = 1; i < nums.Length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if (nums[i] == nums[i - 1] + 1) {
+                currLength++;
+            } else {
+                longestLength = Math.Max (longestLength, currLength);
+                currLength = 1;
+            }
+        }
+        longestLength = Math.Max (longestLength, currLength);
+        return longestLength;
     }
 }
