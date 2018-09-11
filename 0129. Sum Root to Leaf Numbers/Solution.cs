94afae2367d3676ed6e0387030d854706e44ac27
rosen369 <rosen369@outlook.com>
2018-09-11 17:19:57 +0800

    DFS solution

---
+++ 0129. Sum Root to Leaf Numbers/Solution.cs
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int SumNumbers (TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        var res = new List<int> ();
+        DFS (root, res, 0);
+        return res.Sum ();
+    }
+
+    public void DFS (TreeNode node, IList<int> res, int curr) {
+        if (node.left == null && node.right == null) {
+            curr += node.val;
+            res.Add (curr);
+            return;
+        }
+        curr = curr * 10;
+        curr += node.val;
+        if (node.left != null) {
+            DFS (node.left, res, curr);
+        }
+        if (node.right != null) {
+            DFS (node.right, res, curr);
+        }
+    }
+}
