e90c158f4efd54147e4915171c15f96c4db8b5cf
rosen369 <rosen369@outlook.com>
2018-09-14 14:17:57 +0800

    Time Limit Exceeded

--- 0132. Palindrome Partitioning II/Solution.cs
+++ 0132. Palindrome Partitioning II/Solution.cs
@@ -1,29 +1,31 @@
 public class Solution {
     public int MinCut (string s) {
-        var min = s.Length - 1;
-        min = BackTrack (s, 0, min);
-        return min;
-    }
-
-    public int BackTrack (string s, int step, int min) {
         if (string.IsNullOrEmpty (s)) {
             return 0;
         }
-        if (step >= min) {
-            return step;
+        var dic = new Dictionary<string, int> ();
+        BackTrack (s, dic);
+        return dic[s];
+    }
+
+    public void BackTrack (string s, Dictionary<string, int> dic) {
+        if (dic.ContainsKey (s)) {
+            return;
         }
         if (IsPalindrome (s)) {
-            return step;
+            dic.Add (s, 0);
+            return;
         }
+        var min = s.Length;
         for (int i = 1; i <= s.Length - 1; i++) {
             var left = s.Substring (0, i);
             var right = s.Substring (i);
             if (IsPalindrome (left)) {
-                var res = BackTrack (right, step + 1, min);
-                min = Math.Min (min, res);
+                BackTrack (right, dic);
+                min = Math.Min (min, dic[right]);
             }
         }
-        return min;
+        dic.Add (s, 1 + min);
     }
 
     public bool IsPalindrome (string s) {
