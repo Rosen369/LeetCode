dbdb5661422d4b7bf0ca77ba82d335c98e4ca85e
rosen369 <rosen369@outlook.com>
2018-09-14 15:15:33 +0800

    dynamic programming solution

--- 0132. Palindrome Partitioning II/Solution.cs
+++ 0132. Palindrome Partitioning II/Solution.cs
@@ -1,39 +1,20 @@
 public class Solution {
     public int MinCut (string s) {
-        if (string.IsNullOrEmpty (s)) {
-            return 0;
-        }
-        var dic = new Dictionary<string, int> ();
-        BackTrack (s, dic);
-        return dic[s];
-    }
-
-    public void BackTrack (string s, Dictionary<string, int> dic) {
-        if (dic.ContainsKey (s)) {
-            return;
-        }
-        if (IsPalindrome (s)) {
-            dic.Add (s, 0);
-            return;
-        }
-        var min = s.Length;
-        for (int i = 1; i <= s.Length - 1; i++) {
-            var left = s.Substring (0, i);
-            var right = s.Substring (i);
-            if (IsPalindrome (left)) {
-                BackTrack (right, dic);
-                min = Math.Min (min, dic[right]);
+        var len = s.Length;
+        var cut = new int[len];
+        var dp = new bool[len, len];
+        for (int i = 0; i < len; i++) {
+            var min = i;
+            for (int j = 0; j <= i; j++) {
+                if (s[j] == s[i]) {
+                    if (j + 1 > i - 1 || dp[j + 1, i - 1]) {
+                        dp[j, i] = true;
+                        min = j == 0 ? 0 : Math.Min (min, cut[j - 1] + 1);
+                    }
+                }
             }
+            cut[i] = min;
         }
-        dic.Add (s, 1 + min);
-    }
-
-    public bool IsPalindrome (string s) {
-        for (int i = 0; i < s.Length / 2; i++) {
-            if (s[i] != s[s.Length - 1 - i]) {
-                return false;
-            }
-        }
-        return true;
+        return cut[len - 1];
     }
 }
