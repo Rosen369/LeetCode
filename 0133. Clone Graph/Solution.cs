09a95ed1ea2698c0717db97c477c9c2d7ba7cc93
rosen369 <rosen369@outlook.com>
2018-09-17 11:15:56 +0800

    DFS solution

--- 0133. Clone Graph/Solution.cs
+++ 0133. Clone Graph/Solution.cs
@@ -8,6 +8,27 @@
  */
 public class Solution {
     public UndirectedGraphNode CloneGraph (UndirectedGraphNode node) {
+        var dict = new Dictionary<int, UndirectedGraphNode> ();
+        var clone = DFS (node, dict);
+        return clone;
+    }
 
+    public UndirectedGraphNode DFS (UndirectedGraphNode node, Dictionary<int, UndirectedGraphNode> dict) {
+        if (node == null) {
+            return null;
+        }
+        if (dict.ContainsKey (node.label)) {
+            return dict[node.label];
+        }
+        var clone = new UndirectedGraphNode (node.label);
+        dict.Add (clone.label, clone);
+        foreach (var neighbor in node.neighbors) {
+            if (dict.ContainsKey (neighbor.label)) {
+                clone.neighbors.Add (dict[neighbor.label]);
+            } else {
+                clone.neighbors.Add (DFS (neighbor, dict));
+            }
+        }
+        return clone;
     }
 }
