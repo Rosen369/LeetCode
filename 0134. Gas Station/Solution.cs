12a6691886a657aa3baaa860729b58e781027483
rosen369 <rosen369@outlook.com>
2018-09-17 14:21:03 +0800

    one pass solution

--- 0134. Gas Station/Solution.cs
+++ 0134. Gas Station/Solution.cs
@@ -1,5 +1,23 @@
 public class Solution {
     public int CanCompleteCircuit (int[] gas, int[] cost) {
-
+        var start = 0;
+        while (start < gas.Length) {
+            var fuel = 0;
+            for (int curr = start; curr < start + gas.Length; curr++) {
+                var pos = curr;
+                if (curr >= gas.Length) {
+                    pos = curr - gas.Length;
+                }
+                fuel += gas[pos] - cost[pos];
+                if (fuel < 0) {
+                    start = curr + 1;
+                    break;
+                }
+            }
+            if (fuel >= 0) {
+                return start;
+            }
+        }
+        return -1;
     }
 }
