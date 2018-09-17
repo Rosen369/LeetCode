c98fc8630b8d039c50f0c86009ff17e267f5ad7b
rosen369 <rosen369@outlook.com>
2018-09-17 09:35:18 +0800

    rename variable

--- 0132. Palindrome Partitioning II/Solution.cs
+++ 0132. Palindrome Partitioning II/Solution.cs
@@ -3,17 +3,17 @@ public class Solution {
         var len = s.Length;
         var cut = new int[len];
         var dp = new bool[len, len];
-        for (int i = 0; i < len; i++) {
-            var min = i;
-            for (int j = 0; j <= i; j++) {
-                if (s[j] == s[i]) {
-                    if (j + 1 > i - 1 || dp[j + 1, i - 1]) {
-                        dp[j, i] = true;
-                        min = j == 0 ? 0 : Math.Min (min, cut[j - 1] + 1);
+        for (int end = 0; end < len; end++) {
+            var min = end;
+            for (int start = 0; start <= end; start++) {
+                if (s[start] == s[end]) {
+                    if (start + 1 > end - 1 || dp[start + 1, end - 1]) {
+                        dp[start, end] = true;
+                        min = start == 0 ? 0 : Math.Min (min, cut[start - 1] + 1);
                     }
                 }
             }
-            cut[i] = min;
+            cut[end] = min;
         }
         return cut[len - 1];
     }
