5b83db3a70edfa2a72051eaa2134430c7df8ca90
rosen369 <rosen369@outlook.com>
2018-09-18 11:50:10 +0800

    optimize code

--- 0135. Candy/Solution.cs
+++ 0135. Candy/Solution.cs
@@ -3,25 +3,17 @@ public class Solution {
         var left2right = new int[ratings.Length];
         var right2left = new int[ratings.Length];
         for (int i = 0; i < ratings.Length; i++) {
-            if (i == 0) {
+            if (i == 0 || ratings[i] <= ratings[i - 1]) {
                 left2right[i] = 1;
-                continue;
-            }
-            if (ratings[i] > ratings[i - 1]) {
-                left2right[i] = left2right[i - 1] + 1;
             } else {
-                left2right[i] = 1;
+                left2right[i] = left2right[i - 1] + 1;
             }
         }
         for (int i = ratings.Length - 1; i >= 0; i--) {
-            if (i == ratings.Length - 1) {
+            if (i == ratings.Length - 1 || ratings[i] <= ratings[i + 1]) {
                 right2left[i] = 1;
-                continue;
-            }
-            if (ratings[i] > ratings[i + 1]) {
-                right2left[i] = right2left[i + 1] + 1;
             } else {
-                right2left[i] = 1;
+                right2left[i] = right2left[i + 1] + 1;
             }
         }
         var res = 0;
