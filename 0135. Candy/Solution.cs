b7e4ad9b80f5798d60c0b44fd9df43d74e61401d
rosen369 <rosen369@outlook.com>
2018-09-18 11:42:34 +0800

    two array solution

--- 0135. Candy/Solution.cs
+++ 0135. Candy/Solution.cs
@@ -1,5 +1,33 @@
 public class Solution {
     public int Candy (int[] ratings) {
-
+        var left2right = new int[ratings.Length];
+        var right2left = new int[ratings.Length];
+        for (int i = 0; i < ratings.Length; i++) {
+            if (i == 0) {
+                left2right[i] = 1;
+                continue;
+            }
+            if (ratings[i] > ratings[i - 1]) {
+                left2right[i] = left2right[i - 1] + 1;
+            } else {
+                left2right[i] = 1;
+            }
+        }
+        for (int i = ratings.Length - 1; i >= 0; i--) {
+            if (i == ratings.Length - 1) {
+                right2left[i] = 1;
+                continue;
+            }
+            if (ratings[i] > ratings[i + 1]) {
+                right2left[i] = right2left[i + 1] + 1;
+            } else {
+                right2left[i] = 1;
+            }
+        }
+        var res = 0;
+        for (int i = 0; i < ratings.Length; i++) {
+            res += Math.Max (left2right[i], right2left[i]);
+        }
+        return res;
     }
 }
