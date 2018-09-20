6e420f738bdaa3ed4077dd9ba620ba595af20e85
rosen369 <rosen369@outlook.com>
2018-09-20 09:35:02 +0800

    Time Limit Exceeded

--- 0138. Copy List with Random Pointer/Solution.cs
+++ 0138. Copy List with Random Pointer/Solution.cs
@@ -8,6 +8,32 @@
  */
 public class Solution {
     public RandomListNode CopyRandomList (RandomListNode head) {
-
+        if (head == null) {
+            return null;
+        }
+        var curr = head;
+        var oriList = new List<RandomListNode> ();
+        while (curr != null) {
+            oriList.Add (curr);
+            curr = curr.next;
+        }
+        var copyList = new List<RandomListNode> ();
+        for (int i = 0; i < oriList.Count (); i++) {
+            copyList.Add (new RandomListNode (oriList[i].label));
+            if (i != 0) {
+                copyList[i - 1].next = copyList[i];
+            }
+        }
+        for (int i = 0; i < oriList.Count (); i++) {
+            if (oriList[i].random == null) {
+                continue;
+            }
+            for (int j = 0; j < oriList.Count (); j++) {
+                if (Object.ReferenceEquals (oriList[i].random, oriList[j])) {
+                    copyList[i].random = copyList[j];
+                }
+            }
+        }
+        return copyList[0];
     }
 }
