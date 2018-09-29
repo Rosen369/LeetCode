ea8086f234d08c64b141a4e69da5522bb355ba12
rosen369 <rosen369@outlook.com>
2018-09-29 10:20:29 +0800

    dynamic programming solution

--- 0139. Word Break/Solution.cs
+++ 0139. Word Break/Solution.cs
@@ -1,5 +1,18 @@
 public class Solution {
     public bool WordBreak (string s, IList<string> wordDict) {
-
+        var dp = new bool[s.Length + 1];
+        dp[0] = true;
+        for (int i = 0; i < s.Length; i++) {
+            for (int j = 0; j < wordDict.Count (); j++) {
+                if (i + wordDict[j].Length > s.Length) {
+                    continue;
+                }
+                var sub = s.Substring (i, wordDict[j].Length);
+                if (dp[i] == true && sub == wordDict[j]) {
+                    dp[i + wordDict[j].Length] = true;
+                }
+            }
+        }
+        return dp[s.Length];
     }
 }
