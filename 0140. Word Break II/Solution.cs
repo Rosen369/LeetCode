88c2694c30cdebf3e4b9298bc3dccd5d71ae6369
rosen369 <rosen369@outlook.com>
2018-10-08 10:53:06 +0800

    DFS solution

--- 0140. Word Break II/Solution.cs
+++ 0140. Word Break II/Solution.cs
@@ -1,24 +1,32 @@
 public class Solution {
     public IList<string> WordBreak (string s, IList<string> wordDict) {
-        var res = new List<string> ();
-        BackTrack (s, wordDict, res, new List<string> ());
-        return res;
+        var dict = new Dictionary<string, IList<string>> ();
+        DFS (s, wordDict, dict);
+        return dict[s];
     }
 
-    public void BackTrack (string s, IList<string> wordDict, IList<string> res, IList<string> curr) {
-        if (string.IsNullOrEmpty (s)) {
-            res.Add (string.Join (" ", curr.ToArray ()));
-            return;
-        }
+    public void DFS (string s, IList<string> wordDict, IDictionary<string, IList<string>> dict) {
+        dict.Add (s, new List<string> ());
         for (int i = 0; i < wordDict.Count (); i++) {
             var word = wordDict[i];
             if (word.Length > s.Length) {
                 continue;
             }
-            if (s.Substring (0, word.Length) == word) {
-                curr.Add (word);
-                BackTrack (s.Substring (word.Length), wordDict, res, curr);
-                curr.RemoveAt (curr.Count () - 1);
+            if (word == s) {
+                dict[s].Add (s);
+                continue;
+            }
+            var left = s.Substring (0, word.Length);
+            if (left != word) {
+                continue;
+            }
+            var right = s.Substring (word.Length);
+            if (!dict.ContainsKey (right)) {
+                DFS (right, wordDict, dict);
+            }
+            foreach (var combine in dict[right]) {
+                var curr = left + " " + combine;
+                dict[s].Add (curr);
             }
         }
     }
