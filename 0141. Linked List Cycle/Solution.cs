eacbe90ab6be58775fb087c06f09ad0e4f1a1722
rosen369 <rosen369@outlook.com>
2018-10-08 11:26:22 +0800

    two pointer solution

--- 0141. Linked List Cycle/Solution.cs
+++ 0141. Linked List Cycle/Solution.cs
@@ -11,6 +11,18 @@
  */
 public class Solution {
     public bool HasCycle (ListNode head) {
-
+        if (head == null) {
+            return false;
+        }
+        var walker = head;
+        var runner = head;
+        while (runner != null && runner.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if (walker == runner) {
+                return true;
+            }
+        }
+        return false;
     }
 }
