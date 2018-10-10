3c504942d4ca59045033ea60569b07ee752eac93
rosen369 <rosen369@outlook.com>
2018-10-10 14:03:06 +0800

    list solution

--- 0143. Reorder List/Solution.cs
+++ 0143. Reorder List/Solution.cs
@@ -8,6 +8,21 @@
  */
 public class Solution {
     public void ReorderList (ListNode head) {
-
+        var list = new List<ListNode> ();
+        while (head != null) {
+            list.Add (head);
+            head = head.next;
+        }
+        for (int i = 0; i < list.Count (); i++) {
+            if (i == list.Count () / 2) {
+                list[i].next = null;
+                continue;
+            }
+            var next = list.Count () - i;
+            if (i < list.Count () / 2) {
+                next--;
+            }
+            list[i].next = list[next];
+        }
     }
 }
