057f2603320b1d95fd7d03aad44d93d403de3695
rosen369 <rosen369@outlook.com>
2018-10-10 16:48:29 +0800

    iterative and recursive solutions

---
+++ 0145. Binary Tree Postorder Traversal/README.md
@@ -0,0 +1,18 @@
+# Binary Tree Postorder Traversal
+
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+__Example :__
+
+```
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [3,2,1]
+```
+
+__Follow up:__ Recursive solution is trivial, could you do it iteratively?
---
+++ 0145. Binary Tree Postorder Traversal/Solution.cs
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public IList<int> PostorderTraversal (TreeNode root) {
+        var res = new List<int> ();
+        Recursive (res, root);
+        return res;
+    }
+
+    public void Recursive (IList<int> res, TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        Recursive (res, node.left);
+        Recursive (res, node.right);
+        res.Add (node.val);
+    }
+
+    public IList<int> Iterative (TreeNode root) {
+        var res = new List<int> ();
+        if (root == null) {
+            return res;
+        }
+        var stack = new Stack<TreeNode> ();
+        stack.Push (root);
+        while (stack.Count () != 0) {
+            var top = stack.Pop ();
+            if (top.left == null && top.right == null) {
+                res.Add (top.val);
+            } else {
+                var left = top.left;
+                var right = top.right;
+                top.left = null;
+                top.right = null;
+                stack.Push (top);
+                if (right != null) {
+                    stack.Push (right);
+                }
+                if (left != null) {
+                    stack.Push (left);
+                }
+            }
+
+        }
+        return res;
+    }
+}
