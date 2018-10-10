a0c7ff19894b9431eebcf5793678817e53ab1518
rosen369 <rosen369@outlook.com>
2018-10-10 16:34:12 +0800

    iterative and recursive solutions

--- 0144. Binary Tree Preorder Traversal/Solution.cs
+++ 0144. Binary Tree Preorder Traversal/Solution.cs
@@ -9,6 +9,37 @@
  */
 public class Solution {
     public IList<int> PreorderTraversal (TreeNode root) {
+        var res = new List<int> ();
+        Recursive (res, root);
+        return res;
+    }
 
+    public void Recursive (IList<int> res, TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        res.Add (node.val);
+        Recursive (res, node.left);
+        Recursive (res, node.right);
+    }
+
+    public IList<int> Iterative (TreeNode root) {
+        var res = new List<int> ();
+        if (root == null) {
+            return res;
+        }
+        var stack = new Stack<TreeNode> ();
+        stack.Push (root);
+        while (stack.Count () != 0) {
+            var top = stack.Pop ();
+            res.Add (top.val);
+            if (top.right != null) {
+                stack.Push (top.right);
+            }
+            if (top.left != null) {
+                stack.Push (top.left);
+            }
+        }
+        return res;
     }
 }
