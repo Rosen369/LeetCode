5ad961a1f9e4eb7c6f1292f86304dd3624b091eb
rosen369 <rosen369@outlook.com>
2018-10-11 16:33:02 +0800

    resolve insertion sort list

--- 0147. Insertion Sort List/Solution.cs
+++ 0147. Insertion Sort List/Solution.cs
@@ -8,6 +8,32 @@
  */
 public class Solution {
     public ListNode InsertionSortList (ListNode head) {
-
+        if (head == null) {
+            return null;
+        }
+        var curr = head;
+        var next = curr.next;
+        head.next = null;
+        while (next != null) {
+            curr = next;
+            next = curr.next;
+            curr.next = null;
+            if (curr.val < head.val) {
+                curr.next = head;
+                head = curr;
+            } else {
+                var front = head;
+                while (true) {
+                    if (front.next == null || front.next.val > curr.val) {
+                        break;
+                    }
+                    front = front.next;
+                }
+                var tail = front.next;
+                front.next = curr;
+                curr.next = tail;
+            }
+        }
+        return head;
     }
 }
