114286c7bf7644dc0ab5a6c8f544e5c5d5e2cbde
rosen369 <rosen369@outlook.com>
2018-10-11 10:14:56 +0800

    dictionary solution

--- 0146. LRU Cache/Solution.cs
+++ 0146. LRU Cache/Solution.cs
@@ -1,15 +1,40 @@
 public class LRUCache {
-
     public LRUCache (int capacity) {
-
+        _dict = new Dictionary<int, int> ();
+        _keys = new List<int> ();
+        _capacity = capacity;
     }
 
-    public int Get (int key) {
+    private IDictionary<int, int> _dict;
 
+    private readonly IList<int> _keys;
+
+    private readonly int _capacity;
+
+    public int Get (int key) {
+        if (_dict.ContainsKey (key)) {
+            _keys.Remove (key);
+            _keys.Add (key);
+            return _dict[key];
+        }
+        return -1;
     }
 
     public void Put (int key, int value) {
-
+        if (_dict.ContainsKey (key)) {
+            _keys.Remove (key);
+            _keys.Add (key);
+            _dict[key] = value;
+        } else if (_dict.Count () >= _capacity) {
+            var last = _keys[0];
+            _keys.RemoveAt (0);
+            _keys.Add (key);
+            _dict.Remove (last);
+            _dict.Add (key, value);
+        } else {
+            _keys.Add (key);
+            _dict.Add (key, value);
+        }
     }
 }
 
