fd83b6571e73f94c48618986493ddd1f52e92405
rosen369 <rosen369@outlook.com>
2018-10-17 15:11:38 +0800

    binary search solution

--- 0162. Find Peak Element/Solution.cs
+++ 0162. Find Peak Element/Solution.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public int FindPeakElement (int[] nums) {
-
+        var left = 0;
+        var right = nums.Length - 1;
+        while (left < right) {
+            var mid = (left + right) / 2;
+            if (nums[mid] > nums[mid + 1])
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return left;
     }
 }
