de9aaf3665762dff16af9a126cda37c8145d4f52
rosen369 <rosen369@outlook.com>
2018-10-17 14:30:39 +0800

    O(n) solution

--- 0160. Intersection of Two Linked Lists/Solution.cs
+++ 0160. Intersection of Two Linked Lists/Solution.cs
@@ -8,6 +8,38 @@
  */
 public class Solution {
     public ListNode GetIntersectionNode (ListNode headA, ListNode headB) {
-
+        var countA = 0;
+        var currA = headA;
+        while (currA != null) {
+            countA++;
+            currA = currA.next;
+        }
+        var countB = 0;
+        var currB = headB;
+        while (currB != null) {
+            countB++;
+            currB = currB.next;
+        }
+        var max = Math.Max (countA, countB);
+        currA = headA;
+        currB = headB;
+        for (int i = 0; i < max; i++) {
+            if (countA == countB) {
+                if (currA == currB) {
+                    return currA;
+                }
+                currA = currA.next;
+                currB = currB.next;
+                countA--;
+                countB--;
+            } else if (countA > countB) {
+                currA = currA.next;
+                countA--;
+            } else {
+                currB = currB.next;
+                countB--;
+            }
+        }
+        return null;
     }
 }
