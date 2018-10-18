700ea3f9d4d62f9c05dfc52be6c2f3b1445d2de9
rosen369 <rosen369@outlook.com>
2018-10-18 14:55:55 +0800

    resolve compare version numbers

--- 0165. Compare Version Numbers/Solution.cs
+++ 0165. Compare Version Numbers/Solution.cs
@@ -1,5 +1,24 @@
 public class Solution {
     public int CompareVersion (string version1, string version2) {
-
+        var ver1 = version1.Split ('.').Select (e => Convert.ToInt32 (e)).ToList ();
+        var ver2 = version2.Split ('.').Select (e => Convert.ToInt32 (e)).ToList ();
+        var max = Math.Max (ver1.Count (), ver2.Count ());
+        while (ver1.Count () < max) {
+            ver1.Add (0);
+        }
+        while (ver2.Count () < max) {
+            ver2.Add (0);
+        }
+        for (int i = 0; i < max; i++) {
+            if (ver1[i] == ver2[i]) {
+                continue;
+            }
+            if (ver1[i] < ver2[i]) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+        return 0;
     }
 }
