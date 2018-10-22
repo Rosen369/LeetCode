751e19a27d913998f20491f0298eb51edbf35d26
rosen369 <rosen369@outlook.com>
2018-10-22 17:00:22 +0800

    resolve two sum ii

--- 0167. Two Sum II - Input array is sorted/Solution.cs
+++ 0167. Two Sum II - Input array is sorted/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public int[] TwoSum (int[] numbers, int target) {
-
+        var left = 0;
+        var right = numbers.Length - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                break;
+            }
+            if (numbers[left] + numbers[right] > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return new int[] { left + 1, right + 1 };
     }
 }
