7fc6fc9a410af21ca247a63af5ae0c4dd645cb8b
rosen369 <rosen369@outlook.com>
2018-10-22 16:50:13 +0800

    resolve fraction to recurring decimal

--- 0166. Fraction to Recurring Decimal/Solution.cs
+++ 0166. Fraction to Recurring Decimal/Solution.cs
@@ -1,5 +1,37 @@
 public class Solution {
     public string FractionToDecimal (int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
+        }
+        var sb = new StringBuilder ();
+        if ((numerator > 0) ^ (denominator > 0)) {
+            sb.Append ("-");
+        }
+        var num = Math.Abs ((long) numerator);
+        var den = Math.Abs ((long) denominator);
 
+        sb.Append (num / den);
+        num %= den;
+        if (num == 0) {
+            return sb.ToString ();
+        }
+
+        sb.Append (".");
+        var dict = new Dictionary<long, int> ();
+        dict.Add (num, sb.Length);
+        while (num != 0) {
+            num *= 10;
+            sb.Append (num / den);
+            num %= den;
+            if (dict.ContainsKey (num)) {
+                var index = dict[num];
+                sb.Insert (index, "(");
+                sb.Append (")");
+                break;
+            } else {
+                dict.Add (num, sb.Length);
+            }
+        }
+        return sb.ToString ();
     }
 }
