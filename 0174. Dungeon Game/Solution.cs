39880d4bb5be99fa903d89ba8f3b70e8ba4dfef6
rosen369 <rosen369@outlook.com>
2018-10-24 16:29:22 +0800

    dp solution

--- 0174. Dungeon Game/Solution.cs
+++ 0174. Dungeon Game/Solution.cs
@@ -1,5 +1,26 @@
 public class Solution {
     public int CalculateMinimumHP (int[][] dungeon) {
-
+        var row = dungeon.Length;
+        var col = dungeon[0].Length;
+        var dp = new int[row + 1, col + 1];
+        for (int i = 0; i < row - 1; i++) {
+            dp[i, col] = int.MaxValue;
+        }
+        for (int j = 0; j < col - 1; j++) {
+            dp[row, j] = int.MaxValue;
+        }
+        dp[row, col - 1] = 1;
+        dp[row - 1, col] = 1;
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j >= 0; j--) {
+                var need = Math.Min (dp[i + 1, j], dp[i, j + 1]) - dungeon[i][j];
+                if (need <= 0) {
+                    dp[i, j] = 1;
+                } else {
+                    dp[i, j] = need;
+                }
+            }
+        }
+        return dp[0, 0];
     }
 }
