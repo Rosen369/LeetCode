b01cc43447e2260e187d692fd1e1ac2ec5302b0c
rosen369 <rosen369@outlook.com>
2018-10-24 10:12:34 +0800

    math solution

--- 0172. Factorial Trailing Zeroes/Solution.cs
+++ 0172. Factorial Trailing Zeroes/Solution.cs
@@ -1,5 +1,10 @@
 public class Solution {
     public int TrailingZeroes (int n) {
-
+        var res = 0;
+        while (n > 0) {
+            res += n / 5;
+            n /= 5;
+        }
+        return res;
     }
 }
