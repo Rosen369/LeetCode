23ccfae0dd34044d52665ef990b17a5a1bed94a1
rosen369 <rosen369@outlook.com>
2018-10-24 10:57:15 +0800

    stack solution

--- 0173. Binary Search Tree Iterator/Solution.cs
+++ 0173. Binary Search Tree Iterator/Solution.cs
@@ -11,17 +11,32 @@
 public class BSTIterator {
 
     public BSTIterator (TreeNode root) {
+        _stack = new Stack<TreeNode> ();
+        this.PushLeft (root);
+    }
 
+    private Stack<TreeNode> _stack;
+
+    private void PushLeft (TreeNode node) {
+        while (node != null) {
+            _stack.Push (node);
+            node = node.left;
+        }
     }
 
     /** @return whether we have a next smallest number */
     public bool HasNext () {
-
+        if (_stack.Count () != 0) {
+            return true;
+        }
+        return false;
     }
 
     /** @return the next smallest number */
     public int Next () {
-
+        var node = _stack.Pop ();
+        this.PushLeft (node.right);
+        return node.val;
     }
 }
 
