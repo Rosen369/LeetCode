0263052c98a9bc54496a2a3a5892c4a50bed8ad3
rosen369 <rosen369@outlook.com>
2018-10-25 14:31:15 +0800

    hash set solution

--- 0187. Repeated DNA Sequences/Solution.cs
+++ 0187. Repeated DNA Sequences/Solution.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public IList<string> FindRepeatedDnaSequences (string s) {
-
+        var once = new HashSet<string> ();
+        var twice = new HashSet<string> ();
+        for (int i = 0; i < s.Length - 9; i++) {
+            var sub = s.Substring (i, 10);
+            if (once.Contains (sub)) {
+                twice.Add (sub);
+            }
+            once.Add (sub);
+        }
+        return twice.ToList ();
     }
 }
