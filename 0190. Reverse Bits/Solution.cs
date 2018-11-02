d40f36430ffccd3860ba816cee8aff71c21b050e
rosen369 <rosen369@outlook.com>
2018-11-02 16:31:42 +0800

    resolve reverse bits

--- 0190. Reverse Bits/Solution.cs
+++ 0190. Reverse Bits/Solution.cs
@@ -1,5 +1,12 @@
 public class Solution {
     public uint reverseBits (uint n) {
-
+        if (n == 0) return 0;
+        uint res = 0;
+        for (var i = 0; i < 32; i++) {
+            res = res << 1;
+            if ((n & 1) == 1) res++;
+            n = n >> 1;
+        }
+        return res;
     }
 }
