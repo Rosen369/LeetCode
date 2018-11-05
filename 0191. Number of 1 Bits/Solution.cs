1567adb46ea2a463532660779888e6a718467f69
rosen369 <rosen369@outlook.com>
2018-11-05 09:33:02 +0800

    math solution

--- 0191. Number of 1 Bits/Solution.cs
+++ 0191. Number of 1 Bits/Solution.cs
@@ -1,5 +1,15 @@
 public class Solution {
     public int HammingWeight (uint n) {
-
+        if (n == 0) {
+            return 0;
+        }
+        var res = 0;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                res++;
+            }
+            n = n / 2;
+        }
+        return res;
     }
 }
