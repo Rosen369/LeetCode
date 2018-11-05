5b9b84ad252759ebefb37f4c65578fefdecf1dd3
rosen369 <rosen369@outlook.com>
2018-11-05 10:03:24 +0800

    time limit exceeded

--- 0198. House Robber/Solution.cs
+++ 0198. House Robber/Solution.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public int Rob (int[] nums) {
+        if (nums.Length == 0) {
+            return 0;
+        }
+        return Recursive (nums, 0);
+    }
 
+    public int Recursive (int[] nums, int start) {
+        if (start >= nums.Length) {
+            return 0;
+        }
+        if (nums.Length == start + 1) {
+            return nums[start];
+        }
+        var first = nums[start] + Recursive (nums, start + 2);
+        var second = nums[start + 1] + Recursive (nums, start + 3);
+        return Math.Max (first, second);
     }
 }
