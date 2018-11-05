ce9a92f0d80d8f525cc66068dfa9c4c2491d5e30
rosen369 <rosen369@outlook.com>
2018-11-05 15:12:11 +0800

    bit operation solution

--- 0201. Bitwise AND of Numbers Range/Solution.cs
+++ 0201. Bitwise AND of Numbers Range/Solution.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public int RangeBitwiseAnd (int m, int n) {
-
+        if (m == 0) {
+            return 0;
+        }
+        var factor = 1;
+        while (m != n) {
+            m >>= 1;
+            n >>= 1;
+            factor <<= 1;
+        }
+        return m * factor;
     }
 }
