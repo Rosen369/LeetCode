a7cbfcbf9e7b89925eda65d397214d2fe9efaa2f
rosen369 <rosen369@outlook.com>
2018-11-05 10:56:39 +0800

    bfs solution

--- 0199. Binary Tree Right Side View/Solution.cs
+++ 0199. Binary Tree Right Side View/Solution.cs
@@ -9,6 +9,26 @@
  */
 public class Solution {
     public IList<int> RightSideView (TreeNode root) {
-
+        if (root == null) {
+            return new List<int> ();
+        }
+        var curr = new List<TreeNode> ();
+        var next = new List<TreeNode> ();
+        var res = new List<int> ();
+        curr.Add (root);
+        while (curr.Count () != 0) {
+            foreach (var node in curr) {
+                if (node.left != null) {
+                    next.Add (node.left);
+                }
+                if (node.right != null) {
+                    next.Add (node.right);
+                }
+            }
+            res.Add (curr.Last ().val);
+            curr = next;
+            next = new List<TreeNode> ();
+        }
+        return res;
     }
 }
