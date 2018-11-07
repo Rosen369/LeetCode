ab73935adbaa613d5c60622a6ad6746cd670efa7
rosen369 <rosen369@outlook.com>
2018-11-07 09:52:42 +0800

    straight forward solution

--- 0203. Remove Linked List Elements/Solution.cs
+++ 0203. Remove Linked List Elements/Solution.cs
@@ -8,6 +8,17 @@
  */
 public class Solution {
     public ListNode RemoveElements (ListNode head, int val) {
-
+        var res = new ListNode (0);
+        var curr = res;
+        while (head != null) {
+            if (head.val != val) {
+                curr.next = head;
+                curr = curr.next;
+            }
+            var pre = head;
+            head = head.next;
+            pre.next = null;
+        }
+        return res.next;
     }
 }
