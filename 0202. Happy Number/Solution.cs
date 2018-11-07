85be32326b6eb7113464d3eb789a2c6585bacdc8
rosen369 <rosen369@outlook.com>
2018-11-07 09:44:54 +0800

    hash set solution

--- 0202. Happy Number/Solution.cs
+++ 0202. Happy Number/Solution.cs
@@ -1,5 +1,19 @@
 public class Solution {
     public bool IsHappy (int n) {
-
+        var set = new HashSet<int> ();
+        while (n != 1) {
+            var res = 0;
+            while (n > 0) {
+                var bit = n % 10;
+                res += bit * bit;
+                n = n / 10;
+            }
+            if (set.Contains (res)) {
+                return false;
+            }
+            set.Add (res);
+            n = res;
+        }
+        return true;
     }
 }
