6662f77a656ad8d81402a6d33c2be615da981d7b
rosen369 <rosen369@outlook.com>
2018-11-08 09:04:51 +0800

    one round solution

--- 0204. Count Primes/Solution.cs
+++ 0204. Count Primes/Solution.cs
@@ -1,5 +1,15 @@
 public class Solution {
     public int CountPrimes (int n) {
-
+        var notPrime = new bool[n];
+        var count = 0;
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i] == false) {
+                count++;
+                for (int j = 2; i * j < n; j++) {
+                    notPrime[i * j] = true;
+                }
+            }
+        }
+        return count;
     }
 }
