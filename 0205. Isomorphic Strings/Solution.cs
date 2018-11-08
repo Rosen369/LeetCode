12d64df5d6c79fea7486a03a3e06ae158a71611f
rosen369 <rosen369@outlook.com>
2018-11-08 11:23:50 +0800

    hash map solution

--- 0205. Isomorphic Strings/Solution.cs
+++ 0205. Isomorphic Strings/Solution.cs
@@ -1,5 +1,25 @@
 public class Solution {
     public bool IsIsomorphic (string s, string t) {
+        var weightS = Weight (s);
+        var weightT = Weight (t);
+        for (int i = 0; i < s.Length; i++) {
+            if (weightS[i] != weightT[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    public int[] Weight (string s) {
+        var res = new int[s.Length];
+        var dict = new Dictionary<char, int> ();
+        var weight = 0;
+        for (int i = 0; i < s.Length; i++) {
+            if (!dict.ContainsKey (s[i])) {
+                dict.Add (s[i], weight++);
+            }
+            res[i] = dict[s[i]];
+        }
+        return res;
     }
 }
