948446eb11ea7fe45ca54748a965c586db98e9a6
rosen369 <rosen369@outlook.com>
2018-11-09 16:09:41 +0800

    dictionary solution

--- 0208. Implement Trie (Prefix Tree)/Solution.cs
+++ 0208. Implement Trie (Prefix Tree)/Solution.cs
@@ -2,22 +2,49 @@ public class Trie {
 
     /** Initialize your data structure here. */
     public Trie () {
-
+        _children = new Dictionary<char, Trie> ();
     }
+
+    private IDictionary<char, Trie> _children;
+
+    private bool _exist = false;
 
     /** Inserts a word into the trie. */
     public void Insert (string word) {
-
+        if (string.IsNullOrEmpty (word)) {
+            this._exist = true;
+            return;
+        }
+        if (_children.ContainsKey (word[0])) {
+            _children[word[0]].Insert (word.Substring (1));
+        } else {
+            var child = new Trie ();
+            child.Insert (word.Substring (1));
+            _children.Add (word[0], child);
+        }
     }
 
     /** Returns if the word is in the trie. */
     public bool Search (string word) {
-
+        if (string.IsNullOrEmpty (word)) {
+            return this._exist;
+        }
+        if (!_children.ContainsKey (word[0])) {
+            return false;
+        } else {
+            return _children[word[0]].Search (word.Substring (1));
+        }
     }
 
     /** Returns if there is any word in the trie that starts with the given prefix. */
     public bool StartsWith (string prefix) {
-
+        if (string.IsNullOrEmpty (prefix)) {
+            return true;
+        }
+        if (!_children.ContainsKey (prefix[0])) {
+            return false;
+        }
+        return _children[prefix[0]].StartsWith (prefix.Substring (1));
     }
 }
 
