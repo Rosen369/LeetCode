f877298850087927a99eeb4215bf031b71b8d9b0
rosen369 <rosen369@outlook.com>
2018-11-12 11:33:47 +0800

    dfs solution

---
+++ 0210. Course Schedule II/Solution.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    public int[] FindOrder (int numCourses, int[, ] prerequisites) {
+        var preLength = prerequisites.GetLength (0);
+        var dict = new Dictionary<int, IList<int>> ();
+        for (int i = 0; i < preLength; i++) {
+            if (!dict.ContainsKey (prerequisites[i, 0])) {
+                dict[prerequisites[i, 0]] = new List<int> ();
+            }
+            dict[prerequisites[i, 0]].Add (prerequisites[i, 1]);
+        }
+        var visited = new HashSet<int> ();
+        for (int i = 0; i < numCourses; i++) {
+            if (!DFS (dict, visited, new HashSet<int> (), i)) {
+                return new int[0];
+            }
+        }
+        var list = visited.ToList ();
+        return list.ToArray ();
+    }
+
+    public bool DFS (IDictionary<int, IList<int>> dict, HashSet<int> visited, HashSet<int> pre, int curr) {
+        if (visited.Contains (curr)) {
+            return true;
+        }
+        if (pre.Contains (curr)) {
+            return false;
+        }
+        if (dict.ContainsKey (curr)) {
+            var req = dict[curr];
+            for (int i = 0; i < req.Count (); i++) {
+                pre.Add (curr);
+                if (!DFS (dict, visited, pre, req[i])) {
+                    return false;
+                }
+                pre.Remove (curr);
+            }
+        }
+        visited.Add (curr);
+        return true;
+    }
+}
