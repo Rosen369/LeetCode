1c720bb5155b370eaaf9e6e7df177b3eb85c3696
rosen369 <rosen369@outlook.com>
2018-11-14 10:55:05 +0800

    time limit exceeded

--- 0214. Shortest Palindrome/Solution.cs
+++ 0214. Shortest Palindrome/Solution.cs
@@ -1,5 +1,18 @@
 public class Solution {
     public string ShortestPalindrome (string s) {
-
+        if (string.IsNullOrEmpty (s)) {
+            return s;
+        }
+        var revArr = s.ToCharArray ();
+        Array.Reverse (revArr);
+        var rev = new string (revArr);
+        for (int i = 1; i < s.Length; i++) {
+            var leftSub = rev.Substring (i);
+            var rightSub = s.Substring (0, s.Length - i);
+            if (leftSub == rightSub) {
+                return rev.Substring (0, i) + s;
+            }
+        }
+        return string.Empty;
     }
 }
