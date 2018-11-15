f1d7bb8938ff99b80f21839d343df029cf709bb1
rosen369 <rosen369@outlook.com>
2018-11-15 14:37:33 +0800

    kmp loopup table solution

--- 0214. Shortest Palindrome/Solution.cs
+++ 0214. Shortest Palindrome/Solution.cs
@@ -1,18 +1,20 @@
 public class Solution {
     public string ShortestPalindrome (string s) {
-        if (string.IsNullOrEmpty (s)) {
-            return s;
-        }
         var revArr = s.ToCharArray ();
         Array.Reverse (revArr);
         var rev = new string (revArr);
-        for (int i = 1; i < s.Length; i++) {
-            var leftSub = rev.Substring (i);
-            var rightSub = s.Substring (0, s.Length - i);
-            if (leftSub == rightSub) {
-                return rev.Substring (0, i) + s;
+        var p = s + "#" + rev;
+        var f = new int[p.Length];
+        for (int i = 1; i < p.Length; i++) {
+            var t = f[i - 1];
+            while (t > 0 && p[i] != p[t]) {
+                t = f[t - 1];
             }
+            if (p[i] == p[t]) {
+                ++t;
+            }
+            f[i] = t;
         }
-        return string.Empty;
+        return rev.Substring (0, s.Length - f[p.Length - 1]) + s;
     }
 }
