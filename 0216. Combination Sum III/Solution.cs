4f529569aa9ecea828c29bede37e85822872fa6b
rosen369 <rosen369@outlook.com>
2018-11-16 16:57:21 +0800

    backtracking solution

--- 0216. Combination Sum III/Solution.cs
+++ 0216. Combination Sum III/Solution.cs
@@ -1,5 +1,22 @@
 public class Solution {
     public IList<IList<int>> CombinationSum3 (int k, int n) {
+        var res = new List<IList<int>> ();
+        BackTracking (res, new List<int> (), 1, k, n, 0);
+        return res;
+    }
 
+    public void BackTracking (IList<IList<int>> res, IList<int> current, int num, int k, int n, int sum) {
+        if (current.Count () > k) {
+            return;
+        }
+        if (sum == n && current.Count () == k) {
+            res.Add (new List<int> (current));
+            return;
+        }
+        for (int i = num; i <= 9; i++) {
+            current.Add (i);
+            BackTracking (res, current, i + 1, k, n, sum + i);
+            current.RemoveAt (current.Count () - 1);
+        }
     }
 }
