dad1047c627b7175898dbed47247561eddaf92e9
rosen369 <rosen369@outlook.com>
2018-11-20 16:06:54 +0800

    dict solution

--- 0219. Contains Duplicate II/Solution.cs
+++ 0219. Contains Duplicate II/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public bool ContainsNearbyDuplicate (int[] nums, int k) {
-
+        var dict = new Dictionary<int, int> ();
+        for (int i = 0; i < nums.Length; i++) {
+            if (dict.ContainsKey (nums[i])) {
+                if (i - dict[nums[i]] <= k) {
+                    return true;
+                } else {
+                    dict[nums[i]] = i;
+                }
+            } else {
+                dict.Add (nums[i], i);
+            }
+        }
+        return false;
     }
 }
