9f39831137ca6e08ef120acab728a26d942576fb
rosen369 <rosen369@outlook.com>
2018-11-23 16:45:56 +0800

    empty last question answer

--- 0223. Rectangle Area/Solution.cs
+++ 0223. Rectangle Area/Solution.cs
@@ -1,28 +1,5 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int x) { val = x; }
- * }
- */
 public class Solution {
-    public int CountNodes (TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        var left = root;
-        var right = root;
-        var level = 0;
-        while (right != null) {
-            level++;
-            right = right.right;
-            left = left.left;
-        }
-        if (left == null) {
-            return (int) Math.Pow (2, level) - 1;
-        }
-        return 1 + CountNodes (root.left) + CountNodes (root.right);
+    public int ComputeArea (int A, int B, int C, int D, int E, int F, int G, int H) {
+
     }
 }
