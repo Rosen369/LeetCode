6fd6f0072f14254e1a1c7bbd13e34224dfa7cff4
rosen369 <rosen369@outlook.com>
2018-11-26 17:23:04 +0800

    observation solution

--- 0223. Rectangle Area/Solution.cs
+++ 0223. Rectangle Area/Solution.cs
@@ -1,5 +1,9 @@
 public class Solution {
     public int ComputeArea (int A, int B, int C, int D, int E, int F, int G, int H) {
-
+        var left = Math.Max (A, E);
+        var right = Math.Max (Math.Min (C, G), left);
+        var bottom = Math.Max (B, F);
+        var top = Math.Max (Math.Min (D, H), bottom);
+        return (C - A) * (D - B) - (right - left) * (top - bottom) + (G - E) * (H - F);
     }
 }
