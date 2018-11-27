d5967836a26c902b5e28c0fec80b7e4185eb3bf1
rosen369 <rosen369@outlook.com>
2018-11-27 15:32:04 +0800

    better stack solution

--- 0224. Basic Calculator/Solution.cs
+++ 0224. Basic Calculator/Solution.cs
@@ -1,57 +1,34 @@
 public class Solution {
     public int Calculate (string s) {
-        if (string.IsNullOrEmpty (s)) {
-            return 0;
-        }
-        var stack = new Stack<char> ();
+        var stack = new Stack<int> ();
+        var result = 0;
+        var number = 0;
+        var sign = 1;
         for (int i = 0; i < s.Length; i++) {
-            if (s[i] == ' ') {
-                continue;
-            }
-            if (s[i] == ')') {
-                var cList = new List<char> ();
-                while (stack.Peek () != '(') {
-                    cList.Add (stack.Pop ());
-                }
-                stack.Pop ();
-                var numStr = CalcList (cList).ToString ();
-                for (int j = 0; j < numStr.Length; j++) {
-                    stack.Push (numStr[j]);
-                }
-            } else {
-                stack.Push (s[i]);
-            }
-        }
-        var sList = stack.ToList ();
-        return CalcList (sList);
-    }
-
-    public int CalcList (IList<char> list) {
-        var res = 0;
-        if (list.Count () == 0) {
-            return res;
-        }
-        list = list.Reverse ().ToList ();
-        list.Add ('+');
-        var numStr = string.Empty;
-        var opt = '+';
-        for (int i = 0; i < list.Count (); i++) {
-            if (list[i] == '-' || list[i] == '+') {
-                if (string.IsNullOrEmpty (numStr)) {
-                    numStr += list[i];
-                    continue;
-                }
-                if (opt == '-') {
-                    res -= Convert.ToInt32 (numStr);
-                } else {
-                    res += Convert.ToInt32 (numStr);
-                }
-                opt = list[i];
-                numStr = string.Empty;
-            } else {
-                numStr += list[i];
+            var c = s[i];
+            if (char.IsDigit (c)) {
+                number = 10 * number + (int) (c - '0');
+            } else if (c == '+') {
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            } else if (c == '-') {
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            } else if (c == '(') {
+                stack.Push (result);
+                stack.Push (sign);
+                sign = 1;
+                result = 0;
+            } else if (c == ')') {
+                result += sign * number;
+                number = 0;
+                result *= stack.Pop ();
+                result += stack.Pop ();
             }
         }
-        return res;
+        result += sign * number;
+        return result;
     }
 }
