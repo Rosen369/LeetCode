d4a5b10b69bf2b0cca2b84866a5bac228d7c68b7
rosen369 <rosen369@outlook.com>
2018-11-27 16:17:06 +0800

    one queue solution

--- 0225. Implement Stack using Queues/Solution.cs
+++ 0225. Implement Stack using Queues/Solution.cs
@@ -2,27 +2,33 @@ public class MyStack {
 
     /** Initialize your data structure here. */
     public MyStack () {
-
+        _queue = new Queue<int> ();
     }
+
+    private Queue<int> _queue;
 
     /** Push element x onto stack. */
     public void Push (int x) {
-
+        _queue.Enqueue (x);
+        for (int i = 0; i < _queue.Count - 1; i++) {
+            var n = _queue.Dequeue ();
+            _queue.Enqueue (n);
+        }
     }
 
     /** Removes the element on top of the stack and returns that element. */
     public int Pop () {
-
+        return _queue.Dequeue ();
     }
 
     /** Get the top element. */
     public int Top () {
-
+        return _queue.Peek ();
     }
 
     /** Returns whether the stack is empty. */
     public bool Empty () {
-
+        return _queue.Count == 0;
     }
 }
 
