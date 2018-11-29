af005f93d9ec409a380d9dda431dd6fca7709b27
rosen369 <rosen369@outlook.com>
2018-11-29 15:46:25 +0800

    in order solution

--- 0230. Kth Smallest Element in a BST/Solution.cs
+++ 0230. Kth Smallest Element in a BST/Solution.cs
@@ -9,6 +9,24 @@
  */
 public class Solution {
     public int KthSmallest (TreeNode root, int k) {
+        return InOrder (root, ref k);
+    }
 
+    public int InOrder (TreeNode root, ref int k) {
+        if (root == null) {
+            return -1;
+        }
+        var left = InOrder (root.left, ref k);
+        if (left != -1) {
+            return left;
+        }
+        if (--k == 0) {
+            return root.val;
+        }
+        var right = InOrder (root.right, ref k);
+        if (right != -1) {
+            return right;
+        }
+        return -1;
     }
 }
