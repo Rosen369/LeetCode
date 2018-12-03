2e16f51179c2d8e283764a1b412dd2f30405d8f1
rosen369 <rosen369@outlook.com>
2018-12-03 10:46:41 +0800

    two stacks solution

--- 0232. Implement Queue using Stacks/Solution.cs
+++ 0232. Implement Queue using Stacks/Solution.cs
@@ -2,27 +2,37 @@ public class MyQueue {
 
     /** Initialize your data structure here. */
     public MyQueue () {
-
+        _inStack = new Stack<int> ();
+        _outStack = new Stack<int> ();
     }
+
+    private Stack<int> _inStack;
+    private Stack<int> _outStack;
 
     /** Push element x to the back of queue. */
     public void Push (int x) {
-
+        _inStack.Push (x);
     }
 
     /** Removes the element from in front of queue and returns that element. */
     public int Pop () {
-
+        this.Peek ();
+        return _outStack.Pop ();
     }
 
     /** Get the front element. */
     public int Peek () {
-
+        if (_outStack.Count == 0) {
+            while (_inStack.Count != 0) {
+                _outStack.Push (_inStack.Pop ());
+            }
+        }
+        return _outStack.Peek ();
     }
 
     /** Returns whether the queue is empty. */
     public bool Empty () {
-
+        return _inStack.Count + _outStack.Count == 0;
     }
 }
 
