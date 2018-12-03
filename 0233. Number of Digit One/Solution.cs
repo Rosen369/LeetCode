a72f776e33bbb90c66ea7ba2c0c561c71a1e0f27
rosen369 <rosen369@outlook.com>
2018-12-03 11:24:25 +0800

    math solution

--- 0233. Number of Digit One/Solution.cs
+++ 0233. Number of Digit One/Solution.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public int CountDigitOne (int n) {
-
+        int ones = 0;
+        for (long m = 1; m <= n; m *= 10) {
+            var a = n / m;
+            var b = n % m;
+            ones += (int) ((a + 8) / 10 * m);
+            if (a % 10 == 1) {
+                ones += (int) b + 1;
+            }
+        }
+        return ones;
     }
 }
