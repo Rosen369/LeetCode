18399a6bc31073181e602b722297a8787019f2be
rosen369 <rosen369@outlook.com>
2018-12-04 14:42:37 +0800

    add lowest common ancestor of a binary search tree

--- 0235. Lowest Common Ancestor of a Binary Search Tree/Solution.cs
+++ 0235. Lowest Common Ancestor of a Binary Search Tree/Solution.cs
@@ -1,43 +1,14 @@
 /**
- * Definition for singly-linked list.
- * public class ListNode {
+ * Definition for a binary tree node.
+ * public class TreeNode {
  *     public int val;
- *     public ListNode next;
- *     public ListNode(int x) { val = x; }
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
  * }
  */
 public class Solution {
-    public bool IsPalindrome (ListNode head) {
-        if (head == null || head.next == null) {
-            return true;
-        }
-        var slow = head;
-        var fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        slow = slow.next;
-        var end = this.Reverse (slow);
-        var start = head;
-        while (end != null) {
-            if (start.val != end.val) {
-                return false;
-            }
-            start = start.next;
-            end = end.next;
-        }
-        return true;
-    }
+    public TreeNode LowestCommonAncestor (TreeNode root, TreeNode p, TreeNode q) {
 
-    private ListNode Reverse (ListNode head) {
-        ListNode res = null;
-        while (head != null) {
-            var curr = head;
-            head = head.next;
-            curr.next = res;
-            res = curr;
-        }
-        return res;
     }
 }
