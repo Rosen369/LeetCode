ad8f328a4ad55de02e1b70623f82529ae95434fb
rosen369 <rosen369@outlook.com>
2018-12-04 14:21:55 +0800

    reverse half list solution

--- 0234. Palindrome Linked List/Solution.cs
+++ 0234. Palindrome Linked List/Solution.cs
@@ -8,6 +8,36 @@
  */
 public class Solution {
     public bool IsPalindrome (ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        var slow = head;
+        var fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow = slow.next;
+        var end = this.Reverse (slow);
+        var start = head;
+        while (end != null) {
+            if (start.val != end.val) {
+                return false;
+            }
+            start = start.next;
+            end = end.next;
+        }
+        return true;
+    }
 
+    private ListNode Reverse (ListNode head) {
+        ListNode res = null;
+        while (head != null) {
+            var curr = head;
+            head = head.next;
+            curr.next = res;
+            res = curr;
+        }
+        return res;
     }
 }
