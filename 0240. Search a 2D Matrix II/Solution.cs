a780c5fdb8b4f483e4afd2b0ff5f481965e262fa
rosen369 <rosen369@outlook.com>
2018-12-14 13:36:13 +0800

    O m+n solution

--- 0240. Search a 2D Matrix II/Solution.cs
+++ 0240. Search a 2D Matrix II/Solution.cs
@@ -1,5 +1,21 @@
 public class Solution {
     public bool SearchMatrix (int[, ] matrix, int target) {
-
+        if (matrix == null) {
+            return false;
+        }
+        var row = 0;
+        var col = matrix.GetLength (1) - 1;
+        var rowEnd = matrix.GetLength (0) - 1;
+        var colEnd = 0;
+        while (row <= rowEnd && col >= colEnd) {
+            if (matrix[row, col] == target) {
+                return true;
+            } else if (matrix[row, col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
     }
 }
