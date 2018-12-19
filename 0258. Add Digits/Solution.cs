e0234ee112b4f9dc6494a854bed02712d258a7ee
rosen369 <rosen369@outlook.com>
2018-12-19 17:18:53 +0800

    recursive solution

--- 0258. Add Digits/Solution.cs
+++ 0258. Add Digits/Solution.cs
@@ -1,5 +1,13 @@
 public class Solution {
     public int AddDigits (int num) {
-
+        if (num < 10) {
+            return num;
+        }
+        var str = num.ToString ();
+        var res = 0;
+        for (int i = 0; i < str.Length; i++) {
+            res += str[i] - '0';
+        }
+        return AddDigits (res);
     }
 }
