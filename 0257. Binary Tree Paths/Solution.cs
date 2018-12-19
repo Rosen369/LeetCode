1f9f43064f9cab81bdb29e4bde66ac45446e0e0a
rosen369 <rosen369@outlook.com>
2018-12-19 17:00:01 +0800

    recursive solution

--- 0257. Binary Tree Paths/Solution.cs
+++ 0257. Binary Tree Paths/Solution.cs
@@ -9,6 +9,21 @@
  */
 public class Solution {
     public IList<string> BinaryTreePaths (TreeNode root) {
-
+        var res = new List<string> ();
+        if (root == null) {
+            return res;
+        }
+        var left = BinaryTreePaths (root.left);
+        var right = BinaryTreePaths (root.right);
+        foreach (var leftPath in left) {
+            res.Add (root.val + "->" + leftPath);
+        }
+        foreach (var rightPath in right) {
+            res.Add (root.val + "->" + rightPath);
+        }
+        if (res.Count == 0) {
+            res.Add (root.val.ToString ());
+        }
+        return res;
     }
 }
