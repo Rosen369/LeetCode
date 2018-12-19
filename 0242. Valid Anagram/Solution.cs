fc921bce81721b95cbabf845cb6a283a5736332f
rosen369 <rosen369@outlook.com>
2018-12-19 16:31:08 +0800

    count solution

--- 0242. Valid Anagram/Solution.cs
+++ 0242. Valid Anagram/Solution.cs
@@ -1,5 +1,18 @@
 public class Solution {
     public bool IsAnagram (string s, string t) {
-
+        if (s.Length != t.Length) {
+            return false;
+        }
+        var store = new int[26];
+        for (int i = 0; i < s.Length; i++) {
+            store[s[i] - 'a']++;
+            store[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (store[i] != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
