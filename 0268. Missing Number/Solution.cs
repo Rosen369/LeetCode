e9b321664bbc2df411e3f8fa2251e72c258b5d0a
rosen369 <rosen369@outlook.com>
2018-12-20 16:26:15 +0800

    math solution

--- 0268. Missing Number/Solution.cs
+++ 0268. Missing Number/Solution.cs
@@ -1,5 +1,9 @@
 public class Solution {
     public int MissingNumber (int[] nums) {
-
+        var sum = nums.Length * (nums.Length + 1) / 2;
+        for (int i = 0; i < nums.Length; i++) {
+            sum -= nums[i];
+        }
+        return sum;
     }
 }
