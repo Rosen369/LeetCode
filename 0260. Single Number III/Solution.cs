8d75d223d1a12a8d2d18c3addb08348b2d6029be
rosen369 <rosen369@outlook.com>
2018-12-20 10:48:20 +0800

    bit operation solution

--- 0260. Single Number III/Solution.cs
+++ 0260. Single Number III/Solution.cs
@@ -1,5 +1,18 @@
 public class Solution {
     public int[] SingleNumber (int[] nums) {
-
+        var res = new int[2];
+        var xor = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            xor ^= nums[i];
+        }
+        var pos = xor & -xor;
+        for (int i = 0; i < nums.Length; i++) {
+            if ((nums[i] & pos) == 0) {
+                res[0] ^= nums[i];
+            } else {
+                res[1] ^= nums[i];
+            }
+        }
+        return res;
     }
 }
