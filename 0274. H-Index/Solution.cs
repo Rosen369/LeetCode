aac209216baaa45b096700ae0d052c622bd1497f
rosen369 <rosen369@outlook.com>
2018-12-24 10:58:00 +0800

    sort solution

--- 0274. H-Index/Solution.cs
+++ 0274. H-Index/Solution.cs
@@ -1,5 +1,15 @@
 public class Solution {
     public int HIndex (int[] citations) {
-
+        var res = 0;
+        Array.Sort (citations);
+        Array.Reverse (citations);
+        for (int i = 0; i < citations.Length; i++) {
+            if (citations[i] >= i + 1) {
+                res = i + 1;
+            } else {
+                break;
+            }
+        }
+        return res;
     }
 }
