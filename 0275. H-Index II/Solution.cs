e64196daf2ff40c009e7694a0e8228f8e608febb
rosen369 <rosen369@outlook.com>
2018-12-26 13:40:01 +0800

    binary search solution

--- 0275. H-Index II/Solution.cs
+++ 0275. H-Index II/Solution.cs
@@ -1,5 +1,18 @@
 public class Solution {
     public int HIndex (int[] citations) {
-
+        var len = citations.Length;
+        var left = 0;
+        var right = len - 1;
+        while (left <= right) {
+            var mid = (left + right) / 2;
+            if (citations[mid] == len - mid) {
+                return citations[mid];
+            } else if (citations[mid] > len - mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return len - (right + 1);
     }
 }
