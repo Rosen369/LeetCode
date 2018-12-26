e5e48a127c995f6306c56c15008378226d3136e7
rosen369 <rosen369@outlook.com>
2018-12-26 16:55:42 +0800

    binary search solution

--- 0278. First Bad Version/Solution.cs
+++ 0278. First Bad Version/Solution.cs
@@ -3,6 +3,16 @@
 
 public class Solution : VersionControl {
     public int FirstBadVersion (int n) {
-
+        var left = 1;
+        var right = n;
+        while (left < right) {
+            var mid = left + (right - left) / 2;
+            if (!this.IsBadVersion (mid)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
     }
 }
