5fd01427cce77898cfe57f940132ebb057b53d75
rosen369 <rosen369@outlook.com>
2018-12-27 13:55:45 +0800

    dp solution

--- 0279. Perfect Squares/Solution.cs
+++ 0279. Perfect Squares/Solution.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public int NumSquares (int n) {
-
+        if (n <= 0) {
+            return 0;
+        }
+        var dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.Min (dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
     }
 }
