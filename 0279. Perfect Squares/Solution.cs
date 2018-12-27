6b5f0e0c3a530db4c15d110ff6854d4e143c69a1
rosen369 <rosen369@outlook.com>
2018-12-27 14:02:11 +0800

    amend initial dp value

--- 0279. Perfect Squares/Solution.cs
+++ 0279. Perfect Squares/Solution.cs
@@ -4,6 +4,8 @@ public class Solution {
             return 0;
         }
         var dp = new int[n + 1];
+        Array.Fill (dp, int.MaxValue);
+        dp[0] = 0;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j * j <= i; j++) {
                 dp[i] = Math.Min (dp[i], dp[i - j * j] + 1);
