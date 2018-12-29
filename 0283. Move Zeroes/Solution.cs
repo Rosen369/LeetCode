e6433fa96951e0c022dcdd9cc7ecf873ab6e0f96
rosen369 <rosen369@outlook.com>
2018-12-29 11:36:31 +0800

    swap zero solution

--- 0283. Move Zeroes/Solution.cs
+++ 0283. Move Zeroes/Solution.cs
@@ -1,5 +1,15 @@
 public class Solution {
     public void MoveZeroes (int[] nums) {
-
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] == 0) {
+                for (int j = i + 1; j < nums.Length; j++) {
+                    if (nums[j] != 0) {
+                        nums[i] = nums[j];
+                        nums[j] = 0;
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
