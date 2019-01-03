e76b3c79bf80e231328a0cc263dbe0f9958e04c7
rosen369 <rosen369@outlook.com>
2019-01-03 14:34:53 +0800

    two pointers solution

--- 0287. Find the Duplicate Number/Solution.cs
+++ 0287. Find the Duplicate Number/Solution.cs
@@ -1,5 +1,19 @@
 public class Solution {
     public int FindDuplicate (int[] nums) {
-
+        if (nums.Length == 0) {
+            return -1;
+        }
+        var slow = nums[0];
+        var fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        fast = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
 }
