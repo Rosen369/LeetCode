1cf120f1b940a1d204c3a9529a732d06de2c9fe5
rosen369 <rosen369@outlook.com>
2019-01-03 17:03:53 +0800

    hash set solution

--- 0290. Word Pattern/Solution.cs
+++ 0290. Word Pattern/Solution.cs
@@ -1,5 +1,25 @@
 public class Solution {
     public bool WordPattern (string pattern, string str) {
-
+        var set = new HashSet<string> ();
+        var dict = new Dictionary<char, string> ();
+        var arr = str.Split (' ');
+        if (pattern.Length != arr.Length) {
+            return false;
+        }
+        for (int i = 0; i < pattern.Length; i++) {
+            if (dict.ContainsKey (pattern[i])) {
+                if (arr[i] != dict[pattern[i]]) {
+                    return false;
+                }
+            } else {
+                if (set.Contains (arr[i])) {
+                    return false;
+                } else {
+                    dict.Add (pattern[i], arr[i]);
+                    set.Add (arr[i]);
+                }
+            }
+        }
+        return true;
     }
 }
