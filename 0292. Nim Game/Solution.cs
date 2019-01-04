8b4e1e1860ab965434775e1ed8a22f01a6f5884b
rosen369 <rosen369@outlook.com>
2019-01-04 09:34:34 +0800

    time limit exceeded

--- 0292. Nim Game/Solution.cs
+++ 0292. Nim Game/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public bool CanWinNim (int n) {
-
+        if (n <= 3) {
+            return true;
+        }
+        if (!this.CanWinNim (n - 1)) {
+            return true;
+        }
+        if (!this.CanWinNim (n - 2)) {
+            return true;
+        }
+        if (!this.CanWinNim (n - 3)) {
+            return true;
+        }
+        return false;
     }
 }
