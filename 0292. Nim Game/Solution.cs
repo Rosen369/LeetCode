aaef226af6e1b56b205a1ffd10ebe7cbaad69b94
rosen369 <rosen369@outlook.com>
2019-01-04 11:02:33 +0800

    math solution

--- 0292. Nim Game/Solution.cs
+++ 0292. Nim Game/Solution.cs
@@ -1,17 +1,5 @@
 public class Solution {
     public bool CanWinNim (int n) {
-        if (n <= 3) {
-            return true;
-        }
-        if (!this.CanWinNim (n - 1)) {
-            return true;
-        }
-        if (!this.CanWinNim (n - 2)) {
-            return true;
-        }
-        if (!this.CanWinNim (n - 3)) {
-            return true;
-        }
-        return false;
+        return n % 4 != 0;
     }
 }
