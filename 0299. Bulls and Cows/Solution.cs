4e8091f4c26ce4e5b5b399d6af3a728396c55a78
rosen369 <rosen369@outlook.com>
2019-01-08 15:08:37 +0800

    counting solution

--- 0299. Bulls and Cows/Solution.cs
+++ 0299. Bulls and Cows/Solution.cs
@@ -1,5 +1,21 @@
 public class Solution {
     public string GetHint (string secret, string guess) {
-
+        var countS = new int[10];
+        var countG = new int[10];
+        var bulls = 0;
+        var cows = 0;
+        for (int i = 0; i < secret.Length; i++) {
+            countS[secret[i] - '0']++;
+            countG[guess[i] - '0']++;
+            if (secret[i] == guess[i]) {
+                bulls++;
+            }
+        }
+        var same = 0;
+        for (int i = 0; i < 10; i++) {
+            same += Math.Min (countS[i], countG[i]);
+        }
+        cows = same - bulls;
+        return string.Format ("{0}A{1}B", bulls, cows);
     }
 }
