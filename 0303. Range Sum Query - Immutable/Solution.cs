79c52dc23f35fe0657a65ad67468200b969cbbba
rosen369 <rosen369@outlook.com>
2019-01-09 17:03:42 +0800

    cache solution

--- 0303. Range Sum Query - Immutable/Solution.cs
+++ 0303. Range Sum Query - Immutable/Solution.cs
@@ -1,11 +1,27 @@
 public class NumArray {
 
     public NumArray (int[] nums) {
-
+        this._sum = new int[nums.Length];
+        if (nums.Length > 0) {
+            this._sum[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++) {
+                this._sum[i] = this._sum[i - 1] + nums[i];
+            }
+        }
     }
 
-    public int SumRange (int i, int j) {
+    private int[] _sum;
 
+    public int SumRange (int i, int j) {
+        if (i > j) {
+            return 0;
+        }
+        if (i == 0) {
+            return this._sum[j];
+        } else {
+            var res = this._sum[j] - this._sum[i - 1];
+            return res;
+        }
     }
 }
 
