c4d4b8871701ac23d025d8836c7e5c0315ece9d9
rosen369 <rosen369@outlook.com>
2019-01-11 13:45:46 +0800

    cache solution

--- 0307. Range Sum Query - Mutable/Solution.cs
+++ 0307. Range Sum Query - Mutable/Solution.cs
@@ -1,15 +1,30 @@
 public class NumArray {
 
     public NumArray (int[] nums) {
-
+        this._sum = new int[nums.Length];
+        if (nums.Length > 0) {
+            this._sum[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++) {
+                this._sum[i] = this._sum[i - 1] + nums[i];
+            }
+        }
     }
 
-    public void Update (int i, int val) {
+    private int[] _sum;
 
+    public void Update (int i, int val) {
+        var diff = i == 0 ? val - this._sum[i] : val - this._sum[i] + this._sum[i - 1];
+        for (int j = i; j < this._sum.Length; j++) {
+            this._sum[j] += diff;
+        }
     }
 
     public int SumRange (int i, int j) {
-
+        if (i == 0) {
+            return this._sum[j];
+        } else {
+            return this._sum[j] - this._sum[i - 1];
+        }
     }
 }
 
