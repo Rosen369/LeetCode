a34274ad8f1ab5fd651ae01dab049f4642bf655b
rosen369 <rosen369@outlook.com>
2019-01-24 10:47:18 +0800

    divide and conquer solution

--- 0312. Burst Balloons/Solution.cs
+++ 0312. Burst Balloons/Solution.cs
@@ -1,28 +1,31 @@
 public class Solution {
     public int MaxCoins (int[] nums) {
-        return Backtracking (new List<int> (nums));
+        var len = nums.Length;
+        var expand = new int[len + 2];
+        expand[0] = 1;
+        expand[len + 1] = 1;
+        for (int i = 1; i <= len; i++) {
+            expand[i] = nums[i - 1];
+        }
+        var memo = new int[len + 2, len + 2];
+        return Backtracking (expand, memo, 0, len + 1);
     }
 
-    public int Backtracking (IList<int> nums) {
-        if (nums.Count () == 1) {
-            return nums[0];
+    public int Backtracking (int[] nums, int[, ] memo, int left, int right) {
+        if (left + 1 == right) {
+            return 0;
+        }
+        if (memo[left, right] > 0) {
+            return memo[left, right];
         }
         var max = 0;
-        for (int i = 0; i < nums.Count (); i++) {
-            var left = 1;
-            var right = 1;
-            if (i > 0) {
-                left = nums[i - 1];
-            }
-            if (i < nums.Count () - 1) {
-                right = nums[i + 1];
-            }
-            var curr = left * nums[i] * right;
-            var next = new List<int> (nums);
-            next.RemoveAt (i);
-            curr += Backtracking (next);
+        for (int i = left + 1; i < right; i++) {
+            var lMax = Backtracking (nums, memo, left, i);
+            var rMax = Backtracking (nums, memo, i, right);
+            var curr = nums[left] * nums[i] * nums[right] + lMax + rMax;
             max = Math.Max (max, curr);
         }
+        memo[left, right] = max;
         return max;
     }
 }
