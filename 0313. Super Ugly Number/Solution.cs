855392a019e25ba6307f2aca9ceef71e4593425a
rosen369 <rosen369@outlook.com>
2019-01-24 11:36:31 +0800

    dp solution based on ugly number two

--- 0313. Super Ugly Number/Solution.cs
+++ 0313. Super Ugly Number/Solution.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public int NthSuperUglyNumber (int n, int[] primes) {
-
+        if (n == 1) {
+            return 1;
+        }
+        var dp = new int[n];
+        dp[0] = 1;
+        var times = new int[primes.Length];
+        for (int i = 1; i < n; i++) {
+            dp[i] = int.MaxValue;
+            for (int t = 0; t < times.Length; t++) {
+                dp[i] = Math.Min (dp[i], dp[times[t]] * primes[t]);
+            }
+            for (int t = 0; t < times.Length; t++) {
+                if (dp[i] == dp[times[t]] * primes[t]) times[t]++;
+            }
+        }
+        return dp[n - 1];
     }
 }
