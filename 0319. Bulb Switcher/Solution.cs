6f7fd6a13123b6a05884f78eb4202e6986428bdb
rosen369 <rosen369@outlook.com>
2019-01-30 11:24:00 +0800

    Time Limit Exceeded

--- 0319. Bulb Switcher/Solution.cs
+++ 0319. Bulb Switcher/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public int BulbSwitch (int n) {
-
+        var bulbs = new bool[n];
+        for (int round = 0; round < n; round++) {
+            for (int i = round; i < n; i += round + 1) {
+                bulbs[i] = !bulbs[i];
+            }
+        }
+        var count = 0;
+        for (int i = 0; i < n; i++) {
+            if (bulbs[i]) {
+                count++;
+            }
+        }
+        return count;
     }
 }
