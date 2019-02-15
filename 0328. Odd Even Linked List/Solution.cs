12971d5264c3fa01b9c83e7199bbe5f7ac080ecc
rosen369 <rosen369@outlook.com>
2019-02-15 15:11:48 +0800

    O(n) solution

--- 0328. Odd Even Linked List/Solution.cs
+++ 0328. Odd Even Linked List/Solution.cs
@@ -8,6 +8,33 @@
  */
 public class Solution {
     public ListNode OddEvenList (ListNode head) {
-
+        if (head == null || head.next == null) {
+            return head;
+        }
+        var odd = head;
+        var even = head.next;
+        var isOdd = true;
+        var curr = even.next;
+        odd.next = null;
+        even.next = null;
+        var currOdd = odd;
+        var currEven = even;
+        while (curr != null) {
+            if (isOdd) {
+                currOdd.next = curr;
+                curr = curr.next;
+                currOdd = currOdd.next;
+                currOdd.next = null;
+                isOdd = false;
+            } else {
+                currEven.next = curr;
+                curr = curr.next;
+                currEven = currEven.next;
+                currEven.next = null;
+                isOdd = true;
+            }
+        }
+        currOdd.next = even;
+        return odd;
     }
 }
