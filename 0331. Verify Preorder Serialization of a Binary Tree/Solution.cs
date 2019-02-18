be1fff65d7aac9ccbec0415076c1346453af0e03
rosen369 <rosen369@outlook.com>
2019-02-18 15:46:10 +0800

    stack solution

--- 0331. Verify Preorder Serialization of a Binary Tree/Solution.cs
+++ 0331. Verify Preorder Serialization of a Binary Tree/Solution.cs
@@ -1,5 +1,21 @@
 public class Solution {
     public bool IsValidSerialization (string preorder) {
-
+        var arr = preorder.Split (',');
+        var len = arr.Length;
+        var stack = new Stack<string> ();
+        for (int i = 0; i < len; i++) {
+            if (arr[i] == "#") {
+                while (stack.Count > 1 && stack.Peek () == "#") {
+                    stack.Pop ();
+                    if (stack.Pop () == "#") {
+                        return false;
+                    }
+                }
+                stack.Push ("#");
+            } else {
+                stack.Push (arr[i]);
+            }
+        }
+        return stack.Count == 1 && stack.Pop () == "#";
     }
 }
