9a41d840fff6583d9f2cdf35f44da9e535243d93
rosen369 <rosen369@outlook.com>
2019-02-18 14:45:27 +0800

    greedy solution

--- 0330. Patching Array/Solution.cs
+++ 0330. Patching Array/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public int MinPatches (int[] nums, int n) {
-
+        var pathches = 0;
+        long reaches = 1;
+        var index = 0;
+        while (reaches <= n) {
+            if (index < nums.Length && reaches >= nums[index]) {
+                reaches += nums[index];
+                index++;
+            } else {
+                reaches += reaches;
+                pathches++;
+            }
+        }
+        return pathches;
     }
 }
