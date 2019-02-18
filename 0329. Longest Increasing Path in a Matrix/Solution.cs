ca2e6bc432deabc46bb04f159642d3bf08258059
rosen369 <rosen369@outlook.com>
2019-02-18 11:52:32 +0800

    dp recursive solution

--- 0329. Longest Increasing Path in a Matrix/Solution.cs
+++ 0329. Longest Increasing Path in a Matrix/Solution.cs
@@ -1,5 +1,35 @@
 public class Solution {
     public int LongestIncreasingPath (int[, ] matrix) {
+        var m = matrix.GetLength (0);
+        var n = matrix.GetLength (1);
+        var max = 0;
+        var dp = new int[m, n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                max = Math.Max (max, Recursive (matrix, i, j, m, n, dp));
+            }
+        }
+        return max;
+    }
 
+    public int Recursive (int[, ] matrix, int i, int j, int m, int n, int[, ] dp) {
+        if (dp[i, j] != 0) {
+            return dp[i, j];
+        }
+        var max = 1;
+        if (i - 1 >= 0 && matrix[i - 1, j] > matrix[i, j]) {
+            max = Math.Max (max, 1 + Recursive (matrix, i - 1, j, m, n, dp));
+        }
+        if (i + 1 < m && matrix[i + 1, j] > matrix[i, j]) {
+            max = Math.Max (max, 1 + Recursive (matrix, i + 1, j, m, n, dp));
+        }
+        if (j - 1 >= 0 && matrix[i, j - 1] > matrix[i, j]) {
+            max = Math.Max (max, 1 + Recursive (matrix, i, j - 1, m, n, dp));
+        }
+        if (j + 1 < n && matrix[i, j + 1] > matrix[i, j]) {
+            max = Math.Max (max, 1 + Recursive (matrix, i, j + 1, m, n, dp));
+        }
+        dp[i, j] = max;
+        return max;
     }
 }
