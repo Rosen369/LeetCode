ed667c832e70291b06748f730eea944eef62022a
rosen369 <rosen369@outlook.com>
2019-02-19 10:48:04 +0800

    O(n) solution

--- 0334. Increasing Triplet Subsequence/Solution.cs
+++ 0334. Increasing Triplet Subsequence/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public bool IncreasingTriplet (int[] nums) {
-
+        var first = int.MaxValue;
+        var second = int.MaxValue;
+        for (int i = 0; i < nums.Length; i++) {
+            var third = nums[i];
+            if (third <= first) {
+                first = third;
+            } else if (third <= second) {
+                second = third;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
