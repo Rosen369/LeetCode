cb0caed0f685d7446c2362b129e341bac84b8f34
rosen369 <rosen369@outlook.com>
2019-02-19 15:02:37 +0800

    draw line solution

--- 0335. Self Crossing/Solution.cs
+++ 0335. Self Crossing/Solution.cs
@@ -1,5 +1,42 @@
 public class Solution {
     public bool IsSelfCrossing (int[] x) {
-
+        if (x.Length < 4) {
+            return false;
+        }
+        var b = x[2];
+        var c = x[1];
+        var d = x[0];
+        var e = 0;
+        var f = 0;
+        for (int i = 3; i < x.Length; i++) {
+            var a = x[i];
+            if (d >= b) {
+                if (a >= c) {
+                    return true;
+                }
+                if (a >= c - e && c - e >= 0 && f >= d - b) {
+                    return true;
+                }
+            }
+            f = e;
+            e = d;
+            d = c;
+            c = b;
+            b = a;
+        }
+        return false;
     }
 }
+/*
+            b                              b
+   +----------------+             +----------------+
+   |                |             |                |
+   |                |             |                | a
+ c |                |           c |                |
+   |                | a           |                |    f
+   +----------->    |             |                | <----+
+            d       |             |                |      | e
+                    |             |                       |
+                                  +-----------------------+
+                                               d
+ */
