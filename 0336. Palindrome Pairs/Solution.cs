f09817cbd4978d5e6717cb6895e465d9967dc109
rosen369 <rosen369@outlook.com>
2019-02-20 11:33:41 +0800

    hash map solution

--- 0336. Palindrome Pairs/Solution.cs
+++ 0336. Palindrome Pairs/Solution.cs
@@ -1,13 +1,28 @@
 public class Solution {
     public IList<IList<int>> PalindromePairs (string[] words) {
         var res = new List<IList<int>> ();
+        if (words == null || words.Length < 2) {
+            return res;
+        }
+        var dict = new Dictionary<string, int> ();
         for (int i = 0; i < words.Length; i++) {
-            for (int j = 0; j < words.Length; j++) {
-                if (i == j) {
-                    continue;
+            dict.Add (words[i], i);
+        }
+        for (int i = 0; i < words.Length; i++) {
+            for (int j = 0; j <= words[i].Length; j++) {
+                var left = words[i].Substring (0, j);
+                var right = words[i].Substring (j);
+                if (IsPalindrome (left)) {
+                    var reverse = Reverse (right);
+                    if (dict.ContainsKey (reverse) && dict[reverse] != i) {
+                        res.Add (new List<int> () { dict[reverse], i });
+                    }
                 }
-                if (IsPalindrome (words[i] + words[j])) {
-                    res.Add (new List<int> () { i, j });
+                if (IsPalindrome (right) && right.Length != 0) {
+                    var reverse = Reverse (left);
+                    if (dict.ContainsKey (reverse) && dict[reverse] != i) {
+                        res.Add (new List<int> () { i, dict[reverse] });
+                    }
                 }
             }
         }
@@ -25,5 +40,11 @@ public class Solution {
             end--;
         }
         return true;
+    }
+
+    public string Reverse (string s) {
+        char[] charArray = s.ToCharArray ();
+        Array.Reverse (charArray);
+        return new string (charArray);
     }
 }
