48b16f7b5f36b50cb64e50a9251207e1a3f3aaa1
rosen369 <rosen369@outlook.com>
2019-02-20 10:15:27 +0800

    O(n^2 * k) solution

--- 0336. Palindrome Pairs/Solution.cs
+++ 0336. Palindrome Pairs/Solution.cs
@@ -1,5 +1,29 @@
 public class Solution {
     public IList<IList<int>> PalindromePairs (string[] words) {
+        var res = new List<IList<int>> ();
+        for (int i = 0; i < words.Length; i++) {
+            for (int j = 0; j < words.Length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (IsPalindrome (words[i] + words[j])) {
+                    res.Add (new List<int> () { i, j });
+                }
+            }
+        }
+        return res;
+    }
 
+    public bool IsPalindrome (string word) {
+        var start = 0;
+        var end = word.Length - 1;
+        while (start < end) {
+            if (word[start] != word[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
     }
 }
