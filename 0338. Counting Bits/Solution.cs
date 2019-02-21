3b285795f2519204c727a8f0ffa01ace6b4802d4
rosen369 <rosen369@outlook.com>
2019-02-21 09:53:07 +0800

    dp solution

--- 0338. Counting Bits/Solution.cs
+++ 0338. Counting Bits/Solution.cs
@@ -1,5 +1,13 @@
 public class Solution {
     public int[] CountBits (int num) {
-
+        var res = new int[num + 1];
+        var offset = 1;
+        for (int i = 1; i <= num; i++) {
+            if (offset * 2 == i) {
+                offset *= 2;
+            }
+            res[i] = res[i - offset] + 1;
+        }
+        return res;
     }
 }
