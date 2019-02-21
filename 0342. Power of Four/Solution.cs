3c6fb717d488d3c03d8e093061d55dc75cf3eda7
rosen369 <rosen369@outlook.com>
2019-02-21 15:18:15 +0800

    math solution

--- 0342. Power of Four/Solution.cs
+++ 0342. Power of Four/Solution.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public bool IsPowerOfFour (int num) {
-
+        if (num <= 0) {
+            return false;
+        }
+        if ((num & (num - 1)) != 0) {
+            return false;
+        }
+        if ((num - 1) % 3 != 0) {
+            return false;
+        }
+        return true;
     }
 }
