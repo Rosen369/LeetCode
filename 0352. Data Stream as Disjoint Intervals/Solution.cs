6f8aab073ce526baff62cd9401d6da0e4829a5d2
rosen369 <rosen369@outlook.com>
2019-02-26 15:03:54 +0800

    sorted list solution

--- 0352. Data Stream as Disjoint Intervals/Solution.cs
+++ 0352. Data Stream as Disjoint Intervals/Solution.cs
@@ -11,15 +11,39 @@ public class SummaryRanges {
 
     /** Initialize your data structure here. */
     public SummaryRanges () {
-
+        this._list = new SortedList ();
     }
 
-    public void AddNum (int val) {
+    private SortedList _list;
 
+    public void AddNum (int val) {
+        if (!this._list.ContainsKey (val)) {
+            this._list.Add (val, new Interval (val, val));
+        }
     }
 
     public IList<Interval> GetIntervals () {
-
+        var res = new List<Interval> ();
+        if (this._list.Count == 0) {
+            return res;
+        }
+        var pre = this._list.GetByIndex (0) as Interval;
+        var index = 1;
+        while (index < this._list.Count) {
+            var curr = this._list.GetByIndex (index) as Interval;
+            if (curr != null) {
+                if (pre.end + 1 == curr.start) {
+                    pre.end = curr.end;
+                    this._list[curr.start] = null;
+                } else {
+                    res.Add (pre);
+                    pre = curr;
+                }
+            }
+            index++;
+        }
+        res.Add (pre);
+        return res;
     }
 }
 
