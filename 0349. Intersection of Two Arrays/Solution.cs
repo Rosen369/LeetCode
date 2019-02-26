4ed5537983a3c6f114df5d2e99c85097d55c7791
rosen369 <rosen369@outlook.com>
2019-02-26 09:14:37 +0800

    two set solution

--- 0349. Intersection of Two Arrays/Solution.cs
+++ 0349. Intersection of Two Arrays/Solution.cs
@@ -1,5 +1,12 @@
 public class Solution {
     public int[] Intersection (int[] nums1, int[] nums2) {
-
+        var set = new HashSet<int> (nums1);
+        var intersect = new HashSet<int> ();
+        for (int i = 0; i < nums2.Length; i++) {
+            if (set.Contains (nums2[i])) {
+                intersect.Add (nums2[i]);
+            }
+        }
+        return intersect.ToArray ();
     }
 }
