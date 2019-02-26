8d4007569c5d439982d994446ffb23fa3a418f8a
rosen369 <rosen369@outlook.com>
2019-02-26 11:22:17 +0800

    add data stream as disjoint intervals

---
+++ 0352. Data Stream as Disjoint Intervals/Solution.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     public int start;
+ *     public int end;
+ *     public Interval() { start = 0; end = 0; }
+ *     public Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class SummaryRanges {
+
+    /** Initialize your data structure here. */
+    public SummaryRanges () {
+
+    }
+
+    public void AddNum (int val) {
+
+    }
+
+    public IList<Interval> GetIntervals () {
+
+    }
+}
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * SummaryRanges obj = new SummaryRanges();
+ * obj.AddNum(val);
+ * IList<Interval> param_2 = obj.GetIntervals();
+ */
