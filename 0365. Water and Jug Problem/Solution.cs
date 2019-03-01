3ee536d069ac8ecbd910f080ab500f88688c9b81
rosen369 <rosen369@outlook.com>
2019-03-01 10:49:57 +0800

    math solution

--- 0365. Water and Jug Problem/Solution.cs
+++ 0365. Water and Jug Problem/Solution.cs
@@ -1,5 +1,26 @@
 public class Solution {
     public bool CanMeasureWater (int x, int y, int z) {
+        if (x + y < z) {
+            return false;
+        }
+        if (x == z || y == z || x + y == z) {
+            return true;
+        }
+        var gcd = Gcd (x, y);
+        if (gcd == -1) {
+            return false;
+        }
+        return z % gcd == 0;
+    }
 
+    private int Gcd (int a, int b) {
+        if (a <= 0 || b <= 0)
+            return -1;
+        else if (a > b)
+            return Gcd (a - b, b);
+        else if (a < b)
+            return Gcd (a, b - a);
+        else
+            return a;
     }
 }
