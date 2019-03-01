4d5b2d18e7cd4ec98cd97fe2d707147ffa0b83c3
rosen369 <rosen369@outlook.com>
2019-03-01 11:21:17 +0800

    binary search solution

--- 0367. Valid Perfect Square/Solution.cs
+++ 0367. Valid Perfect Square/Solution.cs
@@ -1,5 +1,18 @@
 public class Solution {
     public bool IsPerfectSquare (int num) {
-
+        var left = 1;
+        var right = num;
+        while (left <= right) {
+            var mid = (long) (left + right) / 2;
+            var sqrt = mid * mid;
+            if (sqrt == num) {
+                return true;
+            } else if (sqrt > num) {
+                right = (int) mid - 1;
+            } else {
+                left = (int) mid + 1;
+            }
+        }
+        return false;
     }
 }
