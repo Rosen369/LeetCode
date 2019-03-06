945c12f360d7526e096f57ce87bd06f1b2b4c0d3
rosen369 <rosen369@outlook.com>
2019-03-06 14:26:26 +0800

    dp solution O(n) time

--- 0376. Wiggle Subsequence/Solution.cs
+++ 0376. Wiggle Subsequence/Solution.cs
@@ -1,5 +1,25 @@
 public class Solution {
     public int WiggleMaxLength (int[] nums) {
-
+        var len = nums.Length;
+        if (len == 0) {
+            return 0;
+        }
+        var up = new int[len];
+        var down = new int[len];
+        up[0] = 1;
+        down[0] = 1;
+        for (int i = 1; i < len; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up[i] = down[i - 1] + 1;
+                down[i] = down[i - 1];
+            } else if (nums[i] < nums[i - 1]) {
+                down[i] = up[i - 1] + 1;
+                up[i] = up[i - 1];
+            } else {
+                down[i] = down[i - 1];
+                up[i] = up[i - 1];
+            }
+        }
+        return Math.Max (up[len - 1], down[len - 1]);
     }
 }
