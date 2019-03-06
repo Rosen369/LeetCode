b8756ae9a478f78c32019765648a11408e21bf0a
rosen369 <rosen369@outlook.com>
2019-03-06 10:06:31 +0800

    dp solution

--- 0375. Guess Number Higher or Lower II/Solution.cs
+++ 0375. Guess Number Higher or Lower II/Solution.cs
@@ -1,5 +1,22 @@
 public class Solution {
     public int GetMoneyAmount (int n) {
+        var dp = new int[n + 1, n + 1];
+        return Recursive (dp, 1, n);
+    }
 
+    public int Recursive (int[, ] dp, int left, int right) {
+        if (left >= right) {
+            return 0;
+        }
+        if (dp[left, right] != 0) {
+            return dp[left, right];
+        }
+        var res = int.MaxValue;
+        for (int i = left; i <= right; i++) {
+            var next = i + Math.Max (Recursive (dp, left, i - 1), Recursive (dp, i + 1, right));
+            res = Math.Min (res, next);
+        }
+        dp[left, right] = res;
+        return res;
     }
 }
