2ce78afdee2da9af8f6a9d77f2f1d24794ba4ec6
rosen369 <rosen369@outlook.com>
2019-03-08 15:10:04 +0800

    reservoir sampling solution

--- 0382. Linked List Random Node/Solution.cs
+++ 0382. Linked List Random Node/Solution.cs
@@ -11,12 +11,28 @@ public class Solution {
     /** @param head The linked list's head.
         Note that the head is guaranteed to be not null, so it contains at least one node. */
     public Solution (ListNode head) {
-
+        this._random = new Random ();
+        this._head = head;
     }
+
+    private Random _random;
+
+    private ListNode _head;
 
     /** Returns a random node's value. */
     public int GetRandom () {
-
+        var curr = this._head;
+        var reservoir = curr.val;
+        var index = 1;
+        while (curr != null) {
+            var p = this._random.Next (index);
+            if (p == 0) {
+                reservoir = curr.val;
+            }
+            curr = curr.next;
+            index++;
+        }
+        return reservoir;
     }
 }
 
