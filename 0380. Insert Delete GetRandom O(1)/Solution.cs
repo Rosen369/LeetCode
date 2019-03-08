587f113ac05a0d0e0767ccb667d2782eb01d0e6c
rosen369 <rosen369@outlook.com>
2019-03-08 09:59:06 +0800

    hash map solution

--- 0380. Insert Delete GetRandom O(1)/Solution.cs
+++ 0380. Insert Delete GetRandom O(1)/Solution.cs
@@ -2,22 +2,47 @@ public class RandomizedSet {
 
     /** Initialize your data structure here. */
     public RandomizedSet () {
-
+        this._dict = new Dictionary<int, int> ();
+        this._nums = new List<int> ();
+        this._random = new Random ();
     }
+
+    private IDictionary<int, int> _dict;
+
+    private IList<int> _nums;
+
+    private Random _random;
 
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public bool Insert (int val) {
-
+        if (this._dict.ContainsKey (val)) {
+            return false;
+        }
+        this._nums.Add (val);
+        this._dict.Add (val, this._nums.Count - 1);
+        return true;
     }
 
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public bool Remove (int val) {
-
+        if (!this._dict.ContainsKey (val)) {
+            return false;
+        }
+        var len = this._nums.Count;
+        var pos = this._dict[val];
+        var last = this._nums[len - 1];
+        this._nums[pos] = last;
+        this._dict[last] = pos;
+        this._nums.RemoveAt (len - 1);
+        this._dict.Remove (val);
+        return true;
     }
 
     /** Get a random element from the set. */
     public int GetRandom () {
-
+        var count = this._dict.Count;
+        var next = this._random.Next (count);
+        return this._nums[next];
     }
 }
 
