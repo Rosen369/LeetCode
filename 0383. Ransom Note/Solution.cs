771e04a37b3623847e154854fb8b5ae5a7f8c1b4
rosen369 <rosen369@outlook.com>
2019-03-11 16:07:44 +0800

    cache solution

--- 0383. Ransom Note/Solution.cs
+++ 0383. Ransom Note/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public bool CanConstruct (string ransomNote, string magazine) {
-
+        var store = new int[26];
+        for (int i = 0; i < magazine.Length; i++) {
+            var pos = magazine[i] - 'a';
+            store[pos]++;
+        }
+        for (int i = 0; i < ransomNote.Length; i++) {
+            var pos = ransomNote[i] - 'a';
+            if (store[pos] == 0) {
+                return false;
+            }
+            store[pos]--;
+        }
+        return true;
     }
 }
