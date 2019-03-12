7d888402e90447da433dc3c55a8e52b739ccad52
rosen369 <rosen369@outlook.com>
2019-03-12 11:56:14 +0800

    swap solution

--- 0384. Shuffle an Array/Solution.cs
+++ 0384. Shuffle an Array/Solution.cs
@@ -1,17 +1,37 @@
 public class Solution {
 
     public Solution (int[] nums) {
-
+        this._nums = nums;
+        this._curr = new int[nums.Length];
+        this._nums.CopyTo (this._curr, 0);
+        this._random = new Random ();
     }
+
+    private int[] _nums;
+
+    private int[] _curr;
+
+    private Random _random;
 
     /** Resets the array to its original configuration and return it. */
     public int[] Reset () {
-
+        this._nums.CopyTo (this._curr, 0);
+        return this._curr;
     }
 
     /** Returns a random shuffling of the array. */
     public int[] Shuffle () {
+        for (int i = 0; i < this._nums.Length; i++) {
+            var pos = this._random.Next (i, _nums.Length);
+            this.Swap (i, pos);
+        }
+        return this._curr;
+    }
 
+    private void Swap (int i, int j) {
+        var temp = this._curr[i];
+        this._curr[i] = this._curr[j];
+        this._curr[j] = temp;
     }
 }
 
