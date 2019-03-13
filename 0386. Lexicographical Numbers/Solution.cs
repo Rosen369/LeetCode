503fe71c86a3576686f8a224dd457d57e3ef49d1
rosen369 <rosen369@outlook.com>
2019-03-13 16:31:35 +0800

    dfs solution

--- 0386. Lexicographical Numbers/Solution.cs
+++ 0386. Lexicographical Numbers/Solution.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public IList<int> LexicalOrder (int n) {
+        var res = new List<int> ();
+        DFS (0, n, res);
+        return res;
+    }
 
+    private void DFS (int curr, int n, IList<int> res) {
+        for (int i = curr * 10; i < curr * 10 + 10; i++) {
+            if (i > n) {
+                return;
+            }
+            if (i == 0) {
+                continue;
+            }
+            res.Add (i);
+            DFS (i, n, res);
+        }
     }
 }
