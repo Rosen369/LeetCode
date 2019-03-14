472254a999155818d4e76e8e78b8f435b906aaa5
rosen369 <rosen369@outlook.com>
2019-03-14 10:53:40 +0800

    add longest absolute file path

--- 0388. Longest Absolute File Path/Solution.cs
+++ 0388. Longest Absolute File Path/Solution.cs
@@ -1,5 +1,21 @@
 public class Solution {
     public int LengthLongestPath (string input) {
-
+        var stack = new Stack<int> ();
+        stack.Push (0);
+        var sArray = input.Split ('\n');
+        var max = 0;
+        for (int i = 0; i < sArray.Length; i++) {
+            var s = sArray[i];
+            var level = s.LastIndexOf ('\t') + 1;
+            while (level + 1 < stack.Count) {
+                stack.Pop ();
+            }
+            var len = stack.Peek () + s.Length - level + 1;
+            stack.Push (len);
+            if (s.Contains (".")) {
+                max = Math.Max (max, len - 1);
+            }
+        }
+        return max;
     }
 }
