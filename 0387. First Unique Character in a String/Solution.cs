0b5a3ff9b9b58f2dfa22966fdbb5ec0d9359352a
rosen369 <rosen369@outlook.com>
2019-03-14 09:27:39 +0800

    cache solution

--- 0387. First Unique Character in a String/Solution.cs
+++ 0387. First Unique Character in a String/Solution.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public int FirstUniqChar (string s) {
-
+        var store = new int[26];
+        for (int i = 0; i < s.Length; i++) {
+            store[s[i] - 'a']++;
+        }
+        for (int i = 0; i < s.Length; i++) {
+            if (store[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
