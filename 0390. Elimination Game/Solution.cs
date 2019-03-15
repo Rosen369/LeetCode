51c904dd498b6de31ca3f1c5e0e4d8bd9b257b8e
rosen369 <rosen369@outlook.com>
2019-03-15 10:13:34 +0800

    DFS solution

--- 0390. Elimination Game/Solution.cs
+++ 0390. Elimination Game/Solution.cs
@@ -1,5 +1,8 @@
 public class Solution {
     public int LastRemaining (int n) {
-
+        if (n == 1) {
+            return 1;
+        }
+        return 2 * (1 + n / 2 - LastRemaining (n / 2));
     }
 }
