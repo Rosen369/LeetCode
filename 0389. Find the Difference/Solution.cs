1daccdf4be5edc25224d1d43c87b7b79a289bf14
rosen369 <rosen369@outlook.com>
2019-03-15 09:21:34 +0800

    sum solution

--- 0389. Find the Difference/Solution.cs
+++ 0389. Find the Difference/Solution.cs
@@ -1,5 +1,12 @@
 public class Solution {
     public char FindTheDifference (string s, string t) {
-
+        var sum = 0;
+        for (int i = 0; i < s.Length; i++) {
+            sum += s[i];
+        }
+        for (int i = 0; i < t.Length; i++) {
+            sum -= t[i];
+        }
+        return (char) - sum;
     }
 }
