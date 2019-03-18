dae821a14bf0fd3d94f397e787a7c9fe33e3894d
rosen369 <rosen369@outlook.com>
2019-03-18 09:44:57 +0800

    straightforward solution

--- 0392. Is Subsequence/Solution.cs
+++ 0392. Is Subsequence/Solution.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public bool IsSubsequence (string s, string t) {
-
+        var sIndex = 0;
+        for (int i = 0; i < t.Length; i++) {
+            if (sIndex == s.Length) {
+                break;
+            }
+            if (s[sIndex] == t[i]) {
+                sIndex++;
+            }
+        }
+        return sIndex == s.Length;
     }
 }
