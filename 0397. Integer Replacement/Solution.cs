26602300f4b409085042fa4811b5847cbe7d85a3
rosen369 <rosen369@outlook.com>
2019-03-19 11:24:31 +0800

    iterative solution

--- 0397. Integer Replacement/Solution.cs
+++ 0397. Integer Replacement/Solution.cs
@@ -1,5 +1,19 @@
 public class Solution {
     public int IntegerReplacement (int n) {
-
+        if (n == int.MaxValue) {
+            return 32;
+        }
+        var res = 0;
+        while (n > 1) {
+            res++;
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else if (n % 4 == 1 || n == 3) {
+                n--;
+            } else {
+                n++;
+            }
+        }
+        return res;
     }
 }
