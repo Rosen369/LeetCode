e7b4cb9cd5cdbe2d63bbd78a29f2fd87ec71981d
rosen369 <rosen369@outlook.com>
2019-03-19 10:17:49 +0800

    time limit exceeded

--- 0396. Rotate Function/Solution.cs
+++ 0396. Rotate Function/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public int MaxRotateFunction (int[] A) {
-
+        if (A.Length == 0) {
+            return 0;
+        }
+        var max = int.MinValue;
+        for (int i = 0; i < A.Length; i++) {
+            var curr = 0;
+            for (int j = 0; j < A.Length; j++) {
+                var pos = (i + j) % A.Length;
+                curr += A[pos] * j;
+            }
+            max = Math.Max (curr, max);
+        }
+        return max;
     }
 }
