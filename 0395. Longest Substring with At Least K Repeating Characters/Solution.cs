6d1b90304c1ed6017da7b1aa062075c0bdb2c89a
rosen369 <rosen369@outlook.com>
2019-03-19 09:55:04 +0800

    divide and conquer solution

--- 0395. Longest Substring with At Least K Repeating Characters/Solution.cs
+++ 0395. Longest Substring with At Least K Repeating Characters/Solution.cs
@@ -1,5 +1,27 @@
 public class Solution {
     public int LongestSubstring (string s, int k) {
-
+        if (string.IsNullOrEmpty (s)) {
+            return 0;
+        }
+        var count = new int[26];
+        for (int i = 0; i < s.Length; i++) {
+            count[s[i] - 'a']++;
+        }
+        var lessThanK = new List<char> ();
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0 && count[i] < k) {
+                var c = (char) ('a' + i);
+                lessThanK.Add (c);
+            }
+        }
+        if (lessThanK.Count == 0) {
+            return s.Length;
+        }
+        var max = 0;
+        var split = s.Split (lessThanK.ToArray ());
+        for (int i = 0; i < split.Length; i++) {
+            max = Math.Max (max, LongestSubstring (split[i], k));
+        }
+        return max;
     }
 }
