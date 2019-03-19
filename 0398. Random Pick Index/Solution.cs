1c16540d2554e5cc748c1b9005f5a5c3743aa1a3
rosen369 <rosen369@outlook.com>
2019-03-19 11:42:31 +0800

    dict solution

--- 0398. Random Pick Index/Solution.cs
+++ 0398. Random Pick Index/Solution.cs
@@ -1,11 +1,23 @@
 public class Solution {
 
     public Solution (int[] nums) {
-
+        this._dict = new Dictionary<int, IList<int>> ();
+        this._random = new Random ();
+        for (int i = 0; i < nums.Length; i++) {
+            if (!_dict.ContainsKey (nums[i])) {
+                _dict.Add (nums[i], new List<int> ());
+            }
+            _dict[nums[i]].Add (i);
+        }
     }
 
-    public int Pick (int target) {
+    private IDictionary<int, IList<int>> _dict;
 
+    private Random _random;
+
+    public int Pick (int target) {
+        var pos = _random.Next (_dict[target].Count);
+        return _dict[target][pos];
     }
 }
 
