f824635d7f65a95fc30014b92ec6ba2dc2d62b89
rosen369 <rosen369@outlook.com>
2019-03-19 10:45:10 +0800

    rotate solution

--- 0396. Rotate Function/Solution.cs
+++ 0396. Rotate Function/Solution.cs
@@ -1,16 +1,18 @@
 public class Solution {
     public int MaxRotateFunction (int[] A) {
-        if (A.Length == 0) {
-            return 0;
+        int n = A.Length;
+        int sum = 0;
+        int candidate = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += A[i];
+            candidate += A[i] * i;
         }
-        var max = int.MinValue;
-        for (int i = 0; i < A.Length; i++) {
-            var curr = 0;
-            for (int j = 0; j < A.Length; j++) {
-                var pos = (i + j) % A.Length;
-                curr += A[pos] * j;
-            }
-            max = Math.Max (curr, max);
+        var max = candidate;
+
+        for (int i = n - 1; i > 0; i--) {
+            candidate = candidate + sum - A[i] * n;
+            max = Math.Max (max, candidate);
         }
         return max;
     }
