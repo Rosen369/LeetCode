5880a62640b783741ae3275a400a483d293a0139
rosen369 <rosen369@outlook.com>
2019-03-21 15:47:21 +0800

    stack solution

--- 0402. Remove K Digits/Solution.cs
+++ 0402. Remove K Digits/Solution.cs
@@ -1,5 +1,28 @@
 public class Solution {
     public string RemoveKdigits (string num, int k) {
-
+        if (k >= num.Length) {
+            return "0";
+        }
+        var index = 0;
+        var stack = new Stack<char> ();
+        while (index < num.Length) {
+            while (stack.Count != 0 && k > 0 && stack.Peek () > num[index]) {
+                stack.Pop ();
+                k--;
+            }
+            stack.Push (num[index]);
+            index++;
+        }
+        while (k > 0) {
+            stack.Pop ();
+            k--;
+        }
+        var sb = new StringBuilder ();
+        while (stack.Count != 0) {
+            sb.Insert (0, stack.Pop ());
+        }
+        var res = sb.ToString ().TrimStart ('0');
+        res = string.IsNullOrEmpty (res) ? "0" : res;
+        return res;
     }
 }
