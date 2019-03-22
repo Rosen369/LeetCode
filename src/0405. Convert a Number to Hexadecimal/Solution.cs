4402bc0628ae001c510ebad8b1c1d18906739643
rosen369 <rosen369@outlook.com>
2019-03-22 14:05:47 +0800

    mask solution

--- src/0405. Convert a Number to Hexadecimal/Solution.cs
+++ src/0405. Convert a Number to Hexadecimal/Solution.cs
@@ -1,5 +1,13 @@
 public class Solution {
     public string ToHex (int num) {
-
+        var map = "0123456789abcdef";
+        if (num == 0) return "0";
+        var n = num & 0xffffffff;
+        var result = string.Empty;
+        while (n != 0) {
+            result = map[(int) (n % 16)] + result;
+            n /= 16;
+        }
+        return result;
     }
 }
