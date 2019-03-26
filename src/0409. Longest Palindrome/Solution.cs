aa569a2a4bbeb8c5e4a36d5fc764728107a6d802
rosen369 <rosen369@outlook.com>
2019-03-26 09:31:35 +0800

    count solution

--- src/0409. Longest Palindrome/Solution.cs
+++ src/0409. Longest Palindrome/Solution.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public int LongestPalindrome (string s) {
-
+        var count = new int[58];
+        for (int i = 0; i < s.Length; i++) {
+            count[s[i] - 'A']++;
+        }
+        var longest = 0;
+        for (int i = 0; i < 58; i++) {
+            if (count[i] == 0) {
+                continue;
+            }
+            if (longest % 2 == 0) {
+                longest += count[i];
+            } else if (count[i] > 1) {
+                longest += count[i] / 2 * 2;
+            }
+        }
+        return longest;
     }
 }
