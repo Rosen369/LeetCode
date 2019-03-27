76cedc92a7f6dc7a4b2cde9425376f4bbc468da6
rosen369 <rosen369@outlook.com>
2019-03-27 09:57:06 +0800

    dp solution

--- src/0413. Arithmetic Slices/Solution.cs
+++ src/0413. Arithmetic Slices/Solution.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public int NumberOfArithmeticSlices (int[] A) {
-
+        var len = A.Length;
+        var dp = new int[len];
+        var sum = 0;
+        for (int i = 2; i < len; i++) {
+            if (A[i - 2] - A[i - 1] == A[i - 1] - A[i]) {
+                dp[i] = dp[i - 1] + 1;
+                sum += dp[i];
+            }
+        }
+        return sum;
     }
 }
