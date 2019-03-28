b7a2b001af660ed9e7c39eadeb5042b776abe2d4
rosen369 <rosen369@outlook.com>
2019-03-28 17:02:06 +0800

    add hash set solution

--- src/0416. Partition Equal Subset Sum/Solution.cs
+++ src/0416. Partition Equal Subset Sum/Solution.cs
@@ -1,4 +1,7 @@
-public class Solution {
+public class Solution1 {
+    //dp solution
+    //Runtime: 124 ms
+    //Memory Usage: 22.4 MB
     public bool CanPartition (int[] nums) {
         var sum = 0;
         for (int i = 0; i < nums.Length; i++) {
@@ -17,5 +20,36 @@ public class Solution {
             }
         }
         return dp[target];
+    }
+}
+
+public class Solution2 {
+    //hashset solution
+    //Runtime: 156 ms
+    //Memory Usage: 32.9 MB
+    public bool CanPartition (int[] nums) {
+        var sum = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            sum += nums[i];
+        }
+        if (sum % 2 == 1) {
+            return false;
+        }
+        var target = sum / 2;
+        var set = new HashSet<int> ();
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] == target) {
+                return true;
+            }
+            var pre = set.ToList ();
+            for (int j = 0; j < pre.Count; j++) {
+                if (nums[i] + pre[j] == target) {
+                    return true;
+                }
+                set.Add (nums[i] + pre[j]);
+            }
+            set.Add (nums[i]);
+        }
+        return false;
     }
 }
