c7b9dce65d5da120acac8d215b6dce4d09171198
rosen369 <rosen369@outlook.com>
2019-03-28 14:49:53 +0800

    time limit exceeded

--- src/0416. Partition Equal Subset Sum/Solution.cs
+++ src/0416. Partition Equal Subset Sum/Solution.cs
@@ -1,5 +1,28 @@
 public class Solution {
     public bool CanPartition (int[] nums) {
+        var sum = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            sum += nums[i];
+        }
+        if (sum % 2 == 1) {
+            return false;
+        }
+        var target = sum / 2;
+        return DFS (nums, 0, 0, target);
+    }
 
+    private bool DFS (int[] nums, int index, int sum, int target) {
+        if (sum == target) {
+            return true;
+        }
+        if (sum > target) {
+            return false;
+        }
+        for (int i = index; i < nums.Length; i++) {
+            if (DFS (nums, i + 1, sum + nums[i], target)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
