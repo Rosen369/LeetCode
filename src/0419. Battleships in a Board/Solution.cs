2f92383c3b5b989fd00496755b05f36cfb2df93a
rosen369 <rosen369@outlook.com>
2019-03-29 16:21:29 +0800

    simple solution

--- src/0419. Battleships in a Board/Solution.cs
+++ src/0419. Battleships in a Board/Solution.cs
@@ -1,5 +1,24 @@
 public class Solution {
+    //Runtime: 96 ms
+    //Memory Usage: 23.3 MB
     public int CountBattleships (char[][] board) {
-
+        var m = board.Length;
+        var n = board[0].Length;
+        var count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] != 'X') {
+                    continue;
+                }
+                if (j != 0 && board[i][j - 1] == 'X') {
+                    continue;
+                }
+                if (i != 0 && board[i - 1][j] == 'X') {
+                    continue;
+                }
+                count++;
+            }
+        }
+        return count;
     }
 }
