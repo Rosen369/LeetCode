6ff85595a641ff8cd5347259b2f6cc000cb18aa0
rosen369 <rosen369@outlook.com>
2019-04-02 11:48:25 +0800

    bit operation solution

--- src/0421. Maximum XOR of Two Numbers in an Array/Solution.cs
+++ src/0421. Maximum XOR of Two Numbers in an Array/Solution.cs
@@ -1,5 +1,21 @@
 public class Solution {
     public int FindMaximumXOR (int[] nums) {
-
+        var max = 0;
+        var mask = 0;
+        for (int i = 31; i >= 0; i--) {
+            mask = mask | (1 << i);
+            var set = new HashSet<int> ();
+            foreach (var num in nums) {
+                set.Add (num & mask);
+            }
+            var tmp = max | (1 << i);
+            foreach (var prefix in set) {
+                if (set.Contains (tmp ^ prefix)) {
+                    max = tmp;
+                    break;
+                }
+            }
+        }
+        return max;
     }
 }
