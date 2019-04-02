fd31abcef706d0501c1fe64f0e563030f9d04c29
rosen369 <rosen369@outlook.com>
2019-04-02 16:48:15 +0800

    math solution

--- src/0423. Reconstruct Original Digits from English/Solution.cs
+++ src/0423. Reconstruct Original Digits from English/Solution.cs
@@ -1,5 +1,31 @@
 public class Solution {
+    //Runtime: 104 ms
+    //Memory Usage: 29 MB
     public string OriginalDigits (string s) {
-
+        var count = new int[10];
+        for (int i = 0; i < s.Length; i++) {
+            if (s[i] == 'z') count[0]++;
+            if (s[i] == 'w') count[2]++;
+            if (s[i] == 'u') count[4]++;
+            if (s[i] == 'x') count[6]++;
+            if (s[i] == 'g') count[8]++;
+            if (s[i] == 'o') count[1]++;
+            if (s[i] == 'h') count[3]++;
+            if (s[i] == 'f') count[5]++;
+            if (s[i] == 's') count[7]++;
+            if (s[i] == 'i') count[9]++;
+        }
+        count[1] -= count[0] + count[2] + count[4];
+        count[3] -= count[8];
+        count[5] -= count[4];
+        count[7] -= count[6];
+        count[9] -= count[5] + count[6] + count[8];
+        var sb = new StringBuilder ();
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < count[i]; j++) {
+                sb.Append (i);
+            }
+        }
+        return sb.ToString ();
     }
 }
