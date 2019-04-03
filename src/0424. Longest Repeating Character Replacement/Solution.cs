b2c7a89bf568005c5c7bb92edb3257ec50488f99
rosen369 <rosen369@outlook.com>
2019-04-03 10:00:55 +0800

    brute force solution

--- src/0424. Longest Repeating Character Replacement/Solution.cs
+++ src/0424. Longest Repeating Character Replacement/Solution.cs
@@ -1,5 +1,31 @@
 public class Solution {
     public int CharacterReplacement (string s, int k) {
-
+        var res = 0;
+        for (int left = 0; left < s.Length; left++) {
+            var replace = k;
+            var len = 0;
+            var right = left;
+            while (right < s.Length) {
+                if (s[left] == s[right]) {
+                    len++;
+                    right++;
+                } else if (replace > 0) {
+                    if (replace == k) {
+                        left = right - 1;
+                    }
+                    len++;
+                    right++;
+                    replace--;
+                } else {
+                    break;
+                }
+            }
+            if (replace > 0) {
+                len += replace;
+            }
+            res = Math.Max (res, len);
+            res = Math.Min (res, s.Length);
+        }
+        return res;
     }
 }
