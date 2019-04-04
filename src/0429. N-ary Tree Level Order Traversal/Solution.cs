1c8b791188639a861be99431c3369654092c9dc2
rosen369 <rosen369@outlook.com>
2019-04-04 11:07:21 +0800

    iterative solution

--- src/0429. N-ary Tree Level Order Traversal/Solution.cs
+++ src/0429. N-ary Tree Level Order Traversal/Solution.cs
@@ -12,6 +12,22 @@ public class Node {
 */
 public class Solution {
     public IList<IList<int>> LevelOrder (Node root) {
-
+        var res = new List<IList<int>> ();
+        if (root == null) {
+            return res;
+        }
+        var level = new List<Node> ();
+        level.Add (root);
+        while (level.Count != 0) {
+            var curr = new List<int> ();
+            var next = new List<Node> ();
+            foreach (var node in level) {
+                curr.Add (node.val);
+                next.AddRange (node.children);
+            }
+            res.Add (curr);
+            level = next;
+        }
+        return res;
     }
 }
