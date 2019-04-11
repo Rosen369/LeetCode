267ada61d73ec6c83fa7b7fc4e354ff823e99284
rosen369 <rosen369@outlook.com>
2019-04-11 11:45:52 +0800

    split solution

--- src/0434. Number of Segments in a String/Solution.cs
+++ src/0434. Number of Segments in a String/Solution.cs
@@ -1,5 +1,12 @@
 public class Solution {
     public int CountSegments (string s) {
-
+        var splits = s.Split (' ');
+        var count = 0;
+        for (int i = 0; i < splits.Length; i++) {
+            if (!string.IsNullOrEmpty (splits[i])) {
+                count++;
+            }
+        }
+        return count;
     }
 }
