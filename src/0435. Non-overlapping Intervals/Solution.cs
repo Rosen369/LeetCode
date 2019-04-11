5a13e6ce2f413b2336863dce0c613027ce554c8a
rosen369 <rosen369@outlook.com>
2019-04-11 14:46:38 +0800

    least is most solution

--- src/0435. Non-overlapping Intervals/Solution.cs
+++ src/0435. Non-overlapping Intervals/Solution.cs
@@ -9,6 +9,19 @@
  */
 public class Solution {
     public int EraseOverlapIntervals (Interval[] intervals) {
-
+        if (intervals.Length == 0) {
+            return 0;
+        }
+        var list = new List<Interval> (intervals);
+        list = list.OrderBy (e => e.end).ToList ();
+        var count = 1;
+        var end = list[0].end;
+        for (int i = 1; i < list.Count; i++) {
+            if (list[i].start >= end) {
+                count++;
+                end = list[i].end;
+            }
+        }
+        return intervals.Length - count;
     }
 }
