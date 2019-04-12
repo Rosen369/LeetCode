b1d769acfb17f42246000ae8abee93829f541e4a
rosen369 <rosen369@outlook.com>
2019-04-12 14:22:26 +0800

    DFS solution

--- src/0437. Path Sum III/Solution.cs
+++ src/0437. Path Sum III/Solution.cs
@@ -8,7 +8,30 @@
  * }
  */
 public class Solution {
+    //DFS solution
+    //Runtime: 116 ms
+    //Memory Usage: 23.9 MB
     public int PathSum (TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        var left = PathSum (root.left, sum);
+        var right = PathSum (root.right, sum);
+        var curr = DFS (root, sum, 0);
+        return curr + left + right;
+    }
 
+    private int DFS (TreeNode root, int sum, int pre) {
+        var res = 0;
+        if (root == null) {
+            return res;
+        }
+        var current = root.val + pre;
+        if (current == sum) {
+            res++;
+        }
+        res += DFS (root.left, sum, current);
+        res += DFS (root.right, sum, current);
+        return res;
     }
 }
