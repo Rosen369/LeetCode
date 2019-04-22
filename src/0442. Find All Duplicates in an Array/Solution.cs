bb726229b3b5844afb4e6554eacf52ca030d7c89
rosen369 <rosen369@outlook.com>
2019-04-22 10:01:37 +0800

    indices solution

--- src/0442. Find All Duplicates in an Array/Solution.cs
+++ src/0442. Find All Duplicates in an Array/Solution.cs
@@ -1,5 +1,13 @@
 public class Solution {
     public IList<int> FindDuplicates (int[] nums) {
-
+        var res = new List<int> ();
+        for (int i = 0; i < nums.Length; i++) {
+            var index = Math.Abs (nums[i]) - 1;
+            if (nums[index] < 0) {
+                res.Add (index + 1);
+            }
+            nums[index] = -nums[index];
+        }
+        return res;
     }
 }
