098b3bed07a1f5cf814bc4c695afb0df8f49cdef
rosen369 <rosen369@outlook.com>
2019-04-24 14:38:13 +0800

    dict solution

--- src/0447. Number of Boomerangs/Solution.cs
+++ src/0447. Number of Boomerangs/Solution.cs
@@ -1,5 +1,28 @@
 public class Solution {
     public int NumberOfBoomerangs (int[][] points) {
-
+        var res = 0;
+        var len = points.Length;
+        for (int i = 0; i < len; i++) {
+            var dict = new Dictionary<int, int> ();
+            for (int j = 0; j < len; j++) {
+                if (i == j) {
+                    continue;
+                }
+                var dx = points[i][0] - points[j][0];
+                var dy = points[i][1] - points[j][1];
+                var distence = dx * dx + dy * dy;
+                if (!dict.ContainsKey (distence)) {
+                    dict.Add (distence, 0);
+                }
+                dict[distence]++;
+            }
+            var values = dict.Values.ToList ();
+            for (int j = 0; j < values.Count; j++) {
+                if (values[j] > 1) {
+                    res += values[j] * (values[j] - 1);
+                }
+            }
+        }
+        return res;
     }
 }
