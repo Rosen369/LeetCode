91bfcc1a1a2326fc1b413184d26a3334aeed679c
rosen369 <rosen369@outlook.com>
2019-04-24 14:14:07 +0800

    add arithmetic slices ii

--- src/0446. Arithmetic Slices II - Subsequence/Solution.cs
+++ src/0446. Arithmetic Slices II - Subsequence/Solution.cs
@@ -1,5 +1,21 @@
 public class Solution {
     public int NumberOfArithmeticSlices (int[] A) {
-
+        var res = 0;
+        var dp = new IDictionary<int, int>[A.Length];
+        for (int i = 0; i < A.Length; i++) {
+            dp[i] = new Dictionary<int, int> ();
+            for (int j = 0; j < i; j++) {
+                var diff = (long) A[i] - (long) A[j];
+                if (diff > int.MaxValue || diff < int.MinValue) {
+                    continue;
+                }
+                var d = (int) diff;
+                var c1 = dp[i].ContainsKey (d) ? dp[i][d] : 0;
+                var c2 = dp[j].ContainsKey (d) ? dp[j][d] : 0;
+                res += c2;
+                dp[i][d] = c1 + c2 + 1;
+            }
+        }
+        return res;
     }
 }
