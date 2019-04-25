f869a64be95be42b9fee16dc2622f2ecf71d372b
rosen369 <rosen369@outlook.com>
2019-04-25 09:47:38 +0800

    mark index solution

--- src/0448. Find All Numbers Disappeared in an Array/Solution.cs
+++ src/0448. Find All Numbers Disappeared in an Array/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public IList<int> FindDisappearedNumbers (int[] nums) {
-
+        var res = new List<int> ();
+        for (int i = 0; i < nums.Length; i++) {
+            var index = Math.Abs (nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] > 0) {
+                res.Add (i + 1);
+            }
+        }
+        return res;
     }
 }
