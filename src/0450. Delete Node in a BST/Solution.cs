c85875823f77258ea128e18f481cc9fab836c144
rosen369 <rosen369@outlook.com>
2019-04-25 11:38:13 +0800

    recursive solution

--- src/0450. Delete Node in a BST/Solution.cs
+++ src/0450. Delete Node in a BST/Solution.cs
@@ -9,6 +9,31 @@
  */
 public class Solution {
     public TreeNode DeleteNode (TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val > key) {
+            root.left = this.DeleteNode (root.left, key);
+        } else if (root.val < key) {
+            root.right = this.DeleteNode (root.right, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+            var minNode = this.FindMinNode (root.right);
+            root.val = minNode.val;
+            root.right = this.DeleteNode (root.right, root.val);
+        }
+        return root;
+    }
 
+    private TreeNode FindMinNode (TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
     }
 }
