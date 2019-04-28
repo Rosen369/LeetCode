a94d972cb7408dde78e0efac6a248094244a362c
rosen369 <rosen369@outlook.com>
2019-04-28 15:39:50 +0800

    math solution

--- src/0453. Minimum Moves to Equal Array Elements/Solution.cs
+++ src/0453. Minimum Moves to Equal Array Elements/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
+    //math solution
+    // add to x with m moves
+    // sum + m * (n - 1) = x * n
+    // x = minNum + m
+    // sum - minNum * n = m
     public int MinMoves (int[] nums) {
-
+        var min = int.MaxValue;
+        var sum = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            min = Math.Min (min, nums[i]);
+            sum += nums[i];
+        }
+        var res = sum - min * nums.Length;
+        return res;
     }
 }
