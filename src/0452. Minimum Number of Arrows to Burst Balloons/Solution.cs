cca4c389926fd17afbb6c01e9f622ccc2eb62abf
rosen369 <rosen369@outlook.com>
2019-04-28 10:12:59 +0800

    greedy solution

--- src/0452. Minimum Number of Arrows to Burst Balloons/Solution.cs
+++ src/0452. Minimum Number of Arrows to Burst Balloons/Solution.cs
@@ -1,5 +1,18 @@
 public class Solution {
     public int FindMinArrowShots (int[][] points) {
-
+        if (points.Length == 0) {
+            return 0;
+        }
+        points = points.OrderBy (e => e[1]).ToArray ();
+        var count = 1;
+        var arrow = points[0][1];
+        for (int i = 1; i < points.Length; i++) {
+            if (points[i][0] <= arrow) {
+                continue;
+            }
+            arrow = points[i][1];
+            count++;
+        }
+        return count;
     }
 }
