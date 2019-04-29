2eac8892078c13d11f3987baaac3caa32acd7e75
rosen369 <rosen369@outlook.com>
2019-04-29 09:58:27 +0800

    dict solution

--- src/0454. 4Sum II/Solution.cs
+++ src/0454. 4Sum II/Solution.cs
@@ -1,5 +1,24 @@
 public class Solution {
     public int FourSumCount (int[] A, int[] B, int[] C, int[] D) {
-
+        var count = 0;
+        var dict = new Dictionary<int, int> ();
+        for (int i = 0; i < A.Length; i++) {
+            for (int j = 0; j < B.Length; j++) {
+                var target = A[i] + B[j];
+                if (!dict.ContainsKey (target)) {
+                    dict.Add (target, 0);
+                }
+                dict[target]++;
+            }
+        }
+        for (int i = 0; i < C.Length; i++) {
+            for (int j = 0; j < D.Length; j++) {
+                var target = 0 - C[i] - D[j];
+                if (dict.ContainsKey (target)) {
+                    count += dict[target];
+                }
+            }
+        }
+        return count;
     }
 }
