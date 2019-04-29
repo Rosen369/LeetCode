58474d58991db825b28915d75bd7c52da2aeeb17
rosen369 <rosen369@outlook.com>
2019-04-29 10:36:52 +0800

    sort solution

--- src/0455. Assign Cookies/Solution.cs
+++ src/0455. Assign Cookies/Solution.cs
@@ -1,5 +1,19 @@
 public class Solution {
     public int FindContentChildren (int[] g, int[] s) {
-
+        var res = 0;
+        Array.Sort (g);
+        Array.Reverse (g);
+        Array.Sort (s);
+        Array.Reverse (s);
+        var gIndex = 0;
+        var sIndex = 0;
+        while (gIndex < g.Length && sIndex < s.Length) {
+            if (g[gIndex] <= s[sIndex]) {
+                sIndex++;
+                res++;
+            }
+            gIndex++;
+        }
+        return res;
     }
 }
