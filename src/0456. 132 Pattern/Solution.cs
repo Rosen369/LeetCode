0ba9812742be525f9a997a9ddda32116c26ca1a7
rosen369 <rosen369@outlook.com>
2019-04-29 11:28:01 +0800

    better brute force

--- src/0456. 132 Pattern/Solution.cs
+++ src/0456. 132 Pattern/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public bool Find132pattern (int[] nums) {
-
+        for (int i = 0; i < nums.Length; i++) {
+            var pre = int.MinValue;
+            for (int j = i + 1; j < nums.Length; j++) {
+                if (nums[j] <= nums[i]) {
+                    continue;
+                }
+                if (nums[j] < pre) {
+                    return true;
+                }
+                pre = nums[j];
+            }
+        }
+        return false;
     }
 }
