e0eb1052c33eea050cda5a56294339dafcfea5e7
rosen369 <rosen369@outlook.com>
2019-05-06 10:03:47 +0800

    add stack solution

--- src/0456. 132 Pattern/Solution.cs
+++ src/0456. 132 Pattern/Solution.cs
@@ -1,4 +1,7 @@
-public class Solution {
+public class Solution1 {
+    //better brute force
+    //Runtime: 1444 ms
+    //Memory Usage: 27.2 MB
     public bool Find132pattern (int[] nums) {
         for (int i = 0; i < nums.Length; i++) {
             var pre = int.MinValue;
@@ -11,6 +14,33 @@ public class Solution {
                 }
                 pre = nums[j];
             }
+        }
+        return false;
+    }
+}
+
+public class Solution2 {
+    //stack solution
+    //Runtime: 108 ms
+    //Memory Usage: 27.8 MB
+    public bool Find132pattern (int[] nums) {
+        if (nums.Length < 3) {
+            return false;
+        }
+        var stack = new Stack<int> ();
+        var min = new int[nums.Length];
+        min[0] = nums[0];
+        for (int i = 1; i < nums.Length; i++) {
+            min[i] = Math.Min (min[i - 1], nums[i]);
+        }
+        for (int i = nums.Length - 1; i >= 0; i--) {
+            while (stack.Count > 0 && stack.Peek () <= min[i]) {
+                stack.Pop ();
+            }
+            if (stack.Count > 0 && stack.Peek () < nums[i]) {
+                return true;
+            }
+            stack.Push (nums[i]);
         }
         return false;
     }
