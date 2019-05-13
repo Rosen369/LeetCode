d82fdd2ce3c21fc035c961f3c52818f1b3ad23df
rosen369 <rosen369@outlook.com>
2019-05-13 09:58:31 +0800

    math solution

--- src/0458. Poor Pigs/Solution.cs
+++ src/0458. Poor Pigs/Solution.cs
@@ -1,5 +1,8 @@
 public class Solution {
     public int PoorPigs (int buckets, int minutesToDie, int minutesToTest) {
-
+        var states = minutesToTest / minutesToDie + 1;
+        var d = Math.Log (buckets, states);
+        var c = Math.Ceiling (d);
+        return Convert.ToInt32 (c);
     }
 }
