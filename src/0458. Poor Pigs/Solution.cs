2aa3646f04fc3949f35b2c95dd59244c971bde90
rosen369 <rosen369@outlook.com>
2019-05-13 10:06:12 +0800

    add same solution

--- src/0458. Poor Pigs/Solution.cs
+++ src/0458. Poor Pigs/Solution.cs
@@ -1,8 +1,25 @@
-public class Solution {
+public class Solution1 {
+    //math solution
+    //Runtime: 40 ms
+    //Memory Usage: 13 MB
     public int PoorPigs (int buckets, int minutesToDie, int minutesToTest) {
         var states = minutesToTest / minutesToDie + 1;
         var d = Math.Log (buckets, states);
         var c = Math.Ceiling (d);
         return Convert.ToInt32 (c);
+    }
+}
+
+public class Solution2 {
+    public int PoorPigs (int buckets, int minutesToDie, int minutesToTest) {
+        var baseNum = minutesToTest / minutesToDie + 1;
+        var result = 1;
+        var num = 0;
+        while (result < buckets) {
+            num++;
+            result *= baseNum;
+        }
+
+        return num;
     }
 }
