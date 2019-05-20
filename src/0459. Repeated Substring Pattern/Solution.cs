67ff527246688f63b43577b07bc4062773cb9cc6
rosen369 <rosen369@outlook.com>
2019-05-20 11:58:05 +0800

    simple compare solution

--- src/0459. Repeated Substring Pattern/Solution.cs
+++ src/0459. Repeated Substring Pattern/Solution.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public bool RepeatedSubstringPattern (string s) {
-
+        var len = s.Length;
+        for (int i = 1; i <= len / 2; i++) {
+            if (len % i != 0) continue;
+            var sub = s.Substring (0, i);
+            var subIndex = 0;
+            var succeed = true;
+            for (int j = 0; j < len; j++) {
+                if (s[j] != sub[subIndex]) {
+                    succeed = false;
+                    break;
+                }
+                subIndex = subIndex == sub.Length - 1 ? 0 : subIndex + 1;
+            }
+            if (succeed) return true;
+        }
+        return false;
     }
 }
