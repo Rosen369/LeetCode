01dbecb8ebc45a36ac2a3e24eaaf3cb9c796e88d
rosen369 <rosen369@outlook.com>
2019-05-24 13:59:29 +0800

    xor count bit solution

--- src/0461. Hamming Distance/Solution.cs
+++ src/0461. Hamming Distance/Solution.cs
@@ -1,5 +1,11 @@
 public class Solution {
     public int HammingDistance (int x, int y) {
-
+        var count = 0;
+        var n = x ^ y;
+        while (n != 0) {
+            count++;
+            n &= (n - 1);
+        }
+        return count;
     }
 }
