fb55f86def8b6738104850420d47602a28412604
rosen369 <rosen369@outlook.com>
2019-05-24 14:33:58 +0800

    median sum solution

--- src/0462. Minimum Moves to Equal Array Elements II/Solution.cs
+++ src/0462. Minimum Moves to Equal Array Elements II/Solution.cs
@@ -1,5 +1,11 @@
 public class Solution {
     public int MinMoves2 (int[] nums) {
-
+        Array.Sort (nums);
+        var median = nums[nums.Length / 2];
+        var res = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            res += Math.Abs (nums[i] - median);
+        }
+        return res;
     }
 }
