ce56d9e91dba287ba5d5db3019668e7fae8c855f
rosen369 <rosen369@outlook.com>
2019-06-06 14:03:49 +0800

    dict solution

--- src/0466. Count The Repetitions/Solution.cs
+++ src/0466. Count The Repetitions/Solution.cs
@@ -1,28 +1,34 @@
 public class Solution {
     public int GetMaxRepetitions (string s1, int n1, string s2, int n2) {
-        var s1N = 0;
-        var s2N = 0;
-        var s1Index = 0;
-        var s2Index = 0;
+        var kToRepeatCount = new Dictionary<int, int> ();
+        var nextIndexToK = new Dictionary<int, int> ();
+        kToRepeatCount.Add (0, 0);
+        nextIndexToK.Add (0, 0);
+        var j = 0;
         var count = 0;
-        while (s1N < n1) {
-            if (s1[s1Index] == s2[s2Index]) {
-                s2Index++;
-                if (s2Index == s2.Length) {
-                    s2N++;
-                    s2Index = 0;
-                    if (s2N == n2) {
+        for (int k = 1; k <= n1; k++) {
+            for (int i = 0; i < s1.Length; i++) {
+                if (s1[i] == s2[j]) {
+                    j++;
+                    if (j == s2.Length) {
+                        j = 0;
                         count++;
-                        s2N = 0;
                     }
                 }
             }
-            s1Index++;
-            if (s1Index == s1.Length) {
-                s1N++;
-                s1Index = 0;
+            if (nextIndexToK.ContainsKey (j)) {
+                var start = nextIndexToK[j];
+                var prefixCount = kToRepeatCount[start];
+                var patternCount = (n1 - start) / (k - start) * (count - prefixCount);
+                var suffixCount = kToRepeatCount[start + (n1 - start) % (k - start)] - prefixCount;
+                return (prefixCount + patternCount + suffixCount) / n2;
             }
+            if (true) {
+
+            }
+            kToRepeatCount[k] = count;
+            nextIndexToK[j] = k;
         }
-        return count;
+        return kToRepeatCount[n1] / n2;
     }
 }
