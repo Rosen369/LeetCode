a9604b35359e28c9c23fab775103b2948c719ee1
rosen369 <rosen369@outlook.com>
2019-06-17 15:54:31 +0800

    dfs solution

--- src/0473. Matchsticks to Square/Solution.cs
+++ src/0473. Matchsticks to Square/Solution.cs
@@ -1,5 +1,32 @@
 public class Solution {
     public bool Makesquare (int[] nums) {
+        if (nums.Length < 4) {
+            return false;
+        }
+        var sum = nums.Sum ();
+        if (sum % 4 != 0) return false;
+        var target = sum / 4;
+        Array.Sort (nums);
+        Array.Reverse (nums);
+        if (nums[0] > target) {
+            return false;
+        }
+        return DFS (nums, new int[4], target, 0);
+    }
 
+    public bool DFS (int[] nums, int[] sums, int target, int index) {
+        if (index == nums.Length) {
+            for (int i = 0; i < 4; i++) {
+                if (sums[i] != target) return false;
+            }
+            return true;
+        }
+        for (int i = 0; i < 4; i++) {
+            if (sums[i] + nums[index] > target) continue;
+            sums[i] += nums[index];
+            if (this.DFS (nums, sums, target, index + 1)) return true;
+            sums[i] -= nums[index];
+        }
+        return false;
     }
 }
