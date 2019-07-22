5de704b3366024a064ce83f54ce4d03e96546d81
rosen369 <rosen369@outlook.com>
2019-07-22 10:23:25 +0800

    two pointer solution

--- src/0475. Heaters/Solution.cs
+++ src/0475. Heaters/Solution.cs
@@ -1,5 +1,24 @@
 public class Solution {
     public int FindRadius (int[] houses, int[] heaters) {
-
+        Array.Sort (houses);
+        Array.Sort (heaters);
+        var radius = 0;
+        var n = 0;
+        for (int i = 0; i < houses.Length; i++) {
+            var house = houses[i];
+            var least = int.MaxValue;
+            for (int j = n; j < heaters.Length; j++) {
+                var heater = heaters[j];
+                var abs = Math.Abs (house - heater);
+                if (abs <= least) {
+                    least = abs;
+                    n = j;
+                } else {
+                    break;
+                }
+            }
+            radius = Math.Max (least, radius);
+        }
+        return radius;
     }
 }
