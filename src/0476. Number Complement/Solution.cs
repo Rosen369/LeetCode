a93b26eb9dd020f178b23ead91d37b349a95bb52
rosen369 <rosen369@outlook.com>
2019-07-23 14:01:02 +0800

    mask solution

--- src/0476. Number Complement/Solution.cs
+++ src/0476. Number Complement/Solution.cs
@@ -1,5 +1,9 @@
 public class Solution {
     public int FindComplement (int num) {
-
+        var mask = 1;
+        while (mask < num) {
+            mask = (mask << 1) | 1;
+        }
+        return ~num & mask;
     }
 }
