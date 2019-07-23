4024ce5d1a671748e766b96879626ed89c97aaf6
rosen369 <rosen369@outlook.com>
2019-07-23 16:13:17 +0800

    bit counting method

--- src/0477. Total Hamming Distance/Solution.cs
+++ src/0477. Total Hamming Distance/Solution.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public int TotalHammingDistance (int[] nums) {
-
+        var res = 0;
+        var max = 0;
+        for (int i = 0; i < 32; i++) {
+            var ones = 0;
+            var zeros = 0;
+            for (int j = 0; j < nums.Length; j++) {
+                if ((nums[j] & 1) == 1) {
+                    ones++;
+                } else {
+                    zeros++;
+                }
+                nums[j] = nums[j] >> 1;
+            }
+            res += ones * zeros;
+        }
+        return res;
     }
 }
