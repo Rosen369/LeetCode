a080929e57d95a07bcad24f65cf92eea877b1986
rosen369 <rosen369@outlook.com>
2019-07-24 10:13:56 +0800

    polar coordinates solution

--- src/0478. Generate Random Point in a Circle/Solution.cs
+++ src/0478. Generate Random Point in a Circle/Solution.cs
@@ -1,11 +1,27 @@
 public class Solution {
 
     public Solution (double radius, double x_center, double y_center) {
-
+        this._radius = radius;
+        this.x_center = x_center;
+        this.y_center = y_center;
+        this._rand = new Random ();
     }
 
-    public double[] RandPoint () {
+    private double _radius;
 
+    private double x_center;
+
+    private double y_center;
+
+    private Random _rand;
+
+    public double[] RandPoint () {
+        var rand = this._rand.NextDouble ();
+        var length = Math.Sqrt (rand) * this._radius;
+        var degree = this._rand.NextDouble () * 2 * Math.PI;
+        var x = this.x_center + length * Math.Cos (degree);
+        var y = this.y_center + length * Math.Sin (degree);
+        return new double[] { x, y };
     }
 }
 
