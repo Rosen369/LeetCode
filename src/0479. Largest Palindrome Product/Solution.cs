4507ec2bf5a7852df44cf1df245ed6ed11038231
rosen369 <rosen369@outlook.com>
2019-07-25 15:32:53 +0800

    from palindrome to product solution

--- src/0479. Largest Palindrome Product/Solution.cs
+++ src/0479. Largest Palindrome Product/Solution.cs
@@ -1,5 +1,26 @@
 public class Solution {
     public int LargestPalindrome (int n) {
-
+        var max = (long) Math.Pow (10, n) - 1;
+        var min = max / 10;
+        for (var h = max; h > min; h--) {
+            var left = h;
+            var right = 0L;
+            for (var i = h; i != 0;) {
+                right = right * 10 + i % 10;
+                i /= 10;
+                left *= 10;
+            }
+            var palindrome = left + right;
+            for (var i = max; i > min; i--) {
+                var j = palindrome / i;
+                if (j > i) {
+                    break;
+                }
+                if (palindrome % i == 0) {
+                    return (int) (palindrome % 1337);
+                }
+            }
+        }
+        return 9;
     }
 }
