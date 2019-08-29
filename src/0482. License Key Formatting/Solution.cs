a98a96bbbee167307d53ce6000f745eae9a2e057
rosen369 <rosen369@outlook.com>
2019-08-29 10:15:53 +0800

    simple solution

--- src/0482. License Key Formatting/Solution.cs
+++ src/0482. License Key Formatting/Solution.cs
@@ -1,5 +1,29 @@
 public class Solution {
     public string LicenseKeyFormatting (string S, int K) {
-
+        S = S.ToUpper ();
+        var sb = new StringBuilder ();
+        var list = new List<char> ();
+        for (int i = 0; i < S.Length; i++) {
+            if (S[i] != '-') {
+                list.Add (S[i]);
+            }
+        }
+        var fGroup = list.Count () % K;
+        if (fGroup > 0) {
+            for (int i = 0; i < fGroup; i++) {
+                sb.Append (list[i]);
+            }
+            sb.Append ('-');
+        }
+        var countK = 0;
+        for (int i = fGroup; i < list.Count (); i++) {
+            sb.Append (list[i]);
+            countK++;
+            if (countK == K) {
+                sb.Append ('-');
+                countK = 0;
+            }
+        }
+        return sb.ToString ().TrimEnd ('-');
     }
 }
