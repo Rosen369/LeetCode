b9aedda8418a7d51a120e99ee9d810ef8ef8393e
rosen369 <rosen369@outlook.com>
2019-09-03 13:54:37 +0800

    math solution with big integer and binary search

--- src/0483. Smallest Good Base/Solution.cs
+++ src/0483. Smallest Good Base/Solution.cs
@@ -1,5 +1,28 @@
 public class Solution {
-    public string SmallestGoodBase (string n) {
-
+    public string SmallestGoodBase (string nn) {
+        var n = Convert.ToInt64 (nn);
+        var res = 0L;
+        for (int k = 60; k >= 2; k--) {
+            var start = 2L;
+            var end = n;
+            while (start < end) {
+                var mid = start + (end - start) / 2;
+                var left = new BigInteger (mid);
+                left = BigInteger.Pow (left, k);
+                left = BigInteger.Subtract (left, BigInteger.One);
+                var right = BigInteger.Multiply (new BigInteger (n), BigInteger.Subtract (new BigInteger (mid), BigInteger.One));
+                var cmr = left.CompareTo (right);
+                if (cmr == 0) {
+                    res = mid;
+                    break;
+                } else if (cmr < 0) {
+                    start = mid + 1;
+                } else {
+                    end = mid;
+                }
+            }
+            if (res != 0) break;
+        }
+        return res.ToString ();
     }
 }
