2528080eb717e4138f08c1e32094667794728041
rosen369 <rosen369@outlook.com>
2019-09-09 16:18:36 +0800

    count solution

--- src/0485. Max Consecutive Ones/Solution.cs
+++ src/0485. Max Consecutive Ones/Solution.cs
@@ -1,5 +1,16 @@
 public class Solution {
     public int FindMaxConsecutiveOnes (int[] nums) {
-
+        var max = 0;
+        var curr = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] == 1) {
+                curr++;
+                max = Math.Max (curr, max);
+            } else {
+                curr = 0;
+                continue;
+            }
+        }
+        return max;
     }
 }
