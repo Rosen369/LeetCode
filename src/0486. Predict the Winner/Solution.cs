5cbf544bd56ca6bd2c823eae2b33e593430d12e0
rosen369 <rosen369@outlook.com>
2019-09-16 16:06:56 +0800

    dp solution

--- src/0486. Predict the Winner/Solution.cs
+++ src/0486. Predict the Winner/Solution.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public bool PredictTheWinner (int[] nums) {
-
+        var dp = new int[nums.Length, nums.Length];
+        for (var s = nums.Length - 1; s >= 0; s--) {
+            dp[s, s] = nums[s];
+            for (var e = s + 1; e < nums.Length; e++) {
+                var a = nums[s] - dp[s + 1, e];
+                var b = nums[e] - dp[s, e - 1];
+                dp[s, e] = Math.Max (a, b);
+            }
+        }
+        return dp[0, nums.Length - 1] >= 0;
     }
 }
