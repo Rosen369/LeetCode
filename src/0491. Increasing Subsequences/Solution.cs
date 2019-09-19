6a9db06da31bcb30a96bcf06b6954629392f9653
rosen369 <rosen369@outlook.com>
2019-09-19 15:27:52 +0800

    dfs solution

--- src/0491. Increasing Subsequences/Solution.cs
+++ src/0491. Increasing Subsequences/Solution.cs
@@ -1,5 +1,27 @@
 public class Solution {
     public IList<IList<int>> FindSubsequences (int[] nums) {
+        var set = new HashSet<string> ();
+        var res = new List<IList<int>> ();
+        for (int i = 0; i < nums.Length; i++) {
+            var combine = new List<int> ();
+            combine.Add (nums[i]);
+            this.Combine (res, set, nums, combine, i + 1, nums[i].ToString ());
+        }
+        return res;
+    }
 
+    private void Combine (List<IList<int>> res, HashSet<string> set, int[] nums, IList<int> combine, int index, string key) {
+        for (int i = index; i < nums.Length; i++) {
+            if (nums[i] >= combine[combine.Count - 1]) {
+                var next = new List<int> (combine);
+                next.Add (nums[i]);
+                var nKey = key + '_' + nums[i];
+                if (!set.Contains (nKey)) {
+                    res.Add (next);
+                    set.Add (nKey);
+                }
+                this.Combine (res, set, nums, next, i + 1, nKey);
+            }
+        }
     }
 }
