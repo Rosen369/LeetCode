89e90006c7e7630694d6ea9c7a1967d99a7d5679
rosen369 <rosen369@outlook.com>
2019-09-19 16:02:43 +0800

    optimized solution

--- src/0491. Increasing Subsequences/Solution.cs
+++ src/0491. Increasing Subsequences/Solution.cs
@@ -2,25 +2,21 @@ public class Solution {
     public IList<IList<int>> FindSubsequences (int[] nums) {
         var set = new HashSet<string> ();
         var res = new List<IList<int>> ();
-        for (int i = 0; i < nums.Length; i++) {
-            var combine = new List<int> ();
-            combine.Add (nums[i]);
-            this.Combine (res, set, nums, combine, i + 1, nums[i].ToString ());
-        }
+        this.Combine (res, set, nums, new List<int> (), 0, string.Empty);
         return res;
     }
 
     private void Combine (List<IList<int>> res, HashSet<string> set, int[] nums, IList<int> combine, int index, string key) {
         for (int i = index; i < nums.Length; i++) {
-            if (nums[i] >= combine[combine.Count - 1]) {
+            if (combine.Count < 1 || nums[i] >= combine[combine.Count - 1]) {
                 var next = new List<int> (combine);
                 next.Add (nums[i]);
-                var nKey = key + '_' + nums[i];
-                if (!set.Contains (nKey)) {
+                var nkey = key + '_' + nums[i];
+                if (next.Count > 1 && !set.Contains (nkey)) {
                     res.Add (next);
-                    set.Add (nKey);
+                    set.Add (nkey);
                 }
-                this.Combine (res, set, nums, next, i + 1, nKey);
+                this.Combine (res, set, nums, next, i + 1, nkey);
             }
         }
     }
