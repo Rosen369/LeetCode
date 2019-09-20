f00bcafaf626ae77d7535166fc4b7eee9a8e1f0b
rosen369 <rosen369@outlook.com>
2019-09-20 13:51:40 +0800

    math solution

--- src/0492. Construct the Rectangle/Solution.cs
+++ src/0492. Construct the Rectangle/Solution.cs
@@ -1,5 +1,11 @@
 public class Solution {
     public int[] ConstructRectangle (int area) {
-
+        var right = (int) Math.Sqrt ((double) area);
+        var left = area / right;
+        while (left * right != area) {
+            right--;
+            left = area / right;
+        }
+        return new int[] { left, right };
     }
 }
