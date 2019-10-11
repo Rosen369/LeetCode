b9347dc9a8f1c9dba1a967d7fca32eff802e1d21
rosen369 <rosen369@outlook.com>
2019-10-11 14:09:38 +0800

    math solution

--- src/0495. Teemo Attacking/Solution.cs
+++ src/0495. Teemo Attacking/Solution.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public int FindPoisonedDuration (int[] timeSeries, int duration) {
-
+        if (timeSeries.Length == 0) return 0;
+        var res = 0;
+        var s = timeSeries[0];
+        var e = timeSeries[0] + duration;
+        for (int i = 1; i < timeSeries.Length; i++) {
+            var n = timeSeries[i];
+            if (n <= e) {
+                e = n + duration;
+            } else {
+                res += e - s;
+                s = n;
+                e = n + duration;
+            }
+        }
+        res += e - s;
+        return res;
     }
 }
