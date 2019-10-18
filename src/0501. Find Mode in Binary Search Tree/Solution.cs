415ddbad3df9e698441fc8ef1d26ccaa3265d0b0
rosen369 <rosen369@outlook.com>
2019-10-18 15:38:33 +0800

    counting solution

--- src/0501. Find Mode in Binary Search Tree/Solution.cs
+++ src/0501. Find Mode in Binary Search Tree/Solution.cs
@@ -9,6 +9,27 @@
  */
 public class Solution {
     public int[] FindMode (TreeNode root) {
+        var res = new List<int> ();
+        var dict = new Dictionary<int, int> ();
+        this.Count (root, dict);
+        var max = int.MinValue;
+        var values = dict.Values.ToList ();
+        for (int i = 0; i < values.Count; i++) {
+            max = Math.Max (max, values[i]);
+        }
+        var keys = dict.Keys.ToList ();
+        for (int i = 0; i < keys.Count; i++) {
+            if (dict[keys[i]] == max) { res.Add (keys[i]); }
+        }
+        return res.ToArray ();
+    }
 
+    private void Count (TreeNode node, IDictionary<int, int> dict) {
+        if (node == null) { return; }
+        var val = node.val;
+        if (!dict.ContainsKey (val)) { dict.Add (val, 0); }
+        dict[val]++;
+        this.Count (node.left, dict);
+        this.Count (node.right, dict);
     }
 }
