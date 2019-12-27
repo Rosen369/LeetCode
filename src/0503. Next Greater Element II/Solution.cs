02fa7c5722d2cd7a8187bdc2dd883ee332e6a4d1
rosen369 <rosen369@outlook.com>
2019-12-27 22:36:11 +0800

    stack solution

--- src/0503. Next Greater Element II/Solution.cs
+++ src/0503. Next Greater Element II/Solution.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public int[] NextGreaterElements (int[] nums) {
-
+        var res = new int[nums.Length];
+        var stack = new Stack<int> ();
+        for (int c = 0; c < 2; c++) {
+            for (int i = nums.Length - 1; i >= 0; i--) {
+                while (stack.Count > 0 && nums[stack.Peek ()] <= nums[i]) {
+                    stack.Pop ();
+                }
+                if (stack.Count > 0) {
+                    res[i] = nums[stack.Peek ()];
+                } else {
+                    res[i] = -1;
+                }
+                stack.Push (i);
+            }
+        }
+        return res;
     }
 }
