a09a942ab5fd6a0a6f928e4fb256c1a109dd7294
rosen369 <rosen369@outlook.com>
2020-01-03 00:48:38 +0800

    stack solution

--- src/0504. Base 7/Solution.cs
+++ src/0504. Base 7/Solution.cs
@@ -1,5 +1,25 @@
 public class Solution {
     public string ConvertToBase7 (int num) {
-
+        if (num == 0) {
+            return "0";
+        }
+        var symbol = num < 0;
+        var stack = new Stack<int> ();
+        while (num != 0) {
+            if (symbol) {
+                stack.Push (-num % 7);
+            } else {
+                stack.Push (num % 7);
+            }
+            num = num / 7;
+        }
+        var sb = new StringBuilder ();
+        if (symbol) {
+            sb.Append ("-");
+        }
+        while (stack.Count > 0) {
+            sb.Append (stack.Pop ());
+        }
+        return sb.ToString ();
     }
 }
