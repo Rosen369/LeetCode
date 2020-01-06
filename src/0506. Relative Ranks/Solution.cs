7df9b49ea6a28c902d69dde02320d995d139378d
rosen369 <rosen369@outlook.com>
2020-01-07 00:49:19 +0800

    dict solution

--- src/0506. Relative Ranks/Solution.cs
+++ src/0506. Relative Ranks/Solution.cs
@@ -1,5 +1,21 @@
 public class Solution {
     public string[] FindRelativeRanks (int[] nums) {
-
+        var ranks = new int[nums.Length];
+        nums.CopyTo (ranks, 0);
+        Array.Sort (ranks);
+        Array.Reverse (ranks);
+        var dict = new Dictionary<int, string> ();
+        for (int i = 0; i < ranks.Length; i++) {
+            var rank = "" + (i + 1);
+            if (rank == "1") rank = "Gold Medal";
+            if (rank == "2") rank = "Silver Medal";
+            if (rank == "3") rank = "Bronze Medal";
+            dict.Add (ranks[i], rank);
+        }
+        var res = new string[nums.Length];
+        for (int i = 0; i < nums.Length; i++) {
+            res[i] = dict[nums[i]];
+        }
+        return res;
     }
 }
