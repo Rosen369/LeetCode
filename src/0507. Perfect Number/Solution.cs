05dec6e42de6770c48b40aaa07cb11408c72d7f2
rosen369 <rosen369@outlook.com>
2020-01-08 00:16:06 +0800

    divide solution

--- src/0507. Perfect Number/Solution.cs
+++ src/0507. Perfect Number/Solution.cs
@@ -1,5 +1,12 @@
 public class Solution {
     public bool CheckPerfectNumber (int num) {
-
+        if (num == 1) return false;
+        var sum = 1;
+        for (int i = 2; i <= Math.Sqrt (num); i++) {
+            if (num % i == 0) {
+                sum += i + num / i;
+            }
+        }
+        return sum == num;
     }
 }
