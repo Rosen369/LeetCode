3b2e9a51bb7d9d578e2cc608870f468b134a3d00
rosen369 <rosen369@outlook.com>
2020-01-09 00:54:24 +0800

    dp solution

--- src/0509. Fibonacci Number/Solution.cs
+++ src/0509. Fibonacci Number/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public int Fib (int N) {
+        var dp = new int[N + 1];
+        return FibDP (N, dp);
+    }
 
+    public int FibDP (int N, int[] dp) {
+        if (N < 2) {
+            return N;
+        }
+        if (dp[N] != 0) {
+            return dp[N];
+        }
+        dp[N] = FibDP (N - 2, dp) + FibDP (N - 1, dp);
+        return dp[N];
     }
 }
