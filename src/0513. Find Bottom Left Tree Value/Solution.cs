6d7ba8cf859b3c18779287c04664ab8c75739869
rosen369 <rosen369@outlook.com>
2020-01-10 22:03:05 +0800

    bfs solution using queue

--- src/0513. Find Bottom Left Tree Value/Solution.cs
+++ src/0513. Find Bottom Left Tree Value/Solution.cs
@@ -9,6 +9,22 @@
  */
 public class Solution {
     public int FindBottomLeftValue (TreeNode root) {
-
+        var res = 0;
+        var queue = new Queue<TreeNode> ();
+        queue.Enqueue (root);
+        while (queue.Count > 0) {
+            var count = queue.Count;
+            res = queue.Peek ().val;
+            for (int i = 0; i < count; i++) {
+                var node = queue.Dequeue ();
+                if (node.left != null) {
+                    queue.Enqueue (node.left);
+                }
+                if (node.right != null) {
+                    queue.Enqueue (node.right);
+                }
+            }
+        }
+        return res;
     }
 }
