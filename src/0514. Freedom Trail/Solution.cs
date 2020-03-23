2b9ec14054b65f55ec50a5ae11cf13d8f4c07a4e
rosen369 <rosen369@outlook.com>
2020-03-24 00:43:02 +0800

    dfs with memorization

--- src/0514. Freedom Trail/Solution.cs
+++ src/0514. Freedom Trail/Solution.cs
@@ -1,5 +1,31 @@
 public class Solution {
     public int FindRotateSteps (string ring, string key) {
+        var dict = new Dictionary<string, int> ();
+        return DFS (ring, key, 0, dict);
+    }
 
+    private int DFS (string ring, string key, int index, IDictionary<string, int> dict) {
+        if (key.Length == index) {
+            return 0;
+        }
+        var hashKey = ring + index;
+        if (dict.ContainsKey (hashKey)) {
+            return dict[hashKey];
+        }
+        var minSteps = int.MaxValue;
+
+        var c = key[index];
+
+        for (int i = 0; i < ring.Length; i++) {
+            if (ring[i] == c) {
+                var steps = 1 + Math.Min (i, ring.Length - i);
+                var next = ring.Substring (i) + ring.Substring (0, i);
+                steps += this.DFS (next, key, index + 1, dict);
+                minSteps = Math.Min (steps, minSteps);
+            }
+        }
+
+        dict.Add (hashKey, minSteps);
+        return minSteps;
     }
 }
