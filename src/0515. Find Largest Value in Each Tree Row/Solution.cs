a50779dd8afb471929d94fa1730ff94f14f6c916
rosen369 <rosen369@outlook.com>
2020-03-24 18:19:29 +0800

    bfs solution

--- src/0515. Find Largest Value in Each Tree Row/Solution.cs
+++ src/0515. Find Largest Value in Each Tree Row/Solution.cs
@@ -9,6 +9,28 @@
  */
 public class Solution {
     public IList<int> LargestValues (TreeNode root) {
-
+        var res = new List<int> ();
+        if (root == null) {
+            return res;
+        }
+        var row = new List<TreeNode> ();
+        row.Add (root);
+        while (row.Count != 0) {
+            var max = int.MinValue;
+            var next = new List<TreeNode> ();
+            for (int i = 0; i < row.Count; i++) {
+                var node = row[i];
+                max = Math.Max (node.val, max);
+                if (node.left != null) {
+                    next.Add (node.left);
+                }
+                if (node.right != null) {
+                    next.Add (node.right);
+                }
+            }
+            res.Add (max);
+            row = next;
+        }
+        return res;
     }
 }
