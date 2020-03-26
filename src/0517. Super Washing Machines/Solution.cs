c5076293ffc54c485ae1233ee4241bb60f67cc31
rosen369 <rosen369@outlook.com>
2020-03-27 00:11:38 +0800

    count method

--- src/0517. Super Washing Machines/Solution.cs
+++ src/0517. Super Washing Machines/Solution.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public int FindMinMoves (int[] machines) {
-
+        var total = 0;
+        for (int i = 0; i < machines.Length; i++) {
+            total += machines[i];
+        }
+        if (total % machines.Length != 0) {
+            return -1;
+        }
+        var target = total / machines.Length;
+        var max = 0;
+        var count = 0;
+        for (int i = 0; i < machines.Length; i++) {
+            count += machines[i] - target;
+            max = Math.Max (max, Math.Abs (count));
+            max = Math.Max (max, machines[i] - target);
+        }
+        return max;
     }
 }
