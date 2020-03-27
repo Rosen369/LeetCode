fa33087d3fd6c3f90c3c526c00cf08bd8e264013
rosen369 <rosen369@outlook.com>
2020-03-27 22:03:15 +0800

    add random flip matrix

---
+++ src/0519. Random Flip Matrix/Solution.cs
@@ -0,0 +1,21 @@
+public class Solution {
+
+    public Solution (int n_rows, int n_cols) {
+
+    }
+
+    public int[] Flip () {
+
+    }
+
+    public void Reset () {
+
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(n_rows, n_cols);
+ * int[] param_1 = obj.Flip();
+ * obj.Reset();
+ */
