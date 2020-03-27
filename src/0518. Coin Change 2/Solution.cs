7e8656c82eb02c6f45147510816884680c97865b
rosen369 <rosen369@outlook.com>
2020-03-27 21:57:17 +0800

    dp solution

--- src/0518. Coin Change 2/Solution.cs
+++ src/0518. Coin Change 2/Solution.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public int Change (int amount, int[] coins) {
-
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int j = 0; j < coins.Length; j++) {
+            for (int i = 1; i <= amount; i++) {
+                if (i - coins[j] >= 0) {
+                    dp[i] += dp[i - coins[j]];
+                }
+            }
+        }
+        return dp[amount];
     }
 }
