8550b504389890feaf6d969ee25379249cb4a02e
rosen369 <rosen369@outlook.com>
2020-04-23 00:43:20 +0800

    hash set solution

--- src/0519. Random Flip Matrix/Solution.cs
+++ src/0519. Random Flip Matrix/Solution.cs
@@ -1,15 +1,38 @@
 public class Solution {
 
     public Solution (int n_rows, int n_cols) {
-
+        this._rows = n_rows;
+        this._cols = n_cols;
+        this._occupied = new HashSet<int> ();
+        this._rand = new Random ();
+        this._total = n_rows * n_cols;
     }
 
-    public int[] Flip () {
+    private int _rows;
 
+    private int _cols;
+
+    private int _total;
+
+    private HashSet<int> _occupied;
+
+    private Random _rand;
+
+    public int[] Flip () {
+        var max = this._total - this._occupied.Count ();
+        var next = this._rand.Next (0, max);
+        while (this._occupied.Contains (next)) {
+            next++;
+        }
+        this._occupied.Add (next);
+        var res = new int[2];
+        res[0] = next / this._cols;
+        res[1] = next % this._cols;
+        return res;
     }
 
     public void Reset () {
-
+        this._occupied = new HashSet<int> ();
     }
 }
 
