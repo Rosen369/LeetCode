6e9406d08ac7e2d4d388739ca0c9c20570bac419
rosen369 <rosen369@outlook.com>
2020-04-23 23:18:01 +0800

    simple solution

--- src/0520. Detect Capital/Solution.cs
+++ src/0520. Detect Capital/Solution.cs
@@ -1,5 +1,19 @@
 public class Solution {
     public bool DetectCapitalUse (string word) {
-
+        if (word.Length == 1) {
+            return true;
+        }
+        var head = word[0] <= 'Z';
+        var flag = word[1] <= 'Z';
+        if (head == false && flag == true) {
+            return false;
+        }
+        for (int i = 2; i < word.Length; i++) {
+            var cap = word[i] <= 'Z';
+            if (cap != flag) {
+                return false;
+            }
+        }
+        return true;
     }
 }
