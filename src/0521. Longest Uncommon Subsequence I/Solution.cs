7a3e6e2ed7466dc02e898ae9580751fe9d4c744d
rosen369 <rosen369@outlook.com>
2020-06-01 22:12:30 +0800

    simple solution

--- src/0521. Longest Uncommon Subsequence I/Solution.cs
+++ src/0521. Longest Uncommon Subsequence I/Solution.cs
@@ -1,5 +1,8 @@
 public class Solution {
     public int FindLUSlength (string a, string b) {
-
+        if (a.Equals (b)) {
+            return -1;
+        }
+        return Math.Max (a.Length, b.Length);
     }
 }
