df54ab7ce6b699b54428acae6d0df8808409c55d
rosen369 <rosen369@outlook.com>
2020-06-03 00:07:15 +0800

    compare solution

--- src/0522. Longest Uncommon Subsequence II/Solution.cs
+++ src/0522. Longest Uncommon Subsequence II/Solution.cs
@@ -1,5 +1,34 @@
 public class Solution {
     public int FindLUSlength (string[] strs) {
+        var res = -1;
+        var n = strs.Length;
+        for (int i = 0; i < n; i++) {
+            if (strs[i].Length < res) {
+                continue;
+            }
+            var j = 0;
+            while (j < n) {
+                if (i != j && IsSubsequence (strs[i], strs[j])) {
+                    break;
+                }
+                j++;
+            }
+            if (j == n) {
+                res = Math.Max (res, strs[i].Length);
+            }
+        }
+        return res;
+    }
 
+    public bool IsSubsequence (string a, string b) {
+        var i = 0;
+        var j = 0;
+        while (i < a.Length && j < b.Length) {
+            if (a[i] == b[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i == a.Length;
     }
 }
