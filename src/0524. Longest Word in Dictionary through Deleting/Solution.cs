2a21c950ecbe6fcd853126b2e9f50e92c6680645
rosen369 <rosen369@outlook.com>
2020-06-03 23:24:22 +0800

    dict solution

--- src/0524. Longest Word in Dictionary through Deleting/Solution.cs
+++ src/0524. Longest Word in Dictionary through Deleting/Solution.cs
@@ -1,5 +1,63 @@
 public class Solution {
     public string FindLongestWord (string s, IList<string> d) {
+        var res = string.Empty;
+        var dict = new Dictionary<char, IList<int>> ();
+        for (int i = 0; i < s.Length; i++) {
+            if (!dict.ContainsKey (s[i])) {
+                dict[s[i]] = new List<int> ();
+            }
+            dict[s[i]].Add (i);
+        }
+        for (int i = 0; i < d.Count; i++) {
+            var w = d[i];
+            if (w.Length < res.Length) {
+                continue;
+            }
+            if (this.CanForm (dict, w)) {
+                if (res.Length == w.Length) {
+                    res = this.Less (w, res) ? w : res;
+                } else {
+                    res = w;
+                }
+            }
+        }
+        return res;
+    }
 
+    private bool CanForm (IDictionary<char, IList<int>> dict, string word) {
+        var pos = -1;
+        for (int i = 0; i < word.Length; i++) {
+            if (dict.ContainsKey (word[i])) {
+                var list = dict[word[i]];
+                var hasBigger = false;
+                for (int j = 0; j < list.Count; j++) {
+                    if (list[j] > pos) {
+                        hasBigger = true;
+                        pos = list[j];
+                        break;
+                    }
+                }
+                if (!hasBigger) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool Less (string a, string b) {
+        for (int i = 0; i < a.Length; i++) {
+            if (a[i] == b[i]) {
+                continue;
+            }
+            if (a[i] < b[i]) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
