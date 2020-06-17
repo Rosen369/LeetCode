7b7a8179056294a11fa87849b264e8bfd695a43e
rosen369 <rosen369@outlook.com>
2020-06-17 22:04:50 +0800

    dict solution

--- src/0525. Contiguous Array/Solution.cs
+++ src/0525. Contiguous Array/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public int FindMaxLength (int[] nums) {
-
+        var max = 0;
+        var dict = new Dictionary<int, int> ();
+        dict.Add (0, -1);
+        var count = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            count += nums[i] == 0 ? -1 : 1;
+            if (dict.ContainsKey (count)) {
+                max = Math.Max (max, i - dict[count]);
+            } else {
+                dict.Add (count, i);
+            }
+        }
+        return max;
     }
 }
