d8768a671f68a7f2f0920dcd102c79d65dd83bbe
rosen369 <rosen369@outlook.com>
2020-06-18 21:52:15 +0800

    cache solution

--- src/0528. Random Pick with Weight/Solution.cs
+++ src/0528. Random Pick with Weight/Solution.cs
@@ -1,11 +1,32 @@
 public class Solution {
 
     public Solution (int[] w) {
-
+        this._w = w;
+        this._rand = new Random ();
+        this.CalcCount ();
     }
 
-    public int PickIndex () {
+    private int[] _w;
 
+    private int _max;
+
+    private Random _rand;
+
+    public int PickIndex () {
+        var next = this._rand.Next (0, this._max);
+        for (int i = 0; i < this._w.Length; i++) {
+            if (next < this._w[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void CalcCount () {
+        for (int i = 1; i < this._w.Length; i++) {
+            this._w[i] += this._w[i - 1];
+        }
+        this._max = this._w[this._w.Length - 1];
     }
 }
 
