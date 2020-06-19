fd9d931bb88b68a33e8e9283a48149fbdcf68503
rosen369 <rosen369@outlook.com>
2020-06-19 22:21:44 +0800

    pre order

--- src/0530. Minimum Absolute Difference in BST/Solution.cs
+++ src/0530. Minimum Absolute Difference in BST/Solution.cs
@@ -13,6 +13,23 @@
  */
 public class Solution {
     public int GetMinimumDifference (TreeNode root) {
+        var list = new List<int> ();
+        return this.PreOrder (root, list);
+    }
 
+    private int PreOrder (TreeNode node, IList<int> list) {
+        var min = int.MaxValue;
+        if (node == null) {
+            return min;
+        }
+        var leftDiff = this.PreOrder (node.left, list);
+        list.Add (node.val);
+        if (list.Count > 1) {
+            min = list[list.Count - 1] - list[list.Count - 2];
+        }
+        var rightDiff = this.PreOrder (node.right, list);
+        min = Math.Min (min, leftDiff);
+        min = Math.Min (min, rightDiff);
+        return min;
     }
 }
