d04287d6b930a061d01d8656f4a4047c9c3ee6d3
rosen369 <rosen369@outlook.com>
2020-06-29 23:00:16 +0800

    dict solution

--- src/0532. K-diff Pairs in an Array/Solution.cs
+++ src/0532. K-diff Pairs in an Array/Solution.cs
@@ -1,5 +1,26 @@
 public class Solution {
     public int FindPairs (int[] nums, int k) {
-
+        if (k < 0) {
+            return 0;
+        }
+        var res = 0;
+        var dict = new Dictionary<int, int> ();
+        for (int i = 0; i < nums.Length; i++) {
+            if (dict.ContainsKey (nums[i])) {
+                if (k == 0 && dict[nums[i]] == 1) {
+                    res++;
+                }
+                dict[nums[i]]++;
+            } else {
+                if (dict.ContainsKey (nums[i] + k)) {
+                    res++;
+                }
+                if (dict.ContainsKey (nums[i] - k)) {
+                    res++;
+                }
+                dict.Add (nums[i], 1);
+            }
+        }
+        return res;
     }
 }
