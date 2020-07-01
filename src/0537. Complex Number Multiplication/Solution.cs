5d887965b3a207dc1a0d09d8b3c279c592d27ff3
rosen369 <rosen369@outlook.com>
2020-07-01 18:29:43 +0800

    simple math

--- src/0537. Complex Number Multiplication/Solution.cs
+++ src/0537. Complex Number Multiplication/Solution.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public string ComplexNumberMultiply (string a, string b) {
+        var a1 = Convert.ToInt32 (a.Split ('+') [0]);
+        var a2 = Convert.ToInt32 (a.Split ('+') [1].Split ('i') [0]);
+        var b1 = Convert.ToInt32 (b.Split ('+') [0]);
+        var b2 = Convert.ToInt32 (b.Split ('+') [1].Split ('i') [0]);
 
+        var c1 = a1 * b1 - a2 * b2;
+        var c2 = a1 * b2 + a2 * b1;
+
+        var res = c1 + "+" + c2 + "i";
+        return res;
     }
 }
