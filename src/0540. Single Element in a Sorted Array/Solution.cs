fe1d0c7eeb844b0179f90a5120620f77b08d213d
rosen369 <rosen369@outlook.com>
2020-07-02 19:20:20 +0800

    binary search solution

--- src/0540. Single Element in a Sorted Array/Solution.cs
+++ src/0540. Single Element in a Sorted Array/Solution.cs
@@ -1,5 +1,18 @@
 public class Solution {
     public int SingleNonDuplicate (int[] nums) {
-
+        var left = 0;
+        var right = nums.Length - 1;
+        while (left < right) {
+            var mid = (left + right) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] != nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 2;
+            }
+        }
+        return nums[left];
     }
 }
