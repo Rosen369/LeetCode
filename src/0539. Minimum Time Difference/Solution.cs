395e900fc25652ce5a4893a7cae88a9f8c5057fb
rosen369 <rosen369@outlook.com>
2020-07-02 18:22:15 +0800

    sort minute array method

--- src/0539. Minimum Time Difference/Solution.cs
+++ src/0539. Minimum Time Difference/Solution.cs
@@ -1,5 +1,18 @@
 public class Solution {
     public int FindMinDifference (IList<string> timePoints) {
-
+        var minutes = new int[timePoints.Count];
+        for (int i = 0; i < timePoints.Count; i++) {
+            var p = timePoints[i];
+            var h = Convert.ToInt32 (timePoints[i].Split (':') [0]);
+            var m = Convert.ToInt32 (timePoints[i].Split (':') [1]);
+            minutes[i] = h * 60 + m;
+        }
+        Array.Sort (minutes);
+        var min = int.MaxValue;
+        for (int i = 0; i < minutes.Length - 1; i++) {
+            min = Math.Min (min, minutes[i + 1] - minutes[i]);
+        }
+        min = Math.Min (min, minutes[0] - minutes[minutes.Length - 1] + 1440);
+        return min;
     }
 }
