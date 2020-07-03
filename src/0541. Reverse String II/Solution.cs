41885b5fbf25d377225dcb4e4f9e5861918319b0
rosen369 <rosen369@outlook.com>
2020-07-03 18:20:32 +0800

    swap solution

--- src/0541. Reverse String II/Solution.cs
+++ src/0541. Reverse String II/Solution.cs
@@ -1,5 +1,22 @@
 public class Solution {
     public string ReverseStr (string s, int k) {
+        var arr = s.ToCharArray ();
+        var start = 0;
+        while (start < s.Length) {
+            var end = Math.Min (start + k - 1, s.Length - 1);
+            this.Swap (arr, start, end);
+            start += 2 * k;
+        }
+        return new string (arr);
+    }
 
+    private void Swap (char[] arr, int left, int right) {
+        while (left < right) {
+            var temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
     }
 }
