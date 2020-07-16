18ccbcdc2186c053e4e5450596c32da1cf343e57
rosen369 <rosen369@outlook.com>
2020-07-16 21:50:30 +0800

    dp solution

--- src/0546. Remove Boxes/Solution.cs
+++ src/0546. Remove Boxes/Solution.cs
@@ -1,5 +1,30 @@
 public class Solution {
     public int RemoveBoxes (int[] boxes) {
+        var n = boxes.Length;
+        var dp = new int[n, n, n];
+        return this.Sub (boxes, dp, 0, n - 1, 0);
+    }
 
+    private int Sub (int[] boxes, int[, , ] dp, int i, int j, int k) {
+        if (i > j) {
+            return 0;
+        }
+        if (dp[i, j, k] > 0) {
+            return dp[i, j, k];
+        }
+
+        for (; i + 1 <= j && boxes[i + 1] == boxes[i]; i++, k++);
+
+        var res = (k + 1) * (k + 1) + this.Sub (boxes, dp, i + 1, j, 0);
+
+        for (int m = i + 1; m <= j; m++) {
+            if (boxes[i] == boxes[m]) {
+                var sep = this.Sub (boxes, dp, i + 1, m - 1, 0) + this.Sub (boxes, dp, m, j, k + 1);
+                res = Math.Max (res, sep);
+            }
+        }
+
+        dp[i, j, k] = res;
+        return res;
     }
 }
