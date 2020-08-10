38dd0ec7886548975c40bb360c8ff4e274bbd7f2
rosen369 <rosen369@outlook.com>
2020-08-11 00:02:49 +0800

    DFS solution

--- src/0547. Friend Circles/Solution.cs
+++ src/0547. Friend Circles/Solution.cs
@@ -1,5 +1,26 @@
 public class Solution {
     public int FindCircleNum (int[][] M) {
+        var visited = new HashSet<int> ();
+        var res = 0;
+        for (int i = 0; i < M.Length; i++) {
+            if (visited.Contains (i)) {
+                continue;
+            }
+            res++;
+            this.DFS (M, visited, i);
+        }
+        return res;
+    }
 
+    private void DFS (int[][] M, HashSet<int> visited, int n) {
+        if (visited.Contains (n)) {
+            return;
+        }
+        visited.Add (n);
+        for (int i = 0; i < M.Length; i++) {
+            if (M[n][i] == 1) {
+                this.DFS (M, visited, i);
+            }
+        }
     }
 }
