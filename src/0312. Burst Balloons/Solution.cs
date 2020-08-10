a4b99c81129cb64f1f5a41571c57e808473c47bc
rosen369 <rosen369@outlook.com>
2020-08-10 22:32:47 +0800

    dp solution

--- src/0312. Burst Balloons/Solution.cs
+++ src/0312. Burst Balloons/Solution.cs
@@ -1,31 +1,27 @@
 public class Solution {
     public int MaxCoins (int[] nums) {
-        var len = nums.Length;
-        var expand = new int[len + 2];
-        expand[0] = 1;
-        expand[len + 1] = 1;
-        for (int i = 1; i <= len; i++) {
-            expand[i] = nums[i - 1];
-        }
-        var memo = new int[len + 2, len + 2];
-        return Backtracking (expand, memo, 0, len + 1);
+        var n = nums.Length;
+        var dp = new int[n, n];
+        return this.DFS (dp, 0, n - 1, nums, 1, 1);
     }
 
-    public int Backtracking (int[] nums, int[, ] memo, int left, int right) {
-        if (left + 1 == right) {
+    private int DFS (int[, ] dp, int i, int j, int[] nums, int left, int right) {
+        if (i > j) {
             return 0;
         }
-        if (memo[left, right] > 0) {
-            return memo[left, right];
+        if (dp[i, j] > 0) {
+            return dp[i, j];
         }
         var max = 0;
-        for (int i = left + 1; i < right; i++) {
-            var lMax = Backtracking (nums, memo, left, i);
-            var rMax = Backtracking (nums, memo, i, right);
-            var curr = nums[left] * nums[i] * nums[right] + lMax + rMax;
-            max = Math.Max (max, curr);
+        for (var n = i; n <= j; n++) {
+            var p1 = this.DFS (dp, i, n - 1, nums, left, nums[n]);
+            var p2 = left * nums[n] * right;
+            var p3 = this.DFS (dp, n + 1, j, nums, nums[n], right);
+
+            max = Math.Max (max, p1 + p2 + p3);
         }
-        memo[left, right] = max;
+
+        dp[i, j] = max;
         return max;
     }
 }
