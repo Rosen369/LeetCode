dc3fd67c316f1464eda22a6de96598732880d6a6
rosen369 <rosen369@outlook.com>
2020-08-11 18:22:15 +0800

    stupid solution

--- src/0551. Student Attendance Record I/Solution.cs
+++ src/0551. Student Attendance Record I/Solution.cs
@@ -1,5 +1,24 @@
 public class Solution {
     public bool CheckRecord (string s) {
+        var a = 0;
+        var l = 0;
+        for (int i = 0; i < s.Length; i++) {
+            if (s[i] == 'A') {
+                a++;
+            }
+            if (a > 1) {
+                return false;
+            }
 
+            if (s[i] == 'L') {
+                l++;
+            } else {
+                l = 0;
+            }
+            if (l > 2) {
+                return false;
+            }
+        }
+        return true;
     }
 }
