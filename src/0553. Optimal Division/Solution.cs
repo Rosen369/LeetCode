07157e201a32a572e5061d0a3cb4a9e438a2e979
rosen369 <rosen369@outlook.com>
2020-09-10 00:57:07 +0800

    math solution

--- src/0553. Optimal Division/Solution.cs
+++ src/0553. Optimal Division/Solution.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public string OptimalDivision (int[] nums) {
-
+        if (nums.Length == 1) {
+            return nums[0] + "";
+        }
+        if (nums.Length == 2) {
+            return nums[0] + "/" + nums[1];
+        }
+        var sb = new StringBuilder ();
+        sb.Append (nums[0]).Append ("/").Append ("(");
+        for (int i = 1; i < nums.Length; i++) {
+            sb.Append (nums[i]).Append ("/");
+        }
+        var res = sb.ToString ().TrimEnd ('/') + ")";
+        return res;
     }
 }
