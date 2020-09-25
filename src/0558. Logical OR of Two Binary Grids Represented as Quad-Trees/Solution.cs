2fbac25dc7c5b7491ad2f53b834e966af0a6e1e2
rosen369 <rosen369@outlook.com>
2020-09-25 22:50:23 +0800

    recursion solution

--- src/0558. Logical OR of Two Binary Grids Represented as Quad-Trees/Solution.cs
+++ src/0558. Logical OR of Two Binary Grids Represented as Quad-Trees/Solution.cs
@@ -22,6 +22,32 @@ public class Node {
 
 public class Solution {
     public Node Intersect (Node quadTree1, Node quadTree2) {
+        var value = quadTree1.val || quadTree2.val;
+        if (quadTree1.isLeaf && quadTree2.isLeaf) {
+            return new Node (value, true, null, null, null, null);
+        }
 
+        var qt1TL = quadTree1.isLeaf ? new Node (quadTree1.val, true, null, null, null, null) : quadTree1.topLeft;
+        var qt1TR = quadTree1.isLeaf ? new Node (quadTree1.val, true, null, null, null, null) : quadTree1.topRight;
+        var qt1BL = quadTree1.isLeaf ? new Node (quadTree1.val, true, null, null, null, null) : quadTree1.bottomLeft;
+        var qt1BR = quadTree1.isLeaf ? new Node (quadTree1.val, true, null, null, null, null) : quadTree1.bottomRight;
+
+        var qt2TL = quadTree2.isLeaf ? new Node (quadTree2.val, true, null, null, null, null) : quadTree2.topLeft;
+        var qt2TR = quadTree2.isLeaf ? new Node (quadTree2.val, true, null, null, null, null) : quadTree2.topRight;
+        var qt2BL = quadTree2.isLeaf ? new Node (quadTree2.val, true, null, null, null, null) : quadTree2.bottomLeft;
+        var qt2BR = quadTree2.isLeaf ? new Node (quadTree2.val, true, null, null, null, null) : quadTree2.bottomRight;
+
+        var tl = this.Intersect (qt1TL, qt2TL);
+        var tr = this.Intersect (qt1TR, qt2TR);
+        var bl = this.Intersect (qt1BL, qt2BL);
+        var br = this.Intersect (qt1BR, qt2BR);
+
+        if (tl.isLeaf && tr.isLeaf && bl.isLeaf && br.isLeaf) {
+            if (tl.val == tr.val && tr.val == bl.val && bl.val == br.val) {
+                return new Node (tl.val, true, null, null, null, null);
+            }
+        }
+
+        return new Node (value, false, tl, tr, bl, br);
     }
 }
