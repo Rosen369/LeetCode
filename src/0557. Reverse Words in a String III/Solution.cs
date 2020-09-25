5a6e2d2f9c3165e8d8983e9d7fdf36302e241f73
rosen369 <rosen369@outlook.com>
2020-09-25 18:04:36 +0800

    stupid solution

--- src/0557. Reverse Words in a String III/Solution.cs
+++ src/0557. Reverse Words in a String III/Solution.cs
@@ -1,5 +1,10 @@
 public class Solution {
     public string ReverseWords (string s) {
-
+        var words = s.Split (' ');
+        for (int i = 0; i < words.Length; i++) {
+            words[i] = new string (words[i].ToCharArray ().Reverse ().ToArray ());
+        }
+        var res = string.Join (" ", words);
+        return res;
     }
 }
