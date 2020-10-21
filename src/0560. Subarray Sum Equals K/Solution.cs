eb71a79132c889a9519e426e06fd40acdf49bbbe
rosen369 <rosen369@outlook.com>
2020-10-21 23:54:13 +0800

    dict solution

--- src/0560. Subarray Sum Equals K/Solution.cs
+++ src/0560. Subarray Sum Equals K/Solution.cs
@@ -1,5 +1,21 @@
 public class Solution {
     public int SubarraySum (int[] nums, int k) {
+        var res = 0;
+        var sum = 0;
+        var dict = new Dictionary<int, int> ();
 
+        dict[0] = 1;
+        for (int i = 0; i < nums.Length; i++) {
+            sum += nums[i];
+            if (dict.ContainsKey (sum - k)) {
+                res += dict[sum - k];
+            }
+            if (dict.ContainsKey (sum)) {
+                dict[sum]++;
+            } else {
+                dict[sum] = 1;
+            }
+        }
+        return res;
     }
 }
