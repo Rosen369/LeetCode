a7cb3c9adcbc58ebb79729751800be9b1dbfd4ec
rosen369 <rosen369@outlook.com>
2020-10-22 18:52:49 +0800

    DFS solution

--- src/0563. Binary Tree Tilt/Solution.cs
+++ src/0563. Binary Tree Tilt/Solution.cs
@@ -12,7 +12,23 @@
  * }
  */
 public class Solution {
-    public int FindTilt (TreeNode root) {
 
+    private int _treeTilt = 0;
+
+    public int FindTilt (TreeNode root) {
+        this.DFS (root);
+        return this._treeTilt;
+    }
+
+    private int DFS (TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        var left = this.DFS (node.left);
+        var right = this.DFS (node.right);
+        var res = node.val + left + right;
+
+        this._treeTilt += Math.Abs (left - right);
+        return res;
     }
 }
